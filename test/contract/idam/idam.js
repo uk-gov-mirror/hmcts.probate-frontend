97332e494ae5e358c95c46dcf5a6254e0f798f0a
douglasrice <douglasrice@yahoo.com>
2018-06-19 17:26:38 +0100

    CNP (#130)

--- test/contract/idam/idam.js
+++ test/contract/idam/idam.js
@@ -2,13 +2,9 @@ const assert = require('chai').assert;
 const expect = require('chai').expect;
 const include = require('chai').include;
 const request = require('superagent');
+const testConfig = require('test/config');
 
-require('superagent-proxy')(request);
-
-const IDAM_URL = 'https://probate-frontend-saat-staging.service.core-compute-saat.internal';
-const PROXY_URL = 'http://proxyout.reform.hmcts.net';
-const PROXY_PORT = 8080;
-const PROXY = PROXY_URL + ':' + PROXY_PORT;
+const IDAM_URL = testConfig.TestIdamLoginUrl;
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
@@ -18,7 +14,6 @@ describe('Idam Tests', function() {
 
         request
             .get(IDAM_URL)
-            .proxy(PROXY)
             .timeout(3600 * 1000)
             .end((err, res) => {
                 if (err) {
