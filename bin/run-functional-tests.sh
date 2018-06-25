b5119ab1083dec6c2d689d8a674b3a80a84a7fc7
reddy1praveen <32567752+reddy1praveen@users.noreply.github.com>
2018-06-25 12:20:37 +0100

    Update run-functional-tests.sh

--- bin/run-functional-tests.sh
+++ bin/run-functional-tests.sh
@@ -5,3 +5,6 @@ export TEST_E2E_URL=$(echo ${TEST_URL} | sed -e  "s/-staging//")
 
 yarn test-e2e
 
+#below 2 lines for function test output in cnp pipeline
+mkdir functional-output
+cp -r output/mochawesome.* functional-output/.
