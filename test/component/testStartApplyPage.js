f920213007f03941c6278523ee5d269d7a779481
Harjeet Kalsi <zcaphkk@ucl.ac.uk>
2018-07-05 12:18:05 +0100

    PRO-3066: Adding test for /startApply page and changing ' and £ to html format

---
+++ test/component/testStartApplyPage.js
@@ -0,0 +1,29 @@
+const TestWrapper = require('test/util/TestWrapper'),
+    TaskList = require('app/steps/ui/tasklist/index');
+
+describe('startApply', () => {
+    let testWrapper;
+    const expectedNextUrlForTaskList = TaskList.getUrl();
+
+    beforeEach(() => {
+        testWrapper = new TestWrapper('StartApply');
+    });
+
+    afterEach(() => {
+        testWrapper.destroy();
+    });
+
+    describe('Verify Content, Errors and Redirection', () => {
+
+        it('test right content loaded on the page', (done) => {
+            const excludeKeys = [];
+
+            testWrapper.testContent(done, excludeKeys);
+        });
+
+        it(`test it redirects to next page: ${expectedNextUrlForTaskList}`, (done) => {
+            testWrapper.testRedirect(done, {}, expectedNextUrlForTaskList);
+        });
+
+    });
+});
