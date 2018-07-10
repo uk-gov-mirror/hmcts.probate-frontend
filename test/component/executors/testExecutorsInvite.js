fb2c859c1152e5f1a17d0ec55dff9935b5db2295
Tom Elliott <tomxelliott@hotmail.co.uk>
2018-07-10 16:08:16 +0100

    PRO-2456: Implement conditional rendering of content on invites page 	  - if more than one other executor then the placement of the apostrophe is different 	  - component tests updated to test for correct content

--- test/component/executors/testExecutorsInvite.js
+++ test/component/executors/testExecutorsInvite.js
@@ -24,8 +24,22 @@ describe('executors-invite', () => {
 
     describe('Verify Content, Errors and Redirection', () => {
 
-        it('test content loaded on the page', (done) => {
-            testWrapper.testContent(done);
+        it('test correct content loaded on the page when only 1 other executor', (done) => {
+            sessionData.executors.executorsNumber = 2;
+            testWrapper.agent.post('/prepare-session/form')
+                .send(sessionData)
+                .end(() => {
+                    testWrapper.testContent(done, ['heading3Multiple']);
+                });
+        });
+
+        it('test correct content loaded on the page when more than 1 other executor', (done) => {
+            sessionData.executors.executorsNumber = 3;
+            testWrapper.agent.post('/prepare-session/form')
+                .send(sessionData)
+                .end(() => {
+                    testWrapper.testContent(done, ['heading3Single']);
+                });
         });
 
         it('test content displays only the applying executors but not the main applicant', (done) => {
