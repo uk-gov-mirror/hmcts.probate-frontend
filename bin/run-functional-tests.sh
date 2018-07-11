cac90c6c1bfb3a87526ebed1374f9442d890acc2
praveenr <praveen.moj@gmail.com>
2018-07-11 17:02:40 +0100

    Added switch for e2e

--- bin/run-functional-tests.sh
+++ bin/run-functional-tests.sh
@@ -3,8 +3,10 @@ set -ex
 
 export TEST_E2E_URL=$(echo ${TEST_URL})
 
-yarn test-e2e
+if [ "$RUN_E2E_TEST" = true ] ; then
+    yarn test-e2e
+    #below 2 lines for function test output in cnp pipeline
+    mkdir functional-output
+    cp -r output/mochawesome.* functional-output/. 2>/dev/null  //fail safe
+fi
 
-#below 2 lines for function test output in cnp pipeline
-mkdir functional-output
-cp -r output/mochawesome.* functional-output/.
