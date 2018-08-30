a42d6f76988feeaebe405b9a850c2aa0b6a0962e
Luca Del Buono <LucaDelBuono@users.noreply.github.com>
2018-08-30 14:35:00 +0100

    PRO-3070: Create the Did the Deceased Live in England or Wales page

--- app/steps/ui/deceased/domicile/index.js
+++ app/steps/ui/deceased/domicile/index.js
@@ -1,9 +1,21 @@
 const ValidationStep = require('app/core/steps/ValidationStep');
-
+const json = require('app/resources/en/translation/will/left.json');
 module.exports = class DeceasedDomicile extends ValidationStep {
 
     static getUrl() {
         return '/deceased-domicile';
     }
 
+    nextStepUrl(ctx) {
+        return this.next(ctx).constructor.getUrl('notInEnglandOrWales');
+    }
+
+    nextStepOptions() {
+        const nextStepOptions = {
+            options: [
+                {key: 'domicile', value: json.optionYes, choice: 'inEnglandOrWales'}
+            ]
+        };
+        return nextStepOptions;
+    }
 };
--- test/component/testStopPage.js
+++ test/component/testStopPage.js
@@ -14,45 +14,45 @@ describe('stop-page', () => {
 
     describe('Verify Content, Errors and Redirection', () => {
 
-        it('test right content loaded on the page', (done) => {
+        it('test right content loaded on the page - no will', (done) => {
             testWrapper.pageUrl = testWrapper.pageToTest.constructor.getUrl('noWill');
-            const excludeKeys = ['notOriginal', 'codicils', 'notExecutor', 'executorApplying', 'ihtNotCompleted', 'mentalCapacity', 'deathCertificate', 'progress'];
+            const excludeKeys = ['notOriginal', 'codicils', 'notExecutor', 'ihtNotCompleted', 'mentalCapacity', 'deathCertificate', 'notInEnglandOrWales', 'progress'];
             testWrapper.testContent(done, excludeKeys);
         });
 
-        it('test right content loaded on the page', (done) => {
+        it('test right content loaded on the page - not original', (done) => {
             testWrapper.pageUrl = testWrapper.pageToTest.constructor.getUrl('notOriginal');
-            const excludeKeys = ['noWill', 'codicils', 'notExecutor', 'executorApplying', 'ihtNotCompleted', 'mentalCapacity', 'deathCertificate', 'progress'];
+            const excludeKeys = ['noWill', 'codicils', 'notExecutor', 'ihtNotCompleted', 'mentalCapacity', 'deathCertificate', 'notInEnglandOrWales', 'progress'];
             testWrapper.testContent(done, excludeKeys);
         });
 
-        it('test right content loaded on the page', (done) => {
+        it('test right content loaded on the page - codicils', (done) => {
             testWrapper.pageUrl = testWrapper.pageToTest.constructor.getUrl('codicils');
-            const excludeKeys = ['noWill', 'notOriginal', 'notExecutor', 'executorApplying', 'ihtNotCompleted', 'mentalCapacity', 'deathCertificate', 'progress'];
+            const excludeKeys = ['noWill', 'notOriginal', 'notExecutor', 'ihtNotCompleted', 'mentalCapacity', 'deathCertificate', 'notInEnglandOrWales', 'progress'];
             testWrapper.testContent(done, excludeKeys);
         });
 
-        it('test right content loaded on the page', (done) => {
+        it('test right content loaded on the page - not executor', (done) => {
             testWrapper.pageUrl = testWrapper.pageToTest.constructor.getUrl('notExecutor');
-            const excludeKeys = ['noWill', 'notOriginal', 'codicils', 'executorApplying', 'ihtNotCompleted', 'mentalCapacity', 'deathCertificate', 'progress'];
+            const excludeKeys = ['noWill', 'notOriginal', 'codicils', 'ihtNotCompleted', 'mentalCapacity', 'deathCertificate', 'notInEnglandOrWales', 'progress'];
             testWrapper.testContent(done, excludeKeys);
         });
 
-        it('test right content loaded on the page - executor applying', (done) => {
-            testWrapper.pageUrl = testWrapper.pageToTest.constructor.getUrl('executorApplying');
+        it('test right content loaded on the page - deceased not in england or wales', (done) => {
+            testWrapper.pageUrl = testWrapper.pageToTest.constructor.getUrl('notInEnglandOrWales');
             const excludeKeys = ['noWill', 'notOriginal', 'codicils', 'notExecutor', 'ihtNotCompleted', 'mentalCapacity', 'deathCertificate', 'progress'];
             testWrapper.testContent(done, excludeKeys);
         });
 
         it('test right content loaded on the page - iht not completed', (done) => {
             testWrapper.pageUrl = testWrapper.pageToTest.constructor.getUrl('ihtNotCompleted');
-            const excludeKeys = ['noWill', 'notOriginal', 'codicils', 'executorApplying', 'notExecutor', 'mentalCapacity', 'deathCertificate', 'progress'];
+            const excludeKeys = ['noWill', 'notOriginal', 'codicils', 'notExecutor', 'mentalCapacity', 'deathCertificate', 'notInEnglandOrWales', 'progress'];
             testWrapper.testContent(done, excludeKeys, {ihtNotCompleted: config.links.ihtNotCompleted});
         });
 
         it('test right content loaded on the page - no death certificate', (done) => {
             testWrapper.pageUrl = testWrapper.pageToTest.constructor.getUrl('deathCertificate');
-            const excludeKeys = ['noWill', 'notOriginal', 'codicils', 'executorApplying', 'notExecutor', 'ihtNotCompleted', 'mentalCapacity', 'contactProbateOffice'];
+            const excludeKeys = ['noWill', 'notOriginal', 'codicils', 'notExecutor', 'ihtNotCompleted', 'mentalCapacity', 'notInEnglandOrWales', 'contactProbateOffice'];
             testWrapper.testContent(done, excludeKeys, {deathCertificate: config.links.deathCertificate});
         });
     });
