792d36cba0424fa6ee78e86576358e2fc3b32e6b
Tom Elliott <tomxelliott@hotmail.co.uk>
2018-08-30 09:20:09 +0100

    PRO-2677: Amended test cases that were not working as expected - now correctly populating session data

--- test/component/executors/testExecutorsAdditionalInviteSent.js
+++ test/component/executors/testExecutorsAdditionalInviteSent.js
@@ -1,16 +1,15 @@
 'use strict';
 const TestWrapper = require('test/util/TestWrapper');
 const TaskList = require('app/steps/ui/tasklist/index');
-// const {assert} = require('chai');
 
 describe('executors-additional-invite-sent', () => {
     let testWrapper;
-    // let sessionData;
+    let sessionData;
     const expectedNextUrlForTaskList = TaskList.getUrl();
 
     beforeEach(() => {
-        testWrapper = new TestWrapper('ExecutorsUpdateInviteSent');
-        // sessionData = require('test/data/executors-invites');
+        testWrapper = new TestWrapper('ExecutorsAdditionalInviteSent');
+        sessionData = require('test/data/executors-invites');
     });
 
     afterEach(() => {
@@ -19,50 +18,40 @@ describe('executors-additional-invite-sent', () => {
     });
 
     describe('Verify Content, Errors and Redirection', () => {
-        it('test correct content loaded on the page when more than 1 other executor added', (done) => {
-            testWrapper.testContent(done);
+
+        it('test correct content loaded on the page when only 1 other executor added', (done) => {
+            sessionData.executors.executorsToNotifyList = [
+                {'fullName': 'other applicant', 'isApplying': true, emailSent: false},
+            ];
+            testWrapper.agent.post('/prepare-session/form')
+                .send(sessionData)
+                .end(() => {
+                    testWrapper.testContent(done, ['header-multiple']);
+                });
         });
 
-        // it('test content displays only the applying executors but not the main applicant', (done) => {
-        //     sessionData.executors.executorsToNotifyList = [
-        //         {'fullName': 'other applicant', 'isApplying': true}
-        //     ];
-        //     testWrapper.agent.post('/prepare-session/form')
-        //         .send(sessionData)
-        //         .end(() => {
-        //             testWrapper.agent.get(testWrapper.pageUrl)
-        //                 .then(response => {
-        //                     assert(response.text.includes('We&rsquo;ve notified the executor you&rsquo;ve added'));
-        //                     done();
-        //                 })
-        //                 .catch(err => {
-        //                     done(err);
-        //                 });
-        //         });
-        // });
-        //
-        // it('test content displays only the applying executors but not the main applicant', (done) => {
-        //         sessionData.executors.executorsToNotifyList = [
-        //             {'fullName': 'other applicant', 'isApplying': true},
-        //             {'fullName': 'harvey', 'isApplying': true}
-        //         ];
-        //     testWrapper.agent.post('/prepare-session/form')
-        //         .send(sessionData)
-        //         .end(() => {
-        //             testWrapper.agent.get(testWrapper.pageUrl)
-        //                 .then(response => {
-        //                     assert(response.text.includes('We&rsquo;ve notified the executors you&rsquo;ve added'));
-        //                     done();
-        //                 })
-        //                 .catch(err => {
-        //                     done(err);
-        //                 });;
-        //         });
-        // });
+        it('test correct content loaded on the page when more than 1 other executor added', (done) => {
+            sessionData.executors.executorsToNotifyList = [
+                {'fullName': 'other applicant', 'isApplying': true, emailSent: false},
+                {'fullName': 'harvey', 'isApplying': true, emailSent: false}
+            ];
+            testWrapper.agent.post('/prepare-session/form')
+                .send(sessionData)
+                .end(() => {
+                    testWrapper.testContent(done, ['header']);
+                });
+        });
 
         it(`test it redirects to next page: ${expectedNextUrlForTaskList}`, (done) => {
             const data = {};
-            testWrapper.testRedirect(done, data, expectedNextUrlForTaskList);
+            sessionData.executors.executorsToNotifyList = [
+                {'fullName': 'other applicant', 'isApplying': true, emailSent: false},
+            ];
+            testWrapper.agent.post('/prepare-session/form')
+                .send(sessionData)
+                .end(() => {
+                    testWrapper.testRedirect(done, data, expectedNextUrlForTaskList);
+                });
         });
     });
 });
