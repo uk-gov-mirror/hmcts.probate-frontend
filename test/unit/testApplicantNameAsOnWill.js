07b926b6ada31c5a29f14c1c3de39f6ee66aa31a
Tom Elliott <tomxelliott@hotmail.co.uk>
2018-09-06 15:40:05 +0100

    PRO-3598: Applied changes after dev review 	  - moved logic out of the template and into the index file to handle current lead applicant name 	  - created function in the formatName class 	  - added unit tests for the new code

--- test/unit/testApplicantNameAsOnWill.js
+++ test/unit/testApplicantNameAsOnWill.js
@@ -15,143 +15,144 @@ describe('ApplicantNameAsOnWill', () => {
         });
     });
 
-    describe('handlePost()', () => {
-        let ctx;
-        let errors;
+        describe('handlePost()', () => {
+            let ctx;
+            let errors;
 
-        it('should return the ctx with the alias and alias reason included', (done) => {
-            ctx = {
-                nameAsOnTheWill: 'No',
-                alias: 'Bobby Alias',
-                aliasReason: 'Divorce'
-            };
-            errors = {};
-            [ctx, errors] = ApplicantNameAsOnWill.handlePost(ctx, errors);
-            expect(ctx).to.deep.equal({
-                nameAsOnTheWill: 'No',
-                alias: 'Bobby Alias',
-                aliasReason: 'Divorce'
+            it('should return the ctx with the alias and alias reason included', (done) => {
+                ctx = {
+                    nameAsOnTheWill: 'No',
+                    alias: 'Bobby Alias',
+                    aliasReason: 'Divorce'
+                };
+                errors = {};
+                [ctx, errors] = ApplicantNameAsOnWill.handlePost(ctx, errors);
+                expect(ctx).to.deep.equal({
+                    nameAsOnTheWill: 'No',
+                    alias: 'Bobby Alias',
+                    aliasReason: 'Divorce'
+                });
+                done();
             });
-            done();
-        });
 
-        it('should return the ctx with the alias, alias reason and other reason included when reason is other', (done) => {
-            ctx = {
-                nameAsOnTheWill: 'No',
-                alias: 'Bobby Alias',
-                aliasReason: 'other',
-                otherReason: 'Legally changed name'
-            };
-            errors = {};
-            [ctx, errors] = ApplicantNameAsOnWill.handlePost(ctx, errors);
-            expect(ctx).to.deep.equal({
-                nameAsOnTheWill: 'No',
-                alias: 'Bobby Alias',
-                aliasReason: 'other',
-                otherReason: 'Legally changed name'
+            it('should return the ctx with the alias, alias reason and other reason included when reason is other', (done) => {
+                ctx = {
+                    nameAsOnTheWill: 'No',
+                    alias: 'Bobby Alias',
+                    aliasReason: 'other',
+                    otherReason: 'Legally changed name'
+                };
+                errors = {};
+                [ctx, errors] = ApplicantNameAsOnWill.handlePost(ctx, errors);
+                expect(ctx).to.deep.equal({
+                    nameAsOnTheWill: 'No',
+                    alias: 'Bobby Alias',
+                    aliasReason: 'other',
+                    otherReason: 'Legally changed name'
+                });
+                done();
             });
-            done();
-        });
 
-        it('should return the ctx with the alias and alias reason removed', (done) => {
-            ctx = {
-                nameAsOnTheWill: 'Yes',
-                alias: 'Bobby Alias',
-                aliasReason: 'Divorce'
-            };
-            errors = {};
-            [ctx, errors] = ApplicantNameAsOnWill.handlePost(ctx, errors);
-            expect(ctx).to.deep.equal({nameAsOnTheWill: 'Yes'});
-            done();
-        });
-
-        it('should return the ctx with the alias, alias reason and other reason removed when reason is other', (done) => {
-            ctx = {
-                nameAsOnTheWill: 'Yes',
-                alias: 'Bobby Alias',
-                aliasReason: 'other',
-                otherReason: 'Legally changed name'
-            };
-            errors = {};
-            [ctx, errors] = ApplicantNameAsOnWill.handlePost(ctx, errors);
-            expect(ctx).to.deep.equal({nameAsOnTheWill: 'Yes'});
-            done();
-        });
-    describe('nextStepOptions()', () => {
-        it('should return the correct next step options', (done) => {
-            const result = ApplicantNameAsOnWill.nextStepOptions();
-            expect(result).to.deep.equal({
-                options: [{
-                    key: 'nameAsOnTheWill',
-                    value: content.optionNo,
-                    choice: 'hasAlias'
-                }]
+            it('should return the ctx with the alias and alias reason removed', (done) => {
+                ctx = {
+                    nameAsOnTheWill: 'Yes',
+                    alias: 'Bobby Alias',
+                    aliasReason: 'Divorce'
+                };
+                errors = {};
+                [ctx, errors] = ApplicantNameAsOnWill.handlePost(ctx, errors);
+                expect(ctx).to.deep.equal({nameAsOnTheWill: 'Yes'});
+                done();
             });
-            done();
-        });
-    });
 
-    describe('handlePost()', () => {
-        let ctx;
-        let errors;
-
-        it('should return the ctx with the alias and alias reason included', (done) => {
-            ctx = {
-                nameAsOnTheWill: 'No',
-                alias: 'Bobby Alias',
-                aliasReason: 'Divorce'
-            };
-            errors = {};
-            [ctx, errors] = ApplicantNameAsOnWill.handlePost(ctx, errors);
-            expect(ctx).to.deep.equal({
-                nameAsOnTheWill: 'No',
-                alias: 'Bobby Alias',
-                aliasReason: 'Divorce'
+            it('should return the ctx with the alias, alias reason and other reason removed when reason is other', (done) => {
+                ctx = {
+                    nameAsOnTheWill: 'Yes',
+                    alias: 'Bobby Alias',
+                    aliasReason: 'other',
+                    otherReason: 'Legally changed name'
+                };
+                errors = {};
+                [ctx, errors] = ApplicantNameAsOnWill.handlePost(ctx, errors);
+                expect(ctx).to.deep.equal({nameAsOnTheWill: 'Yes'});
+                done();
             });
-            done();
-        });
-
-        it('should return the ctx with the alias, alias reason and other reason included when reason is other', (done) => {
-            ctx = {
-                nameAsOnTheWill: 'No',
-                alias: 'Bobby Alias',
-                aliasReason: 'other',
-                otherReason: 'Legally changed name'
-            };
-            errors = {};
-            [ctx, errors] = ApplicantNameAsOnWill.handlePost(ctx, errors);
-            expect(ctx).to.deep.equal({
-                nameAsOnTheWill: 'No',
-                alias: 'Bobby Alias',
-                aliasReason: 'other',
-                otherReason: 'Legally changed name'
+        describe('nextStepOptions()', () => {
+            it('should return the correct next step options', (done) => {
+                const result = ApplicantNameAsOnWill.nextStepOptions();
+                expect(result).to.deep.equal({
+                    options: [{
+                        key: 'nameAsOnTheWill',
+                        value: content.optionNo,
+                        choice: 'hasAlias'
+                    }]
+                });
+                done();
             });
-            done();
         });
 
-        it('should return the ctx with the alias and alias reason removed', (done) => {
-            ctx = {
-                nameAsOnTheWill: 'Yes',
-                alias: 'Bobby Alias',
-                aliasReason: 'Divorce'
-            };
-            errors = {};
-            [ctx, errors] = ApplicantNameAsOnWill.handlePost(ctx, errors);
-            expect(ctx).to.deep.equal({nameAsOnTheWill: 'Yes'});
-            done();
-        });
+        describe('handlePost()', () => {
+            let ctx;
+            let errors;
 
-        it('should return the ctx with the alias, alias reason and other reason removed when reason is other', (done) => {
-            ctx = {
-                nameAsOnTheWill: 'Yes',
-                alias: 'Bobby Alias',
-                aliasReason: 'other',
-                otherReason: 'Legally changed name'
-            };
-            errors = {};
-            [ctx, errors] = ApplicantNameAsOnWill.handlePost(ctx, errors);
-            expect(ctx).to.deep.equal({nameAsOnTheWill: 'Yes'});
-            done();
+            it('should return the ctx with the alias and alias reason included', (done) => {
+                ctx = {
+                    nameAsOnTheWill: 'No',
+                    alias: 'Bobby Alias',
+                    aliasReason: 'Divorce'
+                };
+                errors = {};
+                [ctx, errors] = ApplicantNameAsOnWill.handlePost(ctx, errors);
+                expect(ctx).to.deep.equal({
+                    nameAsOnTheWill: 'No',
+                    alias: 'Bobby Alias',
+                    aliasReason: 'Divorce'
+                });
+                done();
+            });
+
+            it('should return the ctx with the alias, alias reason and other reason included when reason is other', (done) => {
+                ctx = {
+                    nameAsOnTheWill: 'No',
+                    alias: 'Bobby Alias',
+                    aliasReason: 'other',
+                    otherReason: 'Legally changed name'
+                };
+                errors = {};
+                [ctx, errors] = ApplicantNameAsOnWill.handlePost(ctx, errors);
+                expect(ctx).to.deep.equal({
+                    nameAsOnTheWill: 'No',
+                    alias: 'Bobby Alias',
+                    aliasReason: 'other',
+                    otherReason: 'Legally changed name'
+                });
+                done();
+            });
+
+            it('should return the ctx with the alias and alias reason removed', (done) => {
+                ctx = {
+                    nameAsOnTheWill: 'Yes',
+                    alias: 'Bobby Alias',
+                    aliasReason: 'Divorce'
+                };
+                errors = {};
+                [ctx, errors] = ApplicantNameAsOnWill.handlePost(ctx, errors);
+                expect(ctx).to.deep.equal({nameAsOnTheWill: 'Yes'});
+                done();
+            });
+
+            it('should return the ctx with the alias, alias reason and other reason removed when reason is other', (done) => {
+                ctx = {
+                    nameAsOnTheWill: 'Yes',
+                    alias: 'Bobby Alias',
+                    aliasReason: 'other',
+                    otherReason: 'Legally changed name'
+                };
+                errors = {};
+                [ctx, errors] = ApplicantNameAsOnWill.handlePost(ctx, errors);
+                expect(ctx).to.deep.equal({nameAsOnTheWill: 'Yes'});
+                done();
+            });
         });
     });
 });
