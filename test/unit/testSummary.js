629c0aad5ff16e201fa278633e0c0196ae62fdbf
Jake <jake.huggins@solirius.com>
2018-09-18 14:20:41 +0100

    PRO-3358: add unit test for Summary handleGet

---
+++ test/unit/testSummary.js
@@ -0,0 +1,38 @@
+const initSteps = require('app/core/initSteps');
+const assert = require('chai').assert;
+const sinon = require('sinon');
+const when = require('when');
+const services = require('app/components/services');
+const co = require('co');
+
+describe('Summary', () => {
+    const steps = initSteps([__dirname + '/../../app/steps/action/', __dirname + '/../../app/steps/ui']);
+
+    describe('handleGet()', () => {
+
+        let validateFormDataStub;
+
+        beforeEach(() => {
+            validateFormDataStub = sinon.stub(services, 'validateFormData');
+        });
+
+        afterEach(() => {
+            validateFormDataStub.restore();
+        });
+
+        it('ctx.executorsWithOtherNames returns array of execs with other names', (done) => {
+            const expectedResponse = ['Prince', 'Cher'];
+            validateFormDataStub.returns(when(expectedResponse));
+
+            let ctx = {session: {form: {}}};
+            const formdata = {executors: {list: [{fullName: 'Prince', hasOtherName: true}, {fullName: 'Cher', hasOtherName: true}]}};
+            const Summary = steps.Summary;
+
+            co(function* () {
+                [ctx] = yield Summary.handleGet(ctx, formdata);
+                assert.deepEqual(ctx.executorsWithOtherNames, expectedResponse);
+                done();
+            });
+        });
+    });
+});
