1cb976061e6627529f31dac6cf594db10df33f92
shepda <david@x-designs.co.uk>
2018-09-19 16:00:57 +0100

    PRO-3780: Use feature toggle key instead of the value

--- app/steps/ui/applicant/aliasreason/index.js
+++ app/steps/ui/applicant/aliasreason/index.js
@@ -2,7 +2,6 @@
 
 const ValidationStep = require('app/core/steps/ValidationStep');
 const FeatureToggle = require('app/utils/FeatureToggle');
-const config = require('app/config');
 
 class ApplicantAliasReason extends ValidationStep {
 
@@ -11,7 +10,7 @@ class ApplicantAliasReason extends ValidationStep {
     }
 
     isComplete(ctx, formdata, featureToggles) {
-        const isEnabled = FeatureToggle.isEnabled(featureToggles, config.featureToggles.main_applicant_alias);
+        const isEnabled = FeatureToggle.isEnabled(featureToggles, 'main_applicant_alias');
         return [isEnabled ? this.validate()[0] : true, 'noProgress'];
     }
 }
