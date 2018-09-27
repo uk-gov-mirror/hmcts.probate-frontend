3d22de95c411ad8fa317804bcc1ee8cf114609b6
Luca Del Buono <LucaDelBuono@users.noreply.github.com>
2018-09-27 11:18:26 +0100

    PRO-1926: Fix bug with Deceased Address not being saved due to introduction of feature toggle

--- app/steps/ui/deceased/address/index.js
+++ app/steps/ui/deceased/address/index.js
@@ -1,18 +1,11 @@
 'use strict';
 
 const AddressStep = require('app/core/steps/AddressStep');
-const FeatureToggle = require('app/utils/FeatureToggle');
 
 module.exports = class DeceasedAddress extends AddressStep {
 
     static getUrl() {
         return '/deceased-address';
-    }
-
-    handlePost(ctx, errors, formdata, session, hostname, featureToggles) {
-        ctx.isToggleEnabled = FeatureToggle.isEnabled(featureToggles, 'screening_questions');
-
-        return [ctx, errors];
     }
 
     nextStepOptions() {
@@ -21,11 +14,5 @@ module.exports = class DeceasedAddress extends AddressStep {
                 {key: 'isToggleEnabled', value: true, choice: 'toggleOn'}
             ]
         };
-    }
-
-    action(ctx, formdata) {
-        super.action(ctx, formdata);
-        delete ctx.isToggleEnabled;
-        return [ctx, formdata];
     }
 };
