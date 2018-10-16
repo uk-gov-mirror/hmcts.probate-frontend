cbce99062062060fb282cc714e4e73ab48cb8649
shepda <david@x-designs.co.uk>
2018-10-16 14:51:52 +0100

    PRO-3749: Add DocumentUpload class for methods used in the document upload routes
    Also unit tests

--- app/utils/DocumentUpload.js
+++ app/utils/DocumentUpload.js
@@ -1,30 +1,47 @@
 'use strict';
 
+<<<<<<< HEAD
 const fileType = require('file-type');
 const config = require('app/config').documentUpload;
 const content = require('app/resources/en/translation/common');
 
 class DocumentUpload {
     initDocuments(formdata) {
+=======
+class DocumentUpload {
+    static initDocuments(formdata) {
+>>>>>>> PRO-3749: Add DocumentUpload class for methods used in the document upload routes
         if (!formdata.documents) {
             formdata.documents = {};
         }
         return formdata;
     }
 
+<<<<<<< HEAD
     addDocument(filename, url, uploads = []) {
         if (filename && url) {
             uploads.push({filename, url});
+=======
+    static addDocument(uploadedFile, uploads = []) {
+        if (uploadedFile) {
+            uploads.push(uploadedFile);
+>>>>>>> PRO-3749: Add DocumentUpload class for methods used in the document upload routes
         }
         return uploads;
     }
 
+<<<<<<< HEAD
     removeDocument(index, uploads = []) {
         if (!isNaN(index) && uploads.length > 0) {
+=======
+    static removeDocument(index, uploads) {
+        if (!isNaN(index)) {
+>>>>>>> PRO-3749: Add DocumentUpload class for methods used in the document upload routes
             uploads.splice(index, 1);
         }
         return uploads;
     }
+<<<<<<< HEAD
 
     findDocumentId(url = '') {
         return url.split('/').reduce((acc, val) => {
@@ -89,6 +106,8 @@ class DocumentUpload {
             nonJs: errorKey
         };
     }
+=======
+>>>>>>> PRO-3749: Add DocumentUpload class for methods used in the document upload routes
 }
 
 module.exports = DocumentUpload;
