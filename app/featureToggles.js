da7289fae6448c31cb086f8b1eb5184790983213
Tom Elliott <tomxelliott@hotmail.co.uk>
2018-10-18 13:58:27 +0100

    PRO-3965: Add Tasklist route to Feature Toggles

--- app/featureToggles.js
+++ app/featureToggles.js
@@ -7,6 +7,7 @@ const featureToggle = new FeatureToggle();
 router.get('/applicant-alias', (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'main_applicant_alias', featureToggle.togglePage, 'applicant-phone'));
 router.get('/applicant-alias-reason', (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'main_applicant_alias', featureToggle.togglePage, 'applicant-phone'));
 router.get('/summary/*', (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'main_applicant_alias', featureToggle.toggleFeature));
+router.get('/tasklist', (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'main_applicant_alias', featureToggle.toggleFeature));
 
 router.get('/new-start-eligibility', (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'screening_questions', featureToggle.togglePage, 'start-eligibility'));
 router.get('/new-will-left', (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'screening_questions', featureToggle.togglePage, 'will-left'));
