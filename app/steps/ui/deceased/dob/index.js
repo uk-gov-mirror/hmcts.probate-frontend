8c1fed0d38f79e8c591d1f50df80470edac511cc
Luca Del Buono <LucaDelBuono@users.noreply.github.com>
2018-10-22 12:37:48 +0100

    PRO-3799: Swap DoB and DoD validation as order of questions changes with new flow

--- app/steps/ui/deceased/dob/index.js
+++ app/steps/ui/deceased/dob/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const DateStep = require('app/core/steps/DateStep');
+const FieldError = require('app/components/error');
 
 class DeceasedDob extends DateStep {
 
@@ -10,6 +11,19 @@ class DeceasedDob extends DateStep {
 
     dateName() {
         return 'dob';
+    }
+
+    handlePost(ctx, errors) {
+        const dob = new Date(`${ctx.dob_year}-${ctx.dob_month}-${ctx.dob_day}'`);
+
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+
+        if (today < dob) {
+            errors.push(FieldError('dob_date', 'dateInFuture', this.resourcePath, this.generateContent()));
+        }
+
+        return [ctx, errors];
     }
 }
 
--- app/steps/ui/deceased/dod/index.js
+++ app/steps/ui/deceased/dod/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const DateStep = require('app/core/steps/DateStep');
+const FieldError = require('app/components/error');
 
 class DeceasedDod extends DateStep {
 
@@ -10,6 +11,24 @@ class DeceasedDod extends DateStep {
 
     dateName() {
         return 'dod';
+    }
+
+    handlePost(ctx, errors, formdata, session) {
+        const dob = new Date(`${session.form.deceased.dob_year}-${session.form.deceased.dob_month}-${session.form.deceased.dob_day}'`);
+        dob.setHours(0, 0, 0, 0);
+
+        const dod = new Date(`${ctx.dod_year}-${ctx.dod_month}-${ctx.dod_day}'`);
+
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+
+        if (today < dod) {
+            errors.push(FieldError('dod_date', 'dateInFuture', this.resourcePath, this.generateContent()));
+        } else if (dod < dob) {
+            errors.push(FieldError('dod_date', 'dodBeforeDob', this.resourcePath, this.generateContent()));
+        }
+
+        return [ctx, errors];
     }
 }
 
