0d38bb1fe8d5660c26bf64814ee785db8e6eb58f
ruban72 <ruban72@hotmail.com>
2018-10-26 16:59:46 +0100

    PRO-3550: Legal Declaration pdf download

--- app/steps/ui/declaration/index.js
+++ app/steps/ui/declaration/index.js
@@ -190,6 +190,63 @@ class Declaration extends ValidationStep {
         delete ctx.invitesSent;
         return [ctx, formdata];
     }
+
+    renderCallBack(res, html) {
+        const formdata = res.req.session.form;
+        formdata.legalDeclaration = this.buildLegalDeclarationFromHtml(html);
+        res.send(html);
+    }
+
+    buildLegalDeclarationFromHtml(html) {
+        const dom = new JSDOM(html);
+        const $ = (require('jquery'))(dom.window);
+        const legalDeclaration = {};
+        legalDeclaration.headers = [];
+        legalDeclaration.sections = [];
+        const sections = $('.declaration-header, .declaration-subheader, .declaration-item, .list-bullet');
+
+        let section;
+        let declarationItem;
+        for (var sectElement of sections) {
+            const $element = $(sectElement);
+            if ($element.hasClass('declaration-header')) {
+                legalDeclaration.headers.push($element.html());
+            }
+            else if ($element.hasClass('declaration-subheader')) {
+
+                section = this.buildSection(section, $element);
+                legalDeclaration.sections.push(section);
+            }
+            else if ($element.hasClass('declaration-item')) {
+                declarationItem = {};
+                declarationItem.title = $element.html();
+                section.declarationItems.push(declarationItem);
+            }
+            else if ($element.hasClass('list-bullet')) {
+                declarationItem.values = [];
+                const children = $element.children();
+                if (children.length > 0) {
+                    for (var statement of children) {
+                        declarationItem.values.push(statement.textContent);
+                    }
+                }
+            }
+        }
+        return legalDeclaration;
+    }
+
+    buildSection(section, $element) {
+        section = {};
+        if($element.hasClass('heading-medium') || $element.hasClass('heading-large')){
+            section.headingType = 'large';
+        }
+        else{
+            section.headingType = 'small';
+        }
+        section.title = $element.html();
+        section.declarationItems = [];
+        return section;
+    }
 }
 
 module.exports = Declaration;
