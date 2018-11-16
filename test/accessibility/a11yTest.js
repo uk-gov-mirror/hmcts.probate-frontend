a03b545454fb455d171443fe08c81dae7230854f
Luca Del Buono <LucaDelBuono@users.noreply.github.com>
2018-11-16 15:08:36 +0000

    PRI-32: Exclude screening questions from A11Y test

--- test/accessibility/a11yTest.js
+++ test/accessibility/a11yTest.js
@@ -6,60 +6,19 @@ const a11y = require('test/util/a11y');
 const {expect} = require('chai');
 const app = require('app');
 const initSteps = require('app/core/initSteps');
-const {endsWith, has} = require('lodash');
+const {endsWith} = require('lodash');
 const sinon = require('sinon');
 const commonContent = require('app/resources/en/translation/common');
 const services = require('app/components/services');
-const stepsToExclude = ['Summary', 'TaskList', 'PinPage', 'PinSent', 'PinResend', 'AddressLookup', 'ExecutorAddress', 'ExecutorContactDetails', 'ExecutorName', 'ExecutorNotified', 'ExecutorNameAsOnWill', 'ExecutorApplying', 'DeleteExecutor', 'PaymentStatus', 'AddAlias', 'RemoveAlias', 'ExecutorRoles', 'ExecutorsWhenDied'];
-const stepsRequiringScreeningQuestionsToggleOn = [
-    'NewStartEligibility',
-    'NewDeathCertificate',
-    'NewDeceasedDomicile',
-    'NewIhtCompleted',
-    'NewWillLeft',
-    'NewWillOriginal',
-    'NewApplicantExecutor',
-    'NewMentalCapacity',
-    'NewStartApply'
+const stepsToExclude = [
+    'StartEligibility', 'ApplicantExecutor', 'DeceasedDomicile', 'MentalCapacity', 'IhtCompleted', 'WillLeft', 'WillOriginal', 'StartApply',
+    'NewStartEligibility', 'NewApplicantExecutor', 'NewDeceasedDomicile', 'NewMentalCapacity', 'NewIhtCompleted', 'NewWillLeft', 'NewWillOriginal', 'NewStartApply',
+    'Summary', 'TaskList', 'PinPage', 'PinSent', 'PinResend', 'AddressLookup', 'ExecutorAddress', 'ExecutorContactDetails', 'ExecutorName',
+    'ExecutorNotified', 'ExecutorNameAsOnWill', 'ExecutorApplying', 'DeleteExecutor', 'PaymentStatus', 'AddAlias', 'RemoveAlias', 'ExecutorRoles', 'ExecutorsWhenDied'
 ];
-const stepsRequiringEligibilityCookie = {
-    'NewDeathCertificate': '/new-death-certificate',
-    'NewDeceasedDomicile': '/new-deceased-domicile',
-    'NewIhtCompleted': '/new-iht-completed',
-    'NewWillLeft': '/new-will-left',
-    'NewWillOriginal': '/new-will-original',
-    'NewApplicantExecutor': '/new-applicant-executor',
-    'NewMentalCapacity': '/new-mental-capacity',
-    'NewStartApply': '/new-start-apply'
-};
 const steps = initSteps.steps;
 let checkAllAgreedStub;
-
-const nock = require('nock');
-const config = require('app/config');
-const featureToggleUrl = config.featureToggles.url;
-const screeningQuestionsfeatureTogglePath = `${config.featureToggles.path}/${config.featureToggles.screening_questions}`;
-const mainApplicantAliasfeatureTogglePath = `${config.featureToggles.path}/${config.featureToggles.main_applicant_alias}`;
-
-const Cookie = require('cookiejar');
-const cookieJar = Cookie.CookieJar();
-
-let cookieString;
-const cookie = {
-    name: '__eligibility',
-    content: {
-        nextStepUrl: '',
-        pages: [
-            stepsRequiringEligibilityCookie.NewDeathCertificate,
-            stepsRequiringEligibilityCookie.NewDeceasedDomicile,
-            stepsRequiringEligibilityCookie.NewIhtCompleted,
-            stepsRequiringEligibilityCookie.NewWillLeft,
-            stepsRequiringEligibilityCookie.NewWillOriginal,
-            stepsRequiringEligibilityCookie.NewApplicantExecutor,
-            stepsRequiringEligibilityCookie.NewMentalCapacity
-        ]
-    }
-};
+let featureToggleStub;
 
 Object.keys(steps)
     .filter(stepName => stepsToExclude.includes(stepName))
@@ -67,80 +26,56 @@ Object.keys(steps)
 
 for (const step in steps) {
     ((step) => {
+
         let results;
 
-        if (step.name === 'NewApplicantExecutor') {
-            describe(`Verify accessibility for the page ${step.name}`, () => {
-                let server = null;
-                let agent = null;
-                const title = `${step.content.title} - ${commonContent.serviceName}`
-                    .replace(/&lsquo;/g, '‘')
-                    .replace(/&rsquo;/g, '’')
-                    .replace(/\(/g, '\\(')
-                    .replace(/\)/g, '\\)');
+        describe(`Verify accessibility for the page ${step.name}`, () => {
+            let server = null;
+            let agent = null;
+            const title = `${step.content.title} - ${commonContent.serviceName}`
+                .replace(/&lsquo;/g, '‘')
+                .replace(/&rsquo;/g, '’')
+                .replace(/\(/g, '\\(')
+                .replace(/\)/g, '\\)');
 
-                before((done) => {
-                    server = app.init();
-                    agent = request.agent(server.app);
+            before((done) => {
+                checkAllAgreedStub = sinon.stub(services, 'checkAllAgreed')
+                    .returns(Promise.resolve('false'));
 
-                    checkAllAgreedStub = sinon.stub(services, 'checkAllAgreed')
-                        .returns(Promise.resolve('false'));
+                featureToggleStub = sinon.stub(services, 'featureToggle')
+                    .returns(Promise.resolve('true'));
 
-                    nock(featureToggleUrl)
-                        .get(mainApplicantAliasfeatureTogglePath)
-                        .reply(200, 'true');
-
-                    if (stepsRequiringScreeningQuestionsToggleOn.indexOf(step.name) !== -1) {
-                        console.log(`>>>>>>>>>>>>>>>>>> ${step.name}: screening questions toggle ON`);
-                        nock(featureToggleUrl)
-                            .get(screeningQuestionsfeatureTogglePath)
-                            .reply(200, 'true');
+                server = app.init();
+                agent = request.agent(server.app);
+                co(function* () {
+                    let urlSuffix = '';
+                    if (endsWith(agent.get(step.constructor.getUrl()), '*')) {
+                        urlSuffix = '/0';
                     }
-
-                    if (has(stepsRequiringEligibilityCookie, step.name)) {
-                        console.log(`>>>>>>>>>>>>>>>>>> ${step.name}: eligibility cookie ON`);
-                        cookie.content.nextStepUrl = stepsRequiringEligibilityCookie[step.name];
-                        cookieString = `${cookie.name}=${JSON.stringify(cookie.content)}`;
-
-                        cookieJar.setCookies(cookieString);
-
-                        const cookieValue = cookieJar.getCookie(cookie.name, Cookie.CookieAccessInfo.All);
-                        console.log('>>>>>>>>>>>>>>>>>> Cookie value:');
-                        console.log(JSON.parse(cookieValue.value));
-                    }
-
-                    co(function* () {
-                        let urlSuffix = '';
-                        if (endsWith(agent.get(step.constructor.getUrl()), '*')) {
-                            urlSuffix = '/0';
-                        }
-                        results = yield a11y(agent.get(step.constructor.getUrl()).url + urlSuffix, title);
-                    })
-                        .then(done, done)
-                        .catch((error) => {
-                            done(error);
-                        });
-                });
-
-                after(function (done) {
-                    checkAllAgreedStub.restore();
-                    nock.cleanAll();
-
-                    server.http.close();
-                    done();
-                });
-
-                it('should not generate any errors', () => {
-                    const errors = results.filter((res) => res.type === 'error');
-                    expect(errors.length).to.equal(0, JSON.stringify(errors, null, 2));
-                });
-
-                it('should not generate any warnings', () => {
-                    const warnings = results.filter((res) => res.type === 'warning');
-                    expect(warnings.length).to.equal(0, JSON.stringify(warnings, null, 2));
-                });
+                    results = yield a11y(agent.get(step.constructor.getUrl()).url + urlSuffix, title);
+                })
+                    .then(done, done)
+                    .catch((error) => {
+                        done(error);
+                    });
             });
-        }
 
+            after(function (done) {
+                checkAllAgreedStub.restore();
+                featureToggleStub.restore();
+                server.http.close();
+                done();
+            });
+
+            it('should not generate any errors', () => {
+                const errors = results.filter((res) => res.type === 'error');
+                expect(errors.length).to.equal(0, JSON.stringify(errors, null, 2));
+            });
+
+            it('should not generate any warnings', () => {
+                const warnings = results.filter((res) => res.type === 'warning');
+                expect(warnings.length).to.equal(0, JSON.stringify(warnings, null, 2));
+            });
+        });
     })(steps[step]);
 }
