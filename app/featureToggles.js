492c86ffd576ca7774ef4bf271c9fcde24a2d414
Luca Del Buono <LucaDelBuono@users.noreply.github.com>
2018-11-23 14:15:49 +0000

    Revert "PRI-7: Use config params for eligibility cookie redirect url in featureToggles.js"
    This reverts commit af324a43dbab06ad6801a471a7610c8c16a48e01.

--- app/featureToggles.js
+++ app/featureToggles.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const config = require('app/config');
 const router = require('express').Router();
 const FeatureToggle = require('app/utils/FeatureToggle');
 const featureToggle = new FeatureToggle();
@@ -11,9 +10,9 @@ router.get('/summary/*', (req, res, next) => featureToggle.callCheckToggle(req, 
 router.get('/tasklist', (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'main_applicant_alias', featureToggle.toggleFeature));
 
 router.get('/will-left', (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'intestacy_screening_questions', featureToggle.toggleFeature));
-router.get('/died-after-october-2014', (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'intestacy_screening_questions', featureToggle.togglePage, config.redis.eligibilityCookie.redirectUrl));
-router.get('/relationship-to-deceased', (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'intestacy_screening_questions', featureToggle.togglePage, config.redis.eligibilityCookie.redirectUrl));
-router.get('/other-applicants', (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'intestacy_screening_questions', featureToggle.togglePage, config.redis.eligibilityCookie.redirectUrl));
+router.get('/died-after-october-2014', (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'intestacy_screening_questions', featureToggle.togglePage, 'start-eligibility'));
+router.get('/relationship-to-deceased', (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'intestacy_screening_questions', featureToggle.togglePage, 'start-eligibility'));
+router.get('/other-applicants', (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'intestacy_screening_questions', featureToggle.togglePage, 'start-eligibility'));
 
 router.get('/executor-current-name/:index', (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'main_applicant_alias', featureToggle.toggleFeature));
 router.get('/executor-current-name-reason/:index', (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'main_applicant_alias', featureToggle.toggleFeature));
