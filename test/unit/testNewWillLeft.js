2d7526a620075308952b6899bf1f8fd10225dcd7
shepda <david@x-designs.co.uk>
2018-12-11 12:26:23 +0000

    PRI-113: Save the will left flag to the session so we can use it to populate the form data
    Also unit tests

--- test/unit/testNewWillLeft.js
+++ test/unit/testNewWillLeft.js
@@ -16,6 +16,28 @@ describe('NewWillLeft', () => {
         });
     });
 
+    describe('handlePost()', () => {
+        it('should remove session.form and set session.willLeft', (done) => {
+            const ctxToTest = {
+                left: 'Yes'
+            };
+            const errorsToTest = {};
+            const formdata = {};
+            const session = {
+                form: {}
+            };
+            const [ctx, errors] = NewWillLeft.handlePost(ctxToTest, errorsToTest, formdata, session);
+            expect(session).to.deep.equal({
+                willLeft: 'Yes'
+            });
+            expect(ctx).to.deep.equal({
+                left: 'Yes'
+            });
+            expect(errors).to.deep.equal({});
+            done();
+        });
+    });
+
     describe('nextStepUrl()', () => {
         it('should return url for the next step', (done) => {
             const req = {
