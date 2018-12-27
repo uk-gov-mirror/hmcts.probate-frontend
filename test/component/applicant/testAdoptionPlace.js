c85cf1a17f981dea37d1063b15018b3e04249fe6
Luca Del Buono <luca.delbuono@gmail.com>
2018-12-27 13:42:05 +0000

    PRI-71: Add unit and component tests

---
+++ test/component/applicant/testAdoptionPlace.js
@@ -0,0 +1,80 @@
+'use strict';
+
+const TestWrapper = require('test/util/TestWrapper');
+const SpouseNotApplyingReason = require('app/steps/ui/applicant/spousenotapplyingreason/index');
+const AnyOtherChildren = require('app/steps/ui/deceased/anyotherchildren/index');
+const StopPage = require('app/steps/ui/stoppage/index');
+const testHelpBlockContent = require('test/component/common/testHelpBlockContent.js');
+const content = require('app/resources/en/translation/applicant/adoptionplace');
+const contentMaritalStatus = require('app/resources/en/translation/deceased/maritalstatus');
+
+describe('adoption-place', () => {
+    let testWrapper;
+    const expectedNextUrlForSpouseNotApplyingReason = SpouseNotApplyingReason.getUrl();
+    const expectedNextUrlForAnyOtherChildren = AnyOtherChildren.getUrl();
+    const expectedNextUrlForStopPage = StopPage.getUrl('adoptionNotEnglandOrWales');
+
+    beforeEach(() => {
+        testWrapper = new TestWrapper('AdoptionPlace');
+    });
+
+    afterEach(() => {
+        testWrapper.destroy();
+    });
+
+    describe('Verify Content, Errors and Redirection', () => {
+        testHelpBlockContent.runTest('AdoptionPlace');
+
+        it('test content loaded on the page', (done) => {
+            testWrapper.testContent(done, [], {});
+        });
+
+        it('test errors message displayed for missing data', (done) => {
+            testWrapper.testErrors(done, {}, 'required', []);
+        });
+
+        it(`test it redirects to Spouse Not Applying Reason page if adoption took place in England or Wales and deceased was married: ${expectedNextUrlForSpouseNotApplyingReason}`, (done) => {
+            const sessionData = {
+                deceased: {
+                    maritalStatus: contentMaritalStatus.optionMarried
+                }
+            };
+
+            testWrapper.agent.post('/prepare-session/form')
+                .send(sessionData)
+                .end(() => {
+                    const data = {
+                        adoptionPlace: content.optionYes
+                    };
+
+                    testWrapper.testRedirect(done, data, expectedNextUrlForSpouseNotApplyingReason);
+                });
+        });
+
+        it(`test it redirects to Any Other Children page if adoption took place in England or Wales and deceased was not married: ${expectedNextUrlForAnyOtherChildren}`, (done) => {
+            const sessionData = {
+                deceased: {
+                    maritalStatus: contentMaritalStatus.optionDivorced
+                }
+            };
+
+            testWrapper.agent.post('/prepare-session/form')
+                .send(sessionData)
+                .end(() => {
+                    const data = {
+                        adoptionPlace: content.optionYes
+                    };
+
+                    testWrapper.testRedirect(done, data, expectedNextUrlForAnyOtherChildren);
+                });
+        });
+
+        it(`test it redirects to Stop page if adoption took place outside England or Wales: ${expectedNextUrlForStopPage}`, (done) => {
+            const data = {
+                adoptionPlace: content.optionNo
+            };
+
+            testWrapper.testRedirect(done, data, expectedNextUrlForStopPage);
+        });
+    });
+});
