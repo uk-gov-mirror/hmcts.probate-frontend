c3392e343ae68629bed9c3577ea0c32578c90f5e
shepda <david@x-designs.co.uk>
2019-01-10 14:25:54 +0000

    PRI-114: Update the unit tests after using the new services

---
+++ test/unit/testAuthoriseService.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const {expect} = require('chai');
+const sinon = require('sinon');
+const Authorise = require('app/services/Authorise');
+
+describe('AuthoriseService', () => {
+    describe('post()', () => {
+        it('should call log() and fetchText()', (done) => {
+            const endpoint = 'http://localhost';
+            const fetchOptions = {method: 'POST'};
+            const authorise = new Authorise(endpoint, 'abc123');
+            const logSpy = sinon.spy(authorise, 'log');
+            const fetchTextSpy = sinon.spy(authorise, 'fetchText');
+            const fetchOptionsStub = sinon.stub(authorise, 'fetchOptions').returns(fetchOptions);
+
+            authorise.post();
+
+            expect(authorise.log.calledOnce).to.equal(true);
+            expect(authorise.log.calledWith('Post authorise')).to.equal(true);
+            expect(authorise.fetchText.calledOnce).to.equal(true);
+            expect(authorise.fetchText.calledWith(endpoint, fetchOptions)).to.equal(true);
+
+            logSpy.restore();
+            fetchTextSpy.restore();
+            fetchOptionsStub.restore();
+            done();
+        });
+    });
+});
