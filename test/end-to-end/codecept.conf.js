e567bd5085fc8b22e74e8713c8cde5480107b15f
Doug Rice <Douglas.Rice@hmcts.net>
2019-01-25 16:34:10 +0000

    PRI-58: Test only surveyLink

--- test/end-to-end/codecept.conf.js
+++ test/end-to-end/codecept.conf.js
@@ -1,7 +1,7 @@
 const testConfig = require('test/config.js');
 
 exports.config = {
-    'tests': './paths/**/*.js',
+    'tests': './paths/**/surveyLink.js',
     'output': './output',
     'helpers': {
         'Puppeteer': {
