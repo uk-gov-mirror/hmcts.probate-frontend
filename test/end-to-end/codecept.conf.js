1c055a01c25a1f6c04b8b5f04d02e279a5278e7a
Doug Rice <Douglas.Rice@hmcts.net>
2019-01-30 16:39:46 +0000

    PRI-143: Amendments

--- test/end-to-end/codecept.conf.js
+++ test/end-to-end/codecept.conf.js
@@ -1,14 +1,14 @@
 const testConfig = require('test/config.js');
 
 exports.config = {
-    'tests': './paths/**/intestacyPath.js',
+    'tests': './paths/**/*.js',
     'output': './output',
     'helpers': {
         'Puppeteer': {
             'url': testConfig.TestE2EFrontendUrl || 'http://localhost:3000',
             'waitForTimeout': 60000,
             'getPageTimeout': 20000,
-            'show': true,
+            'show': false,
             'waitForNavigation': ['domcontentloaded', 'networkidle0'],
             'chrome': {
                 'ignoreHTTPSErrors': true,
