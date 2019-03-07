691c377ccb0b40363b4f9d2e48002cefa4f74410
Luca Del Buono <luca.delbuono@gmail.com>
2019-03-07 10:57:55 +0000

    PRO-4687: Place fees-related changes on Start Eligibiltity page behind fees_api toggle

---
+++ test/unit/screeners/testStartEligibility.js
@@ -0,0 +1,36 @@
+const initSteps = require('app/core/initSteps');
+const expect = require('chai').expect;
+const steps = initSteps([`${__dirname}/../../../app/steps/action/`, `${__dirname}/../../../app/steps/ui`]);
+const startEligibility = steps.StartEligibility;
+
+describe('StartEligibility', () => {
+    describe('getUrl()', () => {
+        it('should return the correct url', (done) => {
+            const url = startEligibility.constructor.getUrl();
+            expect(url).to.equal('/start-eligibility');
+            done();
+        });
+    });
+
+    describe('handleGet()', () => {
+        it('should return true when the fees api toggle is set', (done) => {
+            const ctxToTest = {};
+            const formdata = {};
+            const featureToggles = {
+                fees_api: true
+            };
+            const [ctx] = startEligibility.handleGet(ctxToTest, formdata, featureToggles);
+            expect(ctx.isFeesApiToggleEnabled).to.equal(true);
+            done();
+        });
+
+        it('should return false when the fees api toggle is not set', (done) => {
+            const ctxToTest = {};
+            const formdata = {};
+            const featureToggles = {};
+            const [ctx] = startEligibility.handleGet(ctxToTest, formdata, featureToggles);
+            expect(ctx.isFeesApiToggleEnabled).to.equal(false);
+            done();
+        });
+    });
+});
