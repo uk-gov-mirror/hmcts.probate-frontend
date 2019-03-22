6b5b058194ea6d683ddf6c6c0d7cd36863f8c251
ruban72 <ruban72@hotmail.com>
2019-03-22 09:20:29 +0000

    PRO-4814: refactored Pact tests to allow multiple executions

--- test/contract/pact/IntestacyFormDataClient.spec.js
+++ test/contract/pact/IntestacyFormDataClient.spec.js
@@ -7,23 +7,29 @@ const {Pact} = require('@pact-foundation/pact');
 const chaiAsPromised = require('chai-as-promised');
 const IntestacyFormData = require('app/services/IntestacyFormData');
 const config = require('app/config');
-
+const getPort = require('get-port');
 const expect = chai.expect;
-const MOCK_SERVER_PORT = 2204;
 
 chai.use(chaiAsPromised);
 
 describe('Pact IntestacyFormData', () => {
-    // (1) Create the Pact object to represent your provider
-    const provider = new Pact({
-        consumer: 'probate_frontend',
-        provider: 'probate_orchestrator_service_intestacy_forms',
-        port: MOCK_SERVER_PORT,
-        log: path.resolve(process.cwd(), 'logs', 'pact.log'),
-        dir: path.resolve(process.cwd(), config.services.pact.pactDirectory),
-        logLevel: 'INFO',
-        spec: 2
+
+    let MOCK_SERVER_PORT;
+    let provider;
+    getPort().then(portNumber => {
+        MOCK_SERVER_PORT = portNumber;
+        // (1) Create the Pact object to represent your provider
+        provider = new Pact({
+            consumer: 'probate_frontend',
+            provider: 'probate_orchestrator_service_intestacy_forms',
+            port: MOCK_SERVER_PORT,
+            log: path.resolve(process.cwd(), 'logs', 'pactIntestacyFormDataClient.log'),
+            dir: path.resolve(process.cwd(), config.services.pact.pactDirectory),
+            logLevel: 'INFO',
+            spec: 2
+        });
     });
+
 
     const ctx = {
         sessionID: 'someSessionId',
@@ -84,54 +90,64 @@ describe('Pact IntestacyFormData', () => {
         return expectedJSON;
     }
 
-    context('when intestacy formdata is posted', () => {
+    // Setup a Mock Server before unit tests run.
+    // This server acts as a Test Double for the real Provider API.
+    // We then call addInteraction() for each test to configure the Mock Service
+    // to act like the Provider
+    // It also sets up expectations for what requests are to come, and will fail
+    // if the calls are not seen.
+    before(() =>
+        provider.setup()
+    )
+
+    // After each individual test (one or more interactions)
+    // we validate that the correct request came through.
+    // This ensures what we _expect_ from the provider, is actually
+    // what we've asked for (and is what gets captured in the contract)
+    afterEach(() => provider.verify())
+
+    describe('when intestacy formdata is posted', () => {
         describe('and is required to be persisted', () => {
-            before(done => {
+            before(() => {
                 // (2) Start the mock server
-                provider
-                    .setup()
-                    // (3) add interactions to the Mock Server, as many as required
-                    .then(() => {
-                        return provider.addInteraction({
-                            // The 'state' field specifies a 'Provider State'
-                            state: 'probate_orchestrator_service persists intestacy formdata with success',
-                            uponReceiving: 'a request to POST intestacy formdata',
-                            withRequest: {
-                                method: 'POST',
-                                path: '/forms/someemailaddress@host.com',
-                                headers: {
-                                    'Content-Type': 'application/json',
-                                    'Session-Id': ctx.sessionID,
-                                    'Authorization': ctx.authToken,
-                                    'ServiceAuthorization': ctx.serviceAuthorization
-                                },
-                                body: getRequestBody()
-                            },
-                            willRespondWith: {
-                                status: 200,
-                                headers: {'Content-Type': 'application/json'},
-                                body: getExpectedResponseBody()
-                            }
-                        });
-                    })
-                    .then(() => done());
-            });
+                provider.addInteraction({
+                    // The 'state' field specifies a 'Provider State'
+                    state: 'probate_orchestrator_service persists intestacy formdata with success',
+                    uponReceiving: 'a request to POST intestacy formdata',
+                    withRequest: {
+                        method: 'POST',
+                        path: '/forms/someemailaddress@host.com',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Session-Id': ctx.sessionID,
+                            'Authorization': ctx.authToken,
+                            'ServiceAuthorization': ctx.serviceAuthorization
+                        },
+                        body: getRequestBody()
+                    },
+                    willRespondWith: {
+                        status: 200,
+                        headers: {'Content-Type': 'application/json'},
+                        body: getExpectedResponseBody()
+                    }
+                })
+            })
 
             // (4) write your test(s)
             // Verify service client works as expected
             it('successfully validated form data', (done) => {
-                const formDataClient = new IntestacyFormData('http://localhost:2204', 'someSessionId');
+                const formDataClient = new IntestacyFormData('http://localhost:' + MOCK_SERVER_PORT, 'someSessionId');
                 const verificationPromise = formDataClient.post('someemailaddress@host.com', FORM_DATA_BODY_PAYLOAD, ctx);
                 expect(verificationPromise).to.eventually.eql(getExpectedResponseBody()).notify(done);
             });
 
-            // (6) write the pact file for this consumer-provider pair,
-            // and shutdown the associated mock server.
-            // You should do this only _once_ per Provider you are testing.
-            after(() => {
-                return provider.finalize();
-            });
+
         });
     });
-
+    // (6) write the pact file for this consumer-provider pair,
+    // and shutdown the associated mock server.
+    // You should do this only _once_ per Provider you are testing.
+    after(() => {
+        return provider.finalize();
+    });
 });
