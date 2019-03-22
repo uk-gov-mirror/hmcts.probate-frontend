3f8a63747a7fa5c14d82840c51b6f1ec529ebb49
Dhruv Patel <dhruv.patel20@gmail.com>
2019-03-22 14:18:14 +0000

    PRO-4769: Allow FE to save a draft via orchestrator.

--- app/services/ProbateFormData.js
+++ app/services/ProbateFormData.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const dateformat = require('dateformat');
 const FormData = require('./FormData');
 
 class ProbateFormData extends FormData {
@@ -17,11 +18,35 @@ class ProbateFormData extends FormData {
         const path = this.replaceEmailInPath(this.config.services.orchestrator.paths.forms, id);
         const logMessage = 'Post probate form data';
         const url = this.endpoint + path;
-        return super.post(data, logMessage, url, ctx);
+        console.log(JSON.stringify(data));
+        return super.post(this.formatForPost(data), logMessage, url, ctx);
     }
 
     getFormType() {
         return 'PA';
+    }
+
+    formatForPost(formdata){
+        formdata.deceased.dod_date = dateformat(formdata.deceased.dod_date, 'yyyy-mm-dd');
+        formdata.deceased.dob_date = dateformat(formdata.deceased.dob_date, 'yyyy-mm-dd');
+        console.log("Formatted POST formdata: " + JSON.stringify(formdata));
+        return formdata;
+    }
+
+    formatForGet(formdata){
+        const dod =  new Date(formdata.deceased.dod_date);
+        formdata.deceased.dod_date = dod;
+        formdata.deceased.dod_day = dod.getDate();
+        formdata.deceased.dod_month = dod.getMonth() + 1;
+        formdata.deceased.dod_year = dod.getFullYear();
+
+        const dob =  new Date(formdata.deceased.dob_date);
+        formdata.deceased.dob_date = dob;
+        formdata.deceased.dob_day = dob.getDate();
+        formdata.deceased.dob_month = dob.getMonth() + 1;
+        formdata.deceased.dob_year = dob.getFullYear();
+        console.log("Formatted GET formdata: " + JSON.stringify(formdata));
+        return formdata;
     }
 }
 
