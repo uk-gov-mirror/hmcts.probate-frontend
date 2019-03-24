9bac6d160dfe396e39381c1b0a3a3e2a74cf7993
Dhruv Patel <dhruv.patel20@gmail.com>
2019-03-24 00:36:48 +0000

    PRO-4769: Removed any changes to form data, from get and post.

--- app/services/ProbateFormData.js
+++ app/services/ProbateFormData.js
@@ -19,34 +19,11 @@ class ProbateFormData extends FormData {
         const logMessage = 'Post probate form data';
         const url = this.endpoint + path;
         console.log(JSON.stringify(data));
-        return super.post(this.formatForPost(data), logMessage, url, ctx);
+        return super.post(data, logMessage, url, ctx);
     }
 
     getFormType() {
         return 'PA';
-    }
-
-    formatForPost(formdata){
-        formdata.deceased.dod_date = dateformat(formdata.deceased.dod_date, 'yyyy-mm-dd');
-        formdata.deceased.dob_date = dateformat(formdata.deceased.dob_date, 'yyyy-mm-dd');
-        console.log("Formatted POST formdata: " + JSON.stringify(formdata));
-        return formdata;
-    }
-
-    formatForGet(formdata){
-        const dod =  new Date(formdata.deceased.dod_date);
-        formdata.deceased.dod_date = dod;
-        formdata.deceased.dod_day = dod.getDate();
-        formdata.deceased.dod_month = dod.getMonth() + 1;
-        formdata.deceased.dod_year = dod.getFullYear();
-
-        const dob =  new Date(formdata.deceased.dob_date);
-        formdata.deceased.dob_date = dob;
-        formdata.deceased.dob_day = dob.getDate();
-        formdata.deceased.dob_month = dob.getMonth() + 1;
-        formdata.deceased.dob_year = dob.getFullYear();
-        console.log("Formatted GET formdata: " + JSON.stringify(formdata));
-        return formdata;
     }
 }
 
