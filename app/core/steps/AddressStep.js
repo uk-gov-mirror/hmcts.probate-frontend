127ff11fdf31741115c2ffba79d8c1f10b2b9e84
Wivenhoe-HMCTS <jason.smith@HMCTS.NET>
2019-04-17 10:02:38 +0100

    PRO-3173: initial changes

--- app/core/steps/AddressStep.js
+++ app/core/steps/AddressStep.js
@@ -17,12 +17,27 @@ class AddressStep extends ValidationStep {
     }
 
     handlePost(ctx, errors) {
-        ctx.address = ctx.postcodeAddress || ctx.freeTextAddress;
+        ctx.address = {
+            addressLine1: ctx.addressLine1,
+            addressLine2: ctx.addressLine2,
+            addressLine3: ctx.addressLine3,
+            postTown: ctx.townOrCity,
+            county: ctx.county,
+            postCode: ctx.newPostCode,
+            country: ctx.country,
+        };
         ctx.postcode = ctx.postcode ? ctx.postcode.toUpperCase() : ctx.postcode;
         if (!ctx.postcodeAddress) {
             delete ctx.addresses;
         }
         delete ctx.referrer;
+        delete ctx.addressLine1;
+        delete ctx.addressLine2;
+        delete ctx.addressLine3;
+        delete ctx.townOrCity;
+        delete ctx.county;
+        delete ctx.newPostCode;
+        delete ctx.country;
         return [ctx, errors];
     }
 }
