d1d752a8e17585e4cb3ca53cb086727cae1d22bf
ruban72 <ruban72@hotmail.com>
2019-04-18 11:26:37 +0100

    PRO-4814: Fixed pact tests

--- app/services/ProbateSubmitData.js
+++ app/services/ProbateSubmitData.js
@@ -6,6 +6,10 @@ class ProbateSubmitData extends SubmitData {
     getFormType() {
         return 'PA';
     }
+
+    getApplicantEmail(data) {
+        return data.applicantEmail;
+    }
 }
 
 module.exports = ProbateSubmitData;
