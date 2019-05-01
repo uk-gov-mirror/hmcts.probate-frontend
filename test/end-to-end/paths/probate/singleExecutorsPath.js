72981b45ebc9e98a6899c3f84b87cc2a0ec5110f
Wivenhoe-HMCTS <jason.smith@HMCTS.NET>
2019-05-01 11:23:34 +0100

    PRO-3173: revised e2e files

--- test/end-to-end/paths/probate/singleExecutorsPath.js
+++ test/end-to-end/paths/probate/singleExecutorsPath.js
@@ -59,7 +59,7 @@ Scenario(TestConfigurator.idamInUseText('Single Executor Journey'), function (I)
     I.enterDeceasedDateOfBirth('01', '01', '1950');
     I.enterDeceasedDateOfDeath('01', '01', '2017');
     I.enterDeceasedAddress();
-    //I.selectDocumentsToUpload();
+    I.selectDocumentsToUpload();
     I.selectInheritanceMethodPaper();
 
     if (TestConfigurator.getUseGovPay() === 'true') {
--- test/service-stubs/business.js
+++ test/service-stubs/business.js
@@ -5,6 +5,7 @@
 const express = require('express');
 const app = express();
 const router = require('express').Router();
+const fs = require('fs');
 
 router.get('invites/allAgreed/:id', (req, res) => {
     res.status(200);
@@ -46,6 +47,29 @@ router.delete('/document/delete/:index', (req, res) => {
     res.status(204).send(true);
 });
 
+router.post('/businessDocument/generateCheckAnswersSummaryPDF', (req, res) => {
+    fs.readFile('test/data/generic.pdf', function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.contentType('application/pdf');
+            res.status(200);
+            res.send(data);
+        }
+    });
+});
+
+router.post('/businessDocument/generateLegalDeclarationPDF', (req, res) => {
+    fs.readFile('test/data/generic.pdf', function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.contentType('application/pdf');
+            res.status(200);
+            res.send(data);
+        }
+    });
+});
 app.use(router);
 
 console.log('Listening on: 8080');
