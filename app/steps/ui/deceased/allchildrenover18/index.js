17c952496284f1fd06b9252be3ebd7daa59c121a
Luca Del Buono <luca.delbuono@gmail.com>
2019-05-21 17:21:18 +0100

    PRI-73: Remove logic for page in next Intestacy step

--- app/steps/ui/deceased/allchildrenover18/index.js
+++ app/steps/ui/deceased/allchildrenover18/index.js
@@ -1,31 +1,11 @@
 'use strict';
+
 const ValidationStep = require('app/core/steps/ValidationStep');
-const FormatName = require('app/utils/FormatName');
-const content = require('app/resources/en/translation/deceased/anychildren');
 
 class AllChildrenOver18 extends ValidationStep {
 
     static getUrl() {
         return '/all-children-over-18';
-    }
-
-    getContextData(req) {
-        const ctx = super.getContextData(req);
-        const formdata = req.session.form;
-        ctx.deceasedName = FormatName.format(formdata.deceased);
-        return ctx;
-    }
-
-    nextStepUrl(ctx) {
-        return this.next(ctx).constructor.getUrl('childrenUnder18');
-    }
-
-    nextStepOptions() {
-        return {
-            options: [
-                {key: 'allChildrenOver18', value: content.optionYes, choice: 'allChildrenOver18'}
-            ]
-        };
     }
 }
 
