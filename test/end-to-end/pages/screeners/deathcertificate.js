8e869e55fffd5c3208420ac687fa9cc1c54db403
Doug Rice <Douglas.Rice@hmcts.net>
2019-06-05 18:08:35 +0100

    PRO-5559: Test

--- test/end-to-end/pages/screeners/deathcertificate.js
+++ test/end-to-end/pages/screeners/deathcertificate.js
@@ -6,11 +6,9 @@ const pageUnderTest = require('app/steps/ui/screeners/deathcertificate');
 module.exports = function (answer) {
     const I = this;
 
-    I.amOnLoadedPage(pageUnderTest.getUrl());
+    I.seeCurrentUrlEquals(pageUnderTest.getUrl());
     I.seeCurrentUrlEquals(pageUnderTest.getUrl());
     I.click(`#deathCertificate-option${answer}`);
 
-    //I.click(commonContent.continue);
-    //I.waitForNavigationToComplete(`input[value="${commonContent.continue}"]`);
     I.waitForNavigationToComplete(commonContent.continue);
 };
