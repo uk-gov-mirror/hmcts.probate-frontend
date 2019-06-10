a3cd10b3d9ea16b8623aac84a2992b6f870797af
Doug Rice <Douglas.Rice@hmcts.net>
2019-06-10 17:30:30 +0100

    PRO-5559: Test

--- test/crossbrowser/supportedBrowsers.js
+++ test/crossbrowser/supportedBrowsers.js
@@ -1,60 +1,58 @@
 const supportedBrowsers = {
-    // microsoftIE11: {
-    //     ie11: {
-    //         browserName: 'internet explorer',
-    //         name: 'Probate_IE11_Win10',
-    //         platform: 'Windows 10',
-    //         version: '11.285'
-    //     }
-    // }
-    // ,
-    // microsoftEdge: {
-    //     edge: {
-    //         browserName: 'MicrosoftEdge',
-    //         name: 'Probate_EDGE_Win10',
-    //         platform: 'Windows 10',
-    //         version: '18.17763'
-    //     }
-    // }
-    //,
-    // chrome: {
-    // chrome_win_latest: {
-    //     browserName: 'chrome',
-    //     name: 'Probate_WIN_CHROME_LATEST',
-    //     platform: 'Windows 10',
-    //     version: 'latest'
-    // }//,
-    // chrome_mac_latest: {
-    //     browserName: 'chrome',
-    //     name: 'Probate_MAC_CHROME_LATEST',
-    //     platform: 'macOS 10.13',
-    //     version: 'latest'
-    // }
-    // }//,
-    // firefox: {
-    // firefox_win_latest: {
-    //     browserName: 'firefox',
-    //     name: 'Probate_WIN_FIREFOX_LATEST',
-    //     platform: 'Windows 10',
-    //     version: 'latest'
-    //     // } //,
-    //     firefox_mac_latest: {
-    //         browserName: 'firefox',
-    //         name: 'Probate_MAC_FIREFOX_LATEST',
-    //         platform: 'macOS 10.14',
-    //         version: 'latest'
-    //     }
-    // }
-    // ,
-    safari: {
-        safari12: {
-            browserName: 'safari',
-            name: 'Probate_SAFARI_12',
+    microsoftIE11: {
+        ie11: {
+            browserName: 'internet explorer',
+            name: 'Probate_IE11_Win10',
+            platform: 'Windows 10',
+            version: '11.285'
+        }
+    },
+    microsoftEdge: {
+        edge: {
+            browserName: 'MicrosoftEdge',
+            name: 'Probate_EDGE_Win10',
+            platform: 'Windows 10',
+            version: '18.17763'
+        }
+    },
+    chrome: {
+        chrome_win_latest: {
+            browserName: 'chrome',
+            name: 'Probate_WIN_CHROME_LATEST',
+            platform: 'Windows 10',
+            version: 'latest'
+        },
+        chrome_mac_latest: {
+            browserName: 'chrome',
+            name: 'Probate_MAC_CHROME_LATEST',
+            platform: 'macOS 10.13',
+            version: 'latest'
+        }
+    },
+    firefox: {
+        firefox_win_latest: {
+            browserName: 'firefox',
+            name: 'Probate_WIN_FIREFOX_LATEST',
+            platform: 'Windows 10',
+            version: 'latest'
+        },
+        firefox_mac_latest: {
+            browserName: 'firefox',
+            name: 'Probate_MAC_FIREFOX_LATEST',
             platform: 'macOS 10.14',
-            version: '12.0',
-            avoidProxy: true
+            version: 'latest'
         }
     }
+    // ,
+    // safari: {
+    //     safari12: {
+    //         browserName: 'safari',
+    //         name: 'Probate_SAFARI_12',
+    //         platform: 'macOS 10.14',
+    //         version: '12.0',
+    //         avoidProxy: true
+    //     }
+    // }
 };
 
 module.exports = supportedBrowsers;
