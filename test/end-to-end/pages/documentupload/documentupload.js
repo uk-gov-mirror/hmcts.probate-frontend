b19ec32e605dbbb907595df8e206df591c1a78c3
Doug Rice <Douglas.Rice@hmcts.net>
2019-06-10 12:42:00 +0100

    PRO-5559: test

--- test/end-to-end/pages/documentupload/documentupload.js
+++ test/end-to-end/pages/documentupload/documentupload.js
@@ -9,11 +9,12 @@ module.exports = function () {
 
     I.seeCurrentUrlEquals(pageUnderTest.getUrl());
 
-    if (I.seeElementInDOM('.dz-hidden-input')) {
-        I.attachFile('.dz-hidden-input', '/uploadDocuments/test_file_for_document_upload.png');
-        I.waitForEnabled('#button', testConfig.TestDocumentToUpload);
-        I.waitForNavigationToComplete(commonContent.continue);
-    } else {
-        I.waitForNavigationToComplete('#input');
-    }
+    // if (I.seeElementInDOM('.dz-hidden-input')) {
+    I.waitForElement('.dz-hidden-input', 60);
+    I.attachFile('.dz-hidden-input', '/uploadDocuments/test_file_for_document_upload.png');
+    I.waitForEnabled('#button', testConfig.TestDocumentToUpload);
+    I.waitForNavigationToComplete(commonContent.continue);
+    //} else {
+    //     I.waitForNavigationToComplete('#input');
+    // }
 };
