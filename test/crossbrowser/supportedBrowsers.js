7c208dd91a179024ff4cbcbb281beed2238cb8a5
Doug Rice <Douglas.Rice@hmcts.net>
2019-06-11 11:13:01 +0100

    PRO-5559: Test

--- test/crossbrowser/supportedBrowsers.js
+++ test/crossbrowser/supportedBrowsers.js
@@ -6,43 +6,43 @@ const supportedBrowsers = {
             platform: 'Windows 10',
             version: '11.285'
         }
-    },
-    microsoftEdge: {
-        edge: {
-            browserName: 'MicrosoftEdge',
-            name: 'Probate_EDGE_Win10',
-            platform: 'Windows 10',
-            version: '18.17763'
-        }
-    },
-    chrome: {
-        chrome_win_latest: {
-            browserName: 'chrome',
-            name: 'Probate_WIN_CHROME_LATEST',
-            platform: 'Windows 10',
-            version: 'latest'
-        },
-        chrome_mac_latest: {
-            browserName: 'chrome',
-            name: 'Probate_MAC_CHROME_LATEST',
-            platform: 'macOS 10.13',
-            version: 'latest'
-        }
-    },
-    firefox: {
-        firefox_win_latest: {
-            browserName: 'firefox',
-            name: 'Probate_WIN_FIREFOX_LATEST',
-            platform: 'Windows 10',
-            version: 'latest'
-        },
-        firefox_mac_latest: {
-            browserName: 'firefox',
-            name: 'Probate_MAC_FIREFOX_LATEST',
-            platform: 'macOS 10.14',
-            version: 'latest'
-        }
-    }
+    }//,
+    // microsoftEdge: {
+    //     edge: {
+    //         browserName: 'MicrosoftEdge',
+    //         name: 'Probate_EDGE_Win10',
+    //         platform: 'Windows 10',
+    //         version: '18.17763'
+    //     }
+    // },
+    // chrome: {
+    //     chrome_win_latest: {
+    //         browserName: 'chrome',
+    //         name: 'Probate_WIN_CHROME_LATEST',
+    //         platform: 'Windows 10',
+    //         version: 'latest'
+    //     },
+    //     chrome_mac_latest: {
+    //         browserName: 'chrome',
+    //         name: 'Probate_MAC_CHROME_LATEST',
+    //         platform: 'macOS 10.13',
+    //         version: 'latest'
+    //     }
+    // },
+    // firefox: {
+    //     firefox_win_latest: {
+    //         browserName: 'firefox',
+    //         name: 'Probate_WIN_FIREFOX_LATEST',
+    //         platform: 'Windows 10',
+    //         version: 'latest'
+    //     },
+    //     firefox_mac_latest: {
+    //         browserName: 'firefox',
+    //         name: 'Probate_MAC_FIREFOX_LATEST',
+    //         platform: 'macOS 10.14',
+    //         version: 'latest'
+    //     }
+    //}
     // ,
     // safari: {
     //     safari12: {
