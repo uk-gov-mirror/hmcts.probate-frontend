19a8fba4b535f8a200aef00e033d02b45916dc77
Doug Rice <Douglas.Rice@hmcts.net>
2019-06-12 18:29:33 +0100

    PRO-5559: Test

--- test/crossbrowser/supportedBrowsers.js
+++ test/crossbrowser/supportedBrowsers.js
@@ -2,45 +2,45 @@ const supportedBrowsers = {
     microsoftIE11: {
         ie11: {
             browserName: 'internet explorer',
-            name: 'Probate_IE11_Win10',
+            name: 'IE11',
             platform: 'Windows 10',
             version: '11.285'
+        }
+    },
+    microsoftEdge: {
+        edge: {
+            browserName: 'MicrosoftEdge',
+            name: 'Edge_Win10',
+            platform: 'Windows 10',
+            version: '18.17763'
+        }
+    },
+    chrome: {
+        chrome_win_latest: {
+            browserName: 'chrome',
+            name: 'DIV_WIN_CHROME_LATEST',
+            platform: 'Windows 10',
+            version: 'latest'
         },
-        microsoftEdge: {
-            edge: {
-                browserName: 'MicrosoftEdge',
-                name: 'Probate_EDGE_Win10',
-                platform: 'Windows 10',
-                version: '18.17763'
-            }
+        chrome_mac_latest: {
+            browserName: 'chrome',
+            name: 'MAC_CHROME_LATEST',
+            platform: 'macOS 10.13',
+            version: 'latest'
+        }
+    },
+    firefox: {
+        firefox_win_latest: {
+            browserName: 'firefox',
+            name: 'WIN_FIREFOX_LATEST',
+            platform: 'Windows 10',
+            version: 'latest'
         },
-        chrome: {
-            chrome_win_latest: {
-                browserName: 'chrome',
-                name: 'Probate_WIN_CHROME_LATEST',
-                platform: 'Windows 10',
-                version: 'latest'
-            },
-            chrome_mac_latest: {
-                browserName: 'chrome',
-                name: 'Probate_MAC_CHROME_LATEST',
-                platform: 'macOS 10.13',
-                version: 'latest'
-            }
-        },
-        firefox: {
-            firefox_win_latest: {
-                browserName: 'firefox',
-                name: 'Probate_WIN_FIREFOX_LATEST',
-                platform: 'Windows 10',
-                version: 'latest'
-            },
-            firefox_mac_latest: {
-                browserName: 'firefox',
-                name: 'Probate_MAC_FIREFOX_LATEST',
-                platform: 'macOS 10.14',
-                version: 'latest'
-            }
+        firefox_mac_latest: {
+            browserName: 'firefox',
+            name: 'MAC_FIREFOX_LATEST',
+            platform: 'macOS 10.13',
+            version: 'latest'
         }
     }
 };
