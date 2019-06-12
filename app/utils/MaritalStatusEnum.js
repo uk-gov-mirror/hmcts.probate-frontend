e456d76fed1f8a3fe1628265fa4a7824be40260d
Wivenhoe-HMCTS <jason.smith@HMCTS.NET>
2019-06-12 16:03:08 +0100

    PRO-4775: add new intestacy fields

--- app/utils/MaritalStatusEnum.js
+++ app/utils/MaritalStatusEnum.js
@@ -1,42 +1,41 @@
 'use strict';
-/*eslint indent: ["error", 2, { "SwitchCase": 2 }]*/
 
 const content = require('app/resources/en/translation/deceased/maritalstatus');
 
 class MaritalStatusEnum {
 
-  static getWidowed() {
-    return 'widowed';
-  }
-  static getMarriedCivilPartnerhip() {
-    return 'marriedCivilPartnership';
-  }
-  static getDivorcedCivilPartnerShip() {
-    return 'divorcedCivilPartnership';
-  }
-  static getJudicially() {
-    return 'judicially';
-  }
-  static getNeverMarried() {
-    return 'neverMarried';
-  }
-
-  static getCCDCode(value) {
-    switch (value) {
-        case content.optionWidowed:
-          return this.getWidowed();
-        case content.optionMarried:
-          return this.getMarriedCivilPartnerhip();
-        case content.optionDivorced:
-          return this.getDivorcedCivilPartnerShip();
-        case content.optionSeparated:
-          return this.getJudicially();
-        case content.optionNotMarried:
-          return this.getNeverMarried();
-        default:
-          throw new Error(`Enumerator MaritalStatusEnum value: ${value} not found`);
+    static getWidowed() {
+        return 'widowed';
     }
-  }
+    static getMarriedCivilPartnerhip() {
+        return 'marriedCivilPartnership';
+    }
+    static getDivorcedCivilPartnerShip() {
+        return 'divorcedCivilPartnership';
+    }
+    static getJudicially() {
+        return 'judicially';
+    }
+    static getNeverMarried() {
+        return 'neverMarried';
+    }
+
+    static getCCDCode(value) {
+        switch (value) {
+        case content.optionWidowed:
+            return this.getWidowed();
+        case content.optionMarried:
+            return this.getMarriedCivilPartnerhip();
+        case content.optionDivorced:
+            return this.getDivorcedCivilPartnerShip();
+        case content.optionSeparated:
+            return this.getJudicially();
+        case content.optionNotMarried:
+            return this.getNeverMarried();
+        default:
+            throw new Error(`Enumerator MaritalStatusEnum value: ${value} not found`);
+        }
+    }
 }
 
 module.exports = MaritalStatusEnum;
--- app/utils/RelationshipToTheDeceasedEnum.js
+++ app/utils/RelationshipToTheDeceasedEnum.js
@@ -1,36 +1,35 @@
 'use strict';
-/*eslint indent: ["error", 2, { "SwitchCase": 2 }]*/
 
 const content = require('app/resources/en/translation/applicant/relationshiptodeceased');
 
 class RelationshipToTheDeceasedEnum {
-  static getPartner() {
-    return 'partner';
-  }
-  static getChild() {
-    return 'child';
-  }
-  static getAdoptedChild() {
-    return 'adoptedChild';
-  }
-  static getOther() {
-    return 'other';
-  }
-
-  static getCCDCode(value) {
-    switch (value) {
-        case content.optionSpousePartner:
-          return this.getPartner();
-        case content.optionChild:
-          return this.getChild();
-        case content.optionAdoptedChild:
-          return this.getAdoptedChild();
-        case content.optionOther:
-          return this.getOther();
-        default:
-          throw new Error(`Enumerator RelationshipToDeceasedEnum value: ${value} not found`);
+    static getPartner() {
+        return 'partner';
     }
-  }
+    static getChild() {
+        return 'child';
+    }
+    static getAdoptedChild() {
+        return 'adoptedChild';
+    }
+    static getOther() {
+        return 'other';
+    }
+
+    static getCCDCode(value) {
+        switch (value) {
+        case content.optionSpousePartner:
+            return this.getPartner();
+        case content.optionChild:
+            return this.getChild();
+        case content.optionAdoptedChild:
+            return this.getAdoptedChild();
+        case content.optionOther:
+            return this.getOther();
+        default:
+            throw new Error(`Enumerator RelationshipToDeceasedEnum value: ${value} not found`);
+        }
+    }
 }
 
 module.exports = RelationshipToTheDeceasedEnum;
