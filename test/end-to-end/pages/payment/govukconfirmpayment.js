7831f7a3c83ce41fd2d34482e300efd5a3c5c0a5
Doug Rice <Douglas.Rice@hmcts.net>
2019-06-13 13:39:05 +0100

    PRO-5559: Test

--- test/end-to-end/pages/payment/govukconfirmpayment.js
+++ test/end-to-end/pages/payment/govukconfirmpayment.js
@@ -1,11 +1,12 @@
 'use strict';
 
-//const testConfig = require('test/config');
+const testConfig = require('test/config');
 
 module.exports = function () {
     const I = this;
 
     I.waitForText('Payment summary', 20);
+    I.seeInCurrentUrl(testConfig.TestGovUkConfirmPaymentUrl);
     I.waitForElement('#confirm', 20);
 
     I.click('#confirm');
