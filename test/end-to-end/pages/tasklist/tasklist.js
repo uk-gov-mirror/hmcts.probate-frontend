2040c2b8e1950fedc94334cf990c8ab21137e3a7
Doug Rice <Douglas.Rice@hmcts.net>
2019-06-13 15:54:11 +0100

    PRO-5559: Test

--- test/end-to-end/pages/tasklist/tasklist.js
+++ test/end-to-end/pages/tasklist/tasklist.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const content = require('app/resources/en/translation/tasklist');
 const pageUnderTest = require('app/steps/ui/tasklist');
 
 module.exports = function () {
     const I = this;
+    I.waitForText(content.introduction, 20);
     I.amOnLoadedPage(pageUnderTest.getUrl());
 
     I.click('.button');
