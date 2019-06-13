fbe2fb3b817ce3e90134010cdbb13063bf29dd6d
Doug Rice <Douglas.Rice@hmcts.net>
2019-06-13 14:55:56 +0100

    PRO-5559: Test

--- test/end-to-end/pages/payment/paymentstatus.js
+++ test/end-to-end/pages/payment/paymentstatus.js
@@ -1,11 +1,13 @@
 'use strict';
 
+const content = require('app/resource/en/translation/payment/status');
 const pageUnderTest = require('app/steps/ui/payment/status');
 
 module.exports = function () {
     const I = this;
 
-    I.amOnLoadedPage(pageUnderTest.getUrl());
+    I.waitForText(content.question, 20);
+    I.seeCurrentUrlEquals(pageUnderTest.getUrl());
 
     I.navByClick('.button');
 };
