a815c93cb1d74cd41fab789067a5d98322970208
Luca Del Buono <luca.delbuono@gmail.com>
2019-06-13 15:13:18 +0100

    Further Document Upload FT cleanup

--- test/component/testDocumentUpload.js
+++ test/component/testDocumentUpload.js
@@ -6,13 +6,6 @@ const commonContent = require('app/resources/en/translation/common');
 const config = require('app/config');
 const nock = require('nock');
 const expect = require('chai').expect;
-const featureToggleUrl = config.featureToggles.url;
-const documentUploadFeatureTogglePath = `${config.featureToggles.path}/${config.featureToggles.document_upload}`;
-const featureTogglesNock = (status = 'true') => {
-    nock(featureToggleUrl)
-        .get(documentUploadFeatureTogglePath)
-        .reply(200, status);
-};
 
 describe('document-upload', () => {
     let testWrapper;
@@ -28,8 +21,6 @@ describe('document-upload', () => {
 
     describe('Verify Content, Errors and Redirection', () => {
         it('test help block content loaded on the page', (done) => {
-            featureTogglesNock('true');
-
             const playbackData = {
                 helpTitle: commonContent.helpTitle,
                 helpHeading1: commonContent.helpHeading1,
@@ -42,8 +33,6 @@ describe('document-upload', () => {
         });
 
         it('test content loaded on the page', (done) => {
-            featureTogglesNock('true');
-
             testWrapper.testContent(done);
         });
 
