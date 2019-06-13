c40b826bacdad5b03f29d20ac943cff8508b9e97
Luca Del Buono <luca.delbuono@gmail.com>
2019-06-13 11:30:44 +0100

    PRO-4771: Summary component tests

--- test/component/summary/testSummary.js
+++ test/component/summary/testSummary.js
@@ -7,15 +7,24 @@ const config = require('app/config');
 const nock = require('nock');
 const featureToggleUrl = config.featureToggles.url;
 const documentUploadFeatureTogglePath = `${config.featureToggles.path}/${config.featureToggles.document_upload}`;
-const featureTogglesNock = (status = 'true') => {
+const intestacyQuestionsFeatureTogglePath = `${config.featureToggles.path}/${config.featureToggles.intestacy_questions}`;
+const featureTogglesNockDocumentUpload = (status = 'true') => {
     nock(featureToggleUrl)
         .get(documentUploadFeatureTogglePath)
+        .reply(200, status);
+};
+const featureTogglesNockIntestacy = (status = 'true') => {
+    nock(featureToggleUrl)
+        .get(intestacyQuestionsFeatureTogglePath)
         .reply(200, status);
 };
 
 describe('summary', () => {
     let testWrapper;
     const expectedNextUrlForTaskList = TaskList.getUrl();
+    const sessionDataIntestacy = {
+        caseType: 'intestacy'
+    };
 
     beforeEach(() => {
         testWrapper = new TestWrapper('Summary');
@@ -27,8 +36,9 @@ describe('summary', () => {
     });
 
     describe('Verify Content, Errors and Redirection', () => {
-        it('test content loaded on the page with the document upload feature toggle OFF', (done) => {
-            featureTogglesNock('false');
+        it('[PROBATE] test content loaded on the page with the document upload feature toggle OFF', (done) => {
+            featureTogglesNockDocumentUpload('false');
+            featureTogglesNockIntestacy('false');
 
             const contentToExclude = [
                 'executorsWhenDiedQuestion',
@@ -50,8 +60,9 @@ describe('summary', () => {
             testWrapper.testContent(done, contentToExclude);
         });
 
-        it('test content loaded on the page with the document upload feature toggle ON and documents uploaded', (done) => {
-            featureTogglesNock('true');
+        it('[PROBATE] test content loaded on the page with the document upload feature toggle ON and documents uploaded', (done) => {
+            featureTogglesNockDocumentUpload('true');
+            featureTogglesNockIntestacy('false');
 
             const contentToExclude = [
                 'executorsWhenDiedQuestion',
@@ -73,6 +84,33 @@ describe('summary', () => {
 
             testWrapper.agent.post('/prepare-session/form')
                 .send(sessionData)
+                .end(() => {
+                    testWrapper.testContent(done, contentToExclude);
+                });
+        });
+
+        it('[INTESTACY] test content loaded on the page', (done) => {
+            featureTogglesNockIntestacy('true');
+
+            const contentToExclude = [
+                'executorsWhenDiedQuestion',
+                'otherNamesLabel',
+                'willWithCodicilHeading',
+                'otherExecutors',
+                'executorsWithOtherNames',
+                'executorApplyingForProbate',
+                'executorsNotApplyingForProbate',
+                'nameOnWill',
+                'currentName',
+                'currentNameReason',
+                'mobileNumber',
+                'emailAddress',
+                'uploadedDocumentsHeading',
+                'uploadedDocumentsEmpty',
+                'applicantHeading'
+            ];
+            testWrapper.agent.post('/prepare-session/form')
+                .send(sessionDataIntestacy)
                 .end(() => {
                     testWrapper.testContent(done, contentToExclude);
                 });
