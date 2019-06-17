d9417f31589bd40d420eca3a8459c69bdcb33341
Luca Del Buono <luca.delbuono@gmail.com>
2019-06-17 14:19:04 +0100

    PRO-4779: Add Intestacy component test

--- app/steps/ui/documents/index.js
+++ app/steps/ui/documents/index.js
@@ -6,6 +6,8 @@ const ExecutorsWrapper = require('app/wrappers/Executors');
 const WillWrapper = require('app/wrappers/Will');
 const RegistryWrapper = require('app/wrappers/Registry');
 const ihtContent = require('app/resources/en/translation/iht/method');
+const deceasedMaritalStatusContent = require('app/resources/en/translation/deceased/maritalstatus');
+const relationshipToDeceasedContent = require('app/resources/en/translation/applicant/relationshiptodeceased');
 const FormatCcdCaseId = require('app/utils/FormatCcdCaseId');
 
 class Documents extends ValidationStep {
@@ -16,17 +18,24 @@ class Documents extends ValidationStep {
 
     handleGet(ctx, formdata) {
         const executorsWrapper = new ExecutorsWrapper(formdata.executors);
+        const willWrapper = new WillWrapper(formdata.will);
         const registryAddress = (new RegistryWrapper(formdata.registry)).address();
         const content = this.generateContent(ctx, formdata);
 
         ctx.registryAddress = registryAddress ? registryAddress : content.address;
 
         if (ctx.journeyType === 'gop') {
-            ctx.hasCodicils = (new WillWrapper(formdata.will)).hasCodicils();
-            ctx.codicilsNumber = (new WillWrapper(formdata.will)).codicilsNumber();
+            ctx.hasCodicils = willWrapper.hasCodicils();
+            ctx.codicilsNumber = willWrapper.codicilsNumber();
             ctx.hasMultipleApplicants = executorsWrapper.hasMultipleApplicants();
             ctx.hasRenunciated = executorsWrapper.hasRenunciated();
             ctx.executorsNameChangedByDeedPollList = executorsWrapper.executorsNameChangedByDeedPoll();
+        } else {
+            ctx.spouseRenouncing = formdata.deceased.maritalStatus === deceasedMaritalStatusContent.optionMarried && (formdata.applicant.relationshipToDeceased === relationshipToDeceasedContent.optionChild || formdata.applicant.relationshipToDeceased === relationshipToDeceasedContent.optionAdoptedChild);
+
+            console.log('MARRIED: ', formdata.deceased.maritalStatus === deceasedMaritalStatusContent.optionMarried);
+            console.log('RELATIONSHIP: ', formdata.applicant.relationshipToDeceased);
+            console.log('RENOUNCING: ', ctx.spouseRenouncing);
         }
 
         ctx.is205 = formdata.iht && formdata.iht.method === ihtContent.optionPaper && formdata.iht.form === 'IHT205';
