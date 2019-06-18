83ee6916cca7602eb36aad8bbacedd3e852a4cc2
Doug Rice <Douglas.Rice@hmcts.net>
2019-06-18 12:09:04 +0100

    PRO-1234: Test

--- test/end-to-end/helpers/WebDriverHelper.js
+++ test/end-to-end/helpers/WebDriverHelper.js
@@ -15,17 +15,18 @@ class WebDriverHelper extends Helper {
         return (this.helpers.WebDriverIO.config.browser === 'internet explorer');
     }
 
-    isEdge() {
+    async isEdge() {
         /* eslint no-console: 0 no-unused-vars: 0 */
-        const browser = this.helpers.WebDriverIO.browser;
-        console('here1', browser);
-        //console.log('browser>>>>', browser.config.browser);
-        // if (browser.config.browser !== 'MicrosoftEdge') {
-        //     console.log('browser.config.browser>>>>>>>>', browser.config.browser);
-        //     browser.waitForExist('.dz-hidden-input', testConfig.TestWaitForElementToAppear * testConfig.TestOneMilliSecond);
-        //     browser.chooseFile('.dz-hidden-input', '/uploadDocuments/test_file_for_document_upload.png');
-        //     browser.waitForEnabled('#button', testConfig.TestDocumentToUpload);
-        // }
+        const helper = this.helpers.WebDriverIO;
+        const browserName = this.helpers.WebDriverIO.config.browser;
+
+        console.log('browserName>>>>', browserName);
+        if (browserName !== 'MicrosoftEdge') {
+            console.log('NOT Edge');
+            await helper.waitForExist('.dz-hidden-input', testConfig.TestWaitForElementToAppear * testConfig.TestOneMilliSecond);
+            await helper.chooseFile('.dz-hidden-input', '/uploadDocuments/test_file_for_document_upload.png');
+            await helper.waitForEnabled('#button', testConfig.TestDocumentToUpload);
+        }
         /* eslint no-useless-return: 0 no-unused-vars: 0 */
         return;
     }
