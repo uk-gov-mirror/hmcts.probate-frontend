dfe8d73aced991f70c694c4bf64e339cf816d7dc
Doug Rice <Douglas.Rice@hmcts.net>
2019-06-18 09:52:24 +0100

    PRO-1234: Test

--- bin/run-crossbrowser-tests.sh
+++ bin/run-crossbrowser-tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ex
 
-#yarn test:crossbrowser_IE11
+yarn test:crossbrowser_IE11
 sleep 2s
-yarn test:crossbrowser_Edge
+#yarn test:crossbrowser_Edge
 #sleep 2s
 #test:crossbrowser_chrome
 #sleep 2s
