6ee565cdb6b1667eae331c0aa6b61b368af2776c
Doug Rice <Douglas.Rice@hmcts.net>
2019-06-18 12:34:10 +0100

    PRO-1234: Test

--- test/end-to-end/helpers/WebDriverHelper.js
+++ test/end-to-end/helpers/WebDriverHelper.js
@@ -15,21 +15,14 @@ class WebDriverHelper extends Helper {
         return (this.helpers.WebDriverIO.config.browser === 'internet explorer');
     }
 
-    async isEdge() {
-        /* eslint no-console: 0 no-unused-vars: 0 */
-        //const helper = this.helpers.WebDriverIO.browser;
+    async uploadDocumentIfNotMicrosoftEdge() {
         const browserName = this.helpers.WebDriverIO.config.browser;
         const helper = this.helpers.WebDriverIO;
 
-        console.log('browserName>>>>', browserName);
         if (browserName !== 'MicrosoftEdge') {
-            console.log('NOT Edge');
             await helper.waitForElement('.dz-hidden-input', testConfig.TestWaitForElementToAppear * testConfig.TestOneMilliSecond);
-            console.log('done step 1');
             await helper.attachFile('.dz-hidden-input', '/uploadDocuments/test_file_for_document_upload.png');
-            console.log('done step 2');
             await helper.waitForEnabled('#button', testConfig.TestDocumentToUpload);
-            console.log('done step 3');
         }
         /* eslint no-useless-return: 0 no-unused-vars: 0 */
         return;
--- test/end-to-end/pages/documentupload/documentupload.js
+++ test/end-to-end/pages/documentupload/documentupload.js
@@ -10,18 +10,8 @@ module.exports = function () {
     I.amOnLoadedPage(pageUnderTest.getUrl());
 
     I.waitForVisible('.document-upload__dropzone-text--choose-file');
-    /* eslint no-console: 0 no-unused-vars: 0 */
-    console.log('>>>>>>>>>>>>>>>>>>>>>>>loading document<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
 
-    I.isEdge();
-    //     //if (I.seeElementInDOM('.dz-hidden-input')) {
-    //     /* eslint no-console: 0 no-unused-vars: 0 */
-    //     console.log('>>>>>>>>>>>>>>>>>>>>>>>loading document<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
-    //     I.waitForElement('.dz-hidden-input', testConfig.TestWaitForElementToAppear);
-    //     I.attachFile('.dz-hidden-input', '/uploadDocuments/test_file_for_document_upload.png');
-    //     I.waitForEnabled('#button', testConfig.TestDocumentToUpload);
-    //     // }
-    // }
+    I.uploadDocumentIfNotMicrosoftEdge();
 
     I.navByClick(commonContent.continue);
 };
