70b37dd4db8ab221eda730dd0b61f1e479849417
Doug Rice <Douglas.Rice@hmcts.net>
2019-06-18 11:00:09 +0100

    PRO-1234: Test

--- test/end-to-end/pages/documentupload/documentupload.js
+++ test/end-to-end/pages/documentupload/documentupload.js
@@ -12,11 +12,13 @@ module.exports = function () {
     I.waitForVisible('.document-upload__dropzone-text--choose-file');
 
     if (I.isEdge() === 'f') {
-        if (I.seeElementInDOM('.dz-hidden-input')) {
-            I.waitForElement('.dz-hidden-input', testConfig.TestWaitForElementToAppear);
-            I.attachFile('.dz-hidden-input', '/uploadDocuments/test_file_for_document_upload.png');
-            I.waitForEnabled('#button', testConfig.TestDocumentToUpload);
-        }
+        //if (I.seeElementInDOM('.dz-hidden-input')) {
+        /* eslint no-console: 0 no-unused-vars: 0 */
+        console.log('>>>>>>>>>>>>>>>>>>>>>>>loading document<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
+        I.waitForElement('.dz-hidden-input', testConfig.TestWaitForElementToAppear);
+        I.attachFile('.dz-hidden-input', '/uploadDocuments/test_file_for_document_upload.png');
+        I.waitForEnabled('#button', testConfig.TestDocumentToUpload);
+        // }
     }
 
     I.navByClick(commonContent.continue);
