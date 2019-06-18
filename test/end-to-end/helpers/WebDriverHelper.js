3ac54a9645dac35103c2f2ffe361d8ef0997f3ba
Doug Rice <Douglas.Rice@hmcts.net>
2019-06-18 12:23:33 +0100

    PRO-1234: Test

--- test/end-to-end/helpers/WebDriverHelper.js
+++ test/end-to-end/helpers/WebDriverHelper.js
@@ -17,15 +17,16 @@ class WebDriverHelper extends Helper {
 
     async isEdge() {
         /* eslint no-console: 0 no-unused-vars: 0 */
-        const helper = this.helpers.WebDriverIO.browser;
+        //const helper = this.helpers.WebDriverIO.browser;
         const browserName = this.helpers.WebDriverIO.config.browser;
+        const helper = this.helpers.WebDriverIO;
 
         console.log('browserName>>>>', browserName);
         if (browserName !== 'MicrosoftEdge') {
             console.log('NOT Edge');
-            await helper.waitForExist('.dz-hidden-input', testConfig.TestWaitForElementToAppear * testConfig.TestOneMilliSecond);
+            await helper.waitForElement('.dz-hidden-input', testConfig.TestWaitForElementToAppear * testConfig.TestOneMilliSecond);
             console.log('done step 1');
-            await helper.chooseFile('.dz-hidden-input', '/uploadDocuments/test_file_for_document_upload.png');
+            await helper.attachFile('.dz-hidden-input', '/uploadDocuments/test_file_for_document_upload.png');
             console.log('done step 2');
             await helper.waitForEnabled('#button', testConfig.TestDocumentToUpload);
             console.log('done step 3');
