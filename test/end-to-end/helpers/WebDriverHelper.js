447dace10e93135005d564bd13fc08655c87ced5
Doug Rice <Douglas.Rice@hmcts.net>
2019-06-18 11:50:07 +0100

    PRO-1234: Test

--- test/end-to-end/helpers/WebDriverHelper.js
+++ test/end-to-end/helpers/WebDriverHelper.js
@@ -18,14 +18,14 @@ class WebDriverHelper extends Helper {
     isEdge() {
         /* eslint no-console: 0 no-unused-vars: 0 */
         const browser = this.helpers.WebDriverIO.browser;
-
+        console('here1');
         console.log('browser>>>>', browser.config.browser);
-        if (browser.config.browser !== 'MicrosoftEdge') {
-            console.log('browser.config.browser>>>>>>>>', browser.config.browser);
-            browser.waitForExist('.dz-hidden-input', testConfig.TestWaitForElementToAppear * testConfig.TestOneMilliSecond);
-            browser.chooseFile('.dz-hidden-input', '/uploadDocuments/test_file_for_document_upload.png');
-            browser.waitForEnabled('#button', testConfig.TestDocumentToUpload);
-        }
+        // if (browser.config.browser !== 'MicrosoftEdge') {
+        //     console.log('browser.config.browser>>>>>>>>', browser.config.browser);
+        //     browser.waitForExist('.dz-hidden-input', testConfig.TestWaitForElementToAppear * testConfig.TestOneMilliSecond);
+        //     browser.chooseFile('.dz-hidden-input', '/uploadDocuments/test_file_for_document_upload.png');
+        //     browser.waitForEnabled('#button', testConfig.TestDocumentToUpload);
+        // }
     }
 
 }
