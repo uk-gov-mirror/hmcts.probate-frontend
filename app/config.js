043da34e5b4f13887d2c5a2a24210f07170a3874
Luca Del Buono <luca.delbuono@gmail.com>
2019-06-21 14:28:00 +0100

    PRO-4771: Re-add webchat FT

--- app/config.js
+++ app/config.js
@@ -12,6 +12,7 @@ const config = {
         fe_shutter_toggle: 'probate-fe-shutter',
         intestacy_questions: 'probate-intestacy-questions',
         fees_api: 'probate-fees-api',
+        webchat: 'probate-webchat',
         appwideToggles: ['webchat']
     },
     app: {
