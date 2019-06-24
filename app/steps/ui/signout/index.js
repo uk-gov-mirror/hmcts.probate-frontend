b17d46f820776fa31fc084b7288ada598bb4fb07
Wivenhoe-HMCTS <jason.smith@HMCTS.NET>
2019-06-24 15:51:14 +0100

    PRO-4787: change journeyType to caseType

--- app/steps/ui/signout/index.js
+++ app/steps/ui/signout/index.js
@@ -12,11 +12,13 @@ class SignOut extends Step {
         return '/sign-out';
     }
 
-    getContextData(req) {
+    * getContextData(req) {
         const ctx = super.getContextData(req);
         const access_token = req.cookies[SECURITY_COOKIE];
         const errorCodes = [400, 401, 403];
         const idamSession = new IdamSession(config.services.idam.apiUrl, req.sessionID);
+
+        yield this.persistFormData(req.session.regId, req.session.form, req.session.id);
 
         return idamSession.delete(access_token)
             .then(result => {
