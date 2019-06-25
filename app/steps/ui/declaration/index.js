3f596604abef93f8794634ce3b02fd913a4fb39d
Wivenhoe-HMCTS <jason.smith@HMCTS.NET>
2019-06-25 09:34:37 +0100

    PRO-4787: change journeyType to caseType

--- app/steps/ui/declaration/index.js
+++ app/steps/ui/declaration/index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const setJourney = require('app/middleware/setJourney');
 const probateDeclarationFactory = require('app/utils/ProbateDeclarationFactory');
 const intestacyDeclarationFactory = require('app/utils/IntestacyDeclarationFactory');
 const ValidationStep = require('app/core/steps/ValidationStep');
@@ -16,6 +15,7 @@ const LegalDocumentJSONObjectBuilder = require('app/utils/LegalDocumentJSONObjec
 const legalDocumentJSONObjBuilder = new LegalDocumentJSONObjectBuilder();
 const InviteData = require('app/services/InviteData');
 const config = require('app/config');
+const caseTypes = require('app/utils/CaseTypes');
 
 class Declaration extends ValidationStep {
     static getUrl() {
@@ -59,7 +59,7 @@ class Declaration extends ValidationStep {
         let templateData;
         let ctx = super.getContextData(req);
         ctx = this.pruneFormData(req.body, ctx);
-        ctx.isIntestacyJourney = setJourney.isIntestacyJourney(req.session);
+        ctx.isIntestacyJourney = caseTypes.isIntestacyCaseType(req.session);
         const formdata = req.session.form;
         const content = this.generateContent(ctx, formdata);
         const formDataForTemplate = this.getFormDataForTemplate(content, formdata);
--- app/steps/ui/signout/index.js
+++ app/steps/ui/signout/index.js
@@ -12,13 +12,13 @@ class SignOut extends Step {
         return '/sign-out';
     }
 
-    * getContextData(req) {
+    getContextData(req) {
         const ctx = super.getContextData(req);
         const access_token = req.cookies[SECURITY_COOKIE];
         const errorCodes = [400, 401, 403];
         const idamSession = new IdamSession(config.services.idam.apiUrl, req.sessionID);
 
-        yield this.persistFormData(req.session.regId, req.session.form, req.session.id);
+        this.persistBeforeSignOut(req);
 
         return idamSession.delete(access_token)
             .then(result => {
@@ -37,6 +37,10 @@ class SignOut extends Step {
             .catch(err => {
                 logger.error(`Error while calling IDAM: ${err}`);
             });
+    }
+
+    * persistBeforeSignOut(req) {
+        yield this.persistFormData(req.session.regId, req.session.form, req.session.id);
     }
 }
 
