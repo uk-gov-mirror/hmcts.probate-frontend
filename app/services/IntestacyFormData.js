d81eed2eefb6ad2c49d9fa4e8b883e9c16a0b399
Wivenhoe-HMCTS <jason.smith@HMCTS.NET>
2019-06-27 11:02:20 +0100

    PRO-4787: remove orchestartion config from submit for intestacy

--- app/services/IntestacyFormData.js
+++ app/services/IntestacyFormData.js
@@ -4,17 +4,19 @@ const FormData = require('./FormData');
 
 class IntestacyFormData extends FormData {
     get(id) {
-        const path = this.replaceEmailInPath(this.config.services.orchestrator.paths.forms, id);
         const logMessage = 'Get intestacy form data';
-        const url = this.endpoint + path;
+        const url = `${this.endpoint}/${id}`;
         return super.get(logMessage, url);
     }
 
     post(id, data) {
-        const path = this.replaceEmailInPath(this.config.services.orchestrator.paths.forms, id);
         const logMessage = 'Post intestacy form data';
-        const url = this.endpoint + path;
-        return super.post(data, logMessage, url);
+        const url = this.endpoint;
+        const bodyData = {
+            id: id,
+            formdata: data,
+        };
+        return super.post(bodyData, logMessage, url);
     }
 }
 
