4aee9811c20aa324a2dc3f22a1594e2e317dc42d
meganaslattery <megan.slattery@virgin.net>
2019-07-02 10:06:29 +0100

    PRO-5537: Update document remove element

--- app/assets/javascripts/document-upload.js
+++ app/assets/javascripts/document-upload.js
@@ -102,7 +102,6 @@ var DocumentUpload = {
     addDocumentRemoveLink: function() {
         $('.dz-preview').each(function(key) {
             $(this).find('.dz-remove').attr('href', '/document-upload/remove/'+key);
-            $(this).find('.dz-remove').attr('data-index', key);
         });
     },
     removeDocument: function(index) {
