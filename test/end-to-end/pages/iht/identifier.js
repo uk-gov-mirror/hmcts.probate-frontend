0bb30434b7d547dcbcf8ee0a1f4efb9dc067f9ac
meganaslattery <megan.slattery@virgin.net>
2019-07-10 16:56:16 +0100

    PRO-5420: Change iht identifier page to use different test value

--- test/end-to-end/pages/iht/identifier.js
+++ test/end-to-end/pages/iht/identifier.js
@@ -6,7 +6,6 @@ const pageUnderTest = require('app/steps/ui/iht/identifier');
 module.exports = function () {
     const I = this;
     I.amOnLoadedPage(pageUnderTest.getUrl());
-    I.fillField('#identifier', '00060497A01573');
-
+    I.fillField('#identifier', '123456789XXXXX');
     I.navByClick(commonContent.saveAndContinue);
 };
