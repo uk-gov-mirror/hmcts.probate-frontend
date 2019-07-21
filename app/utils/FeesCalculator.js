52ebbde9a50ebe19c20275a4da41a501360289e8
Luca Del Buono <luca.delbuono@gmail.com>
2019-07-21 19:05:21 +0100

    Update copies fees keyword for Prod

--- app/utils/FeesCalculator.js
+++ app/utils/FeesCalculator.js
@@ -70,7 +70,7 @@ async function createCallsRequired(formdata, headers, featureToggles, feesLookup
     }
 
     if (featureToggle.isEnabled(featureToggles, 'copies_fees')) {
-        copiesData.keyword = 'NewFee';
+        copiesData.keyword = 'HUG';
     }
 
     copiesData.amount_or_volume = get(formdata, 'copies.uk', 0);
