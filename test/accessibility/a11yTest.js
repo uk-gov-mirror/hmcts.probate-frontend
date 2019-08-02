0797b7201e595bf21981e1956b6f9bf5a66ba5ae
Luca Del Buono <luca.delbuono@gmail.com>
2019-08-02 12:20:52 +0100

    Add new line on a11y test for readability

--- test/accessibility/a11yTest.js
+++ test/accessibility/a11yTest.js
@@ -18,7 +18,8 @@ const steps = initSteps.steps;
 const nock = require('nock');
 const config = require('app/config');
 
-Object.keys(steps).filter(stepName => stepsToExclude.includes(stepName))
+Object.keys(steps)
+    .filter(stepName => stepsToExclude.includes(stepName))
     .forEach((stepName) => delete steps[stepName]);
 
 for (const step in steps) {
