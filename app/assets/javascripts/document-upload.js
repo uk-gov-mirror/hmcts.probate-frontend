309e539f0779462b3fd5ae6e72b5ef2abd4cb5fa
Luca Del Buono <luca.delbuono@gmail.com>
2019-08-06 15:16:18 +0100

    Remove bold from Summary key column and fix uploded document filename font

--- app/assets/javascripts/document-upload.js
+++ app/assets/javascripts/document-upload.js
@@ -102,6 +102,7 @@ var DocumentUpload = {
     },
     updateLinkAttributes: function() {
         $('.dz-preview').each(function(key) {
+            $(this).find('.dz-filename').addClass('govuk-link');
             $(this).find('.dz-remove').addClass('govuk-link');
             $(this).find('.dz-remove').attr('data-index', key);
             $(this).find('.dz-remove').attr('href', '/document-upload/remove/' + key);
