8794175b507cbda5ba81ce87b252319fdb459128
Luca Del Buono <luca.delbuono@gmail.com>
2019-08-27 13:24:10 +0100

    PRO-6069: Revert a11y test

--- test/accessibility/a11yTest.js
+++ test/accessibility/a11yTest.js
@@ -41,11 +41,15 @@ for (const step in steps) {
                     .reply(200, 'false');
 
                 nock(config.featureToggles.url)
-                    .get(`${config.featureToggles.path}/probate-fees-api`)
+                    .get(`${config.featureToggles.path}/probate-intestacy-questions`)
                     .reply(200, 'true');
 
                 nock(config.featureToggles.url)
-                    .get(`${config.featureToggles.path}/probate-multiple-applications`)
+                    .get(`${config.featureToggles.path}/probate-webchat`)
+                    .reply(200, 'true');
+
+                nock(config.featureToggles.url)
+                    .get(`${config.featureToggles.path}/probate-fees-api`)
                     .reply(200, 'true');
 
                 server = app.init();
