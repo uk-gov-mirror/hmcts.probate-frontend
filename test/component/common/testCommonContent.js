c8f34e038e10d0acedb46b22f1f7093ebd091739
Luca Del Buono <luca.delbuono@gmail.com>
2019-08-28 15:46:31 +0100

    PRO-6069: Remove stuff from TestCommonContent

--- test/component/common/testCommonContent.js
+++ test/component/common/testCommonContent.js
@@ -5,7 +5,7 @@ const commonContent = require('app/resources/en/translation/common');
 const config = require('app/config');
 
 class TestCommonContent {
-    static runTest(page, beforeEach, afterEach, cookies = [], pageOutsideIdam = false) {
+    static runTest(page, beforeEach, afterEach, cookies = []) {
         const testWrapper = new TestWrapper(page);
 
         describe('Test the help content', () => {
@@ -22,49 +22,6 @@ class TestCommonContent {
                 };
 
                 testWrapper.testDataPlayback(done, playbackData, [], cookies);
-            });
-
-            testWrapper.destroy();
-            if (typeof afterEach === 'function') {
-                afterEach();
-            }
-        });
-
-        describe('Test the navigation links', () => {
-            if (pageOutsideIdam) {
-                it('test "my account" and "sign out" links are not displayed on the page when the user is not logged in', (done) => {
-                    if (typeof beforeEach === 'function') {
-                        beforeEach();
-                    }
-
-                    const playbackData = {
-                        myApplications: commonContent.myApplications,
-                        signOut: commonContent.signOut
-                    };
-
-                    testWrapper.testContentNotPresent(done, playbackData);
-                });
-            }
-
-            it('test "my account" and "sign out" links are displayed on the page when the user is logged in', (done) => {
-                if (typeof beforeEach === 'function') {
-                    beforeEach();
-                }
-
-                const sessionData = {
-                    applicantEmail: 'test@email.com'
-                };
-
-                testWrapper.agent.post('/prepare-session/form')
-                    .send(sessionData)
-                    .end(() => {
-                        const playbackData = {
-                            myApplications: commonContent.myApplications,
-                            signOut: commonContent.signOut
-                        };
-
-                        testWrapper.testDataPlayback(done, playbackData);
-                    });
             });
 
             testWrapper.destroy();
