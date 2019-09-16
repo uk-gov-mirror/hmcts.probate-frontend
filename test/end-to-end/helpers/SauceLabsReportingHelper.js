ee73db4af3a3bc4fbd43cdae2855f7b23b31c31f
Douglas Rice <Douglas.Rice@hmcts.net>
2019-09-16 17:51:02 +0100

    PRO-6325: Fix nightly e2e

--- test/end-to-end/helpers/SauceLabsReportingHelper.js
+++ test/end-to-end/helpers/SauceLabsReportingHelper.js
@@ -8,7 +8,7 @@ function updateSauceLabsResult(result, sessionId) {
     return 'curl -X PUT -s -d \'{"passed": ' + result + '}\' -u ' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + ' https://eu-central-1.saucelabs.com/rest/v1/' + process.env.SAUCE_USERNAME + '/jobs/' + sessionId;
 }
 
-module.exports = () => {
+module.exports = function() {
 
     // Setting test success on SauceLabs
     event.dispatcher.on(event.test.passed, () => {
