8dd6b0ca89292821470223f789aa9c01e293d78d
Douglas Rice <Douglas.Rice@hmcts.net>
2019-09-16 17:47:33 +0100

    PRO-6235: Fix nightly e2e test

--- test/end-to-end/helpers/SauceLabsReportingHelper.js
+++ test/end-to-end/helpers/SauceLabsReportingHelper.js
@@ -4,9 +4,9 @@ const event = require('codeceptjs').event;
 const container = require('codeceptjs').container;
 const exec = require('child_process').exec;
 
-const updateSauceLabsResult = (result, sessionId) => {
-    return 'curl -X PUT -s -d \'{"passed": ' + result + '}\' -u ' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + ' https://saucelabs.com/rest/v1/' + process.env.SAUCE_USERNAME + '/jobs/' + sessionId;
-};
+function updateSauceLabsResult(result, sessionId) {
+    return 'curl -X PUT -s -d \'{"passed": ' + result + '}\' -u ' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + ' https://eu-central-1.saucelabs.com/rest/v1/' + process.env.SAUCE_USERNAME + '/jobs/' + sessionId;
+}
 
 module.exports = () => {
 
