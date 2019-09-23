ecac4828c1b42e81c1cc0268cfa1c4b96a567663
Luca Del Buono <luca.delbuono@gmail.com>
2019-09-23 13:20:47 +0100

    PRO-5208: Content for Accessibility page

--- app/steps/ui/static/accessibility/index.js
+++ app/steps/ui/static/accessibility/index.js
@@ -5,7 +5,7 @@ const Step = require('app/core/steps/Step');
 class Accessibility extends Step {
 
     static getUrl () {
-        return '/accessibility';
+        return '/accessibility-statement';
     }
 }
 
