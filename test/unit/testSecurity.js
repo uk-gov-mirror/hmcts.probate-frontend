d181cf759287ef56c3c6b4500a76fd4e235f2e35
reddy1praveen <32567752+reddy1praveen@users.noreply.github.com>
2019-10-14 22:01:50 +0100

    Revert "Revert "Pro 6145 fix local set up""

--- test/unit/testSecurity.js
+++ test/unit/testSecurity.js
@@ -14,7 +14,7 @@ describe('Security middleware', () => {
     const role = 'probate-private-beta';
     const loginUrl = 'http://localhost:8000/login';
     const timeoutUrl = '/time-out';
-    const loginUrlWithContinue = `${loginUrl}?response_type=code&state=57473&client_id=probate&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Foauth2%2Fcallback`;
+    const loginUrlWithContinue = `${loginUrl}?response_type=code&state=57473&client_id=ccd_gateway&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Foauth2%2Fcallback`;
     const token = 'dummyToken';
     const appConfig = require('../../app/config');
     const securityCookie = `__auth-token-${appConfig.payloadVersion}`;
