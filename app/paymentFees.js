b3b71862df8f8a437af98978f4c00a3c678440b9
Luca Del Buono <luca.delbuono@gmail.com>
2019-10-17 15:10:50 +0100

    Test

--- app/paymentFees.js
+++ app/paymentFees.js
@@ -4,5 +4,6 @@ const router = require('express').Router();
 const calculatePaymentFees = require('app/middleware/calculatePaymentFees');
 
 router.get('/payment-breakdown', (req, res, next) => calculatePaymentFees(req, res, next));
+router.get('/payment-status', (req, res, next) => calculatePaymentFees(req, res, next));
 
 module.exports = router;
