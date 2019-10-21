45e816a03b8071510ef9c65c26f520df80b0f964
Luca Del Buono <luca.delbuono@gmail.com>
2019-10-21 11:16:17 +0100

    AuthToken and ServiceAuth: fix unit tests

--- test/unit/services/testAllExecutorsAgreed.js
+++ test/unit/services/testAllExecutorsAgreed.js
@@ -9,18 +9,20 @@ describe('AllExecutorsAgreedService', () => {
         it('should call log() and fetchText()', (done) => {
             const endpoint = 'http://localhost';
             const fetchOptions = {method: 'GET'};
-            const formdataId = '123';
+            const ccdCaseId = '123';
+            const authToken = 'authToken';
+            const serviceAuthorisation = 'serviceAuthorisation';
             const allExecutorsAgreed = new AllExecutorsAgreed(endpoint, 'abc123');
             const logSpy = sinon.spy(allExecutorsAgreed, 'log');
             const fetchTextSpy = sinon.spy(allExecutorsAgreed, 'fetchText');
             const fetchOptionsStub = sinon.stub(allExecutorsAgreed, 'fetchOptions').returns(fetchOptions);
 
-            allExecutorsAgreed.get(formdataId);
+            allExecutorsAgreed.get(authToken, serviceAuthorisation, ccdCaseId);
 
             expect(allExecutorsAgreed.log.calledOnce).to.equal(true);
             expect(allExecutorsAgreed.log.calledWith('Get all executors agreed')).to.equal(true);
             expect(allExecutorsAgreed.fetchText.calledOnce).to.equal(true);
-            expect(allExecutorsAgreed.fetchText.calledWith(`${endpoint}/invite/allAgreed/${formdataId}`, fetchOptions)).to.equal(true);
+            expect(allExecutorsAgreed.fetchText.calledWith(`${endpoint}/invite/allAgreed/${ccdCaseId}`, fetchOptions)).to.equal(true);
 
             logSpy.restore();
             fetchTextSpy.restore();
