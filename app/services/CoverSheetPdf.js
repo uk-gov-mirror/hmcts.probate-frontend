d0385c97e7a877d49f0c5a751180aa39833ba10e
ruban72 <ruban72@hotmail.com>
2019-11-04 13:28:28 +0000

    PRO-6535: Coversheet fix for registry details

--- app/services/CoverSheetPdf.js
+++ app/services/CoverSheetPdf.js
@@ -2,17 +2,19 @@
 
 const Pdf = require('./Pdf');
 const FormatName = require('app/utils/FormatName');
+const RegistryWrapper = require('app/wrappers/Registry');
 
 class CoverSheetPdf extends Pdf {
 
     post(req) {
         const pdfTemplate = this.config.pdf.template.coverSheet;
         const formdata = req.session.form;
+        const registryAddress = (new RegistryWrapper(formdata.registry)).address();
         const body = {
             applicantAddress: formdata.applicant.address.formattedAddress,
             applicantName: FormatName.format(formdata.applicant),
             caseReference: formdata.ccdCase.id,
-            submitAddress: formdata.registry.address
+            submitAddress: registryAddress
         };
         const logMessage = 'Post cover sheet pdf';
         return super.post(pdfTemplate, body, logMessage, req);
