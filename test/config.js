50374b6d06cff3ab0eda9ba0a139ed656ec543a2
Luca Del Buono <luca.delbuono@hmcts.net>
2019-12-03 12:10:29 +0000

    PRO-6776: Remove | safe

--- test/config.js
+++ test/config.js
@@ -84,7 +84,7 @@ module.exports = {
     },
     helpline: {
         number: '0300 303 0648',
-        hours: 'Monday to Friday, 8:00am to 8:00pm.<br>Saturday, 8:00am to 2:00pm'
+        hours: 'Monday to Friday, 8:00am to 8:00pm. Saturday, 8:00am to 2:00pm'
     },
 
     pact: {
