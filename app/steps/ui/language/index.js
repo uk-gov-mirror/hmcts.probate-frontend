6dac8a2eb63a60534759a6397a740a84c7caa2bf
Luca Del Buono <luca.delbuono@hmcts.net>
2020-02-10 10:34:19 +0000

    Fix eslint

--- app/steps/ui/language/index.js
+++ app/steps/ui/language/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const ValidationStep = require('app/core/steps/ValidationStep');
-const featureToggle = require('app/utils/FeatureToggle');
 
 class BilingualGOP extends ValidationStep {
 
