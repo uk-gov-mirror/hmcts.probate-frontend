67d87ed19db509378e4c37d949e82abd8fe78c44
Luca Del Buono <luca.delbuono@hmcts.net>
2020-04-06 09:16:46 +0100

    PRO-7186: Add back variables.tf

---
+++ infrastructure/variables.tf
@@ -0,0 +1,26 @@
+// Infrastructural variables
+
+variable "product" {}
+variable "component" {}
+
+variable "location" {
+  default = "UK South"
+}
+
+variable "env" {
+  type = "string"
+}
+
+variable "ilbIp" { }
+
+variable "subscription" {}
+
+// CNP settings
+variable "jenkins_AAD_objectId" {
+  type                        = "string"
+  description                 = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+}
+
+variable "common_tags" {
+  type = "map"
+}
