b6d4b2febb1b25083eb4926f3d133d0f543a40d0
Luca Del Buono <luca.delbuono@hmcts.net>
2020-04-20 14:53:37 +0100

    PRO-7216: Restructure test

--- test/unit/middleware/testCompleteEqualityTask.js
+++ test/unit/middleware/testCompleteEqualityTask.js
@@ -13,95 +13,97 @@ const stopStub = () => {
     equalityStub.close();
     delete require.cache[require.resolve('test/service-stubs/equalityAndDiversityHealth')];
 };
-describe('completeEqualityTask when the service is UP', () => {
-    before(() => startStub());
+describe('completeEqualityTask', () => {
+    describe('when the service is UP', () => {
+        before(() => startStub());
 
-    it('[PROBATE] PCQ status is UP', (done) => {
-        const req = {
-            session: {
-                caseType: 'gop',
-                form: {
-                    applicantEmail: 'test@email.com',
-                    ccdCase: {
-                        id: 1234567890123456
+        it('[PROBATE] PCQ status is UP', (done) => {
+            const req = {
+                session: {
+                    caseType: 'gop',
+                    form: {
+                        applicantEmail: 'test@email.com',
+                        ccdCase: {
+                            id: 1234567890123456
+                        }
                     }
                 }
-            }
-        };
-        const res = {redirect: () => {
-            // Do nothing
-        }};
-        const next = sinon.spy();
+            };
+            const res = {redirect: () => {
+                // Do nothing
+            }};
+            const next = sinon.spy();
 
-        completeEqualityTask(req, res, next);
+            completeEqualityTask(req, res, next);
 
-        setTimeout(() => {
-            expect(req.session.form.equality.pcqId).to.not.equal('Service down');
-            expect(next.calledOnce).to.equal(true);
+            setTimeout(() => {
+                expect(req.session.form.equality.pcqId).to.not.equal('Service down');
+                expect(next.calledOnce).to.equal(true);
 
-            done();
-        }, 500);
+                done();
+            }, 500);
+        });
+
+        after(() => stopStub());
     });
 
-    after(() => stopStub());
+    // describe('when the service is DOWN', () => {
+    //     it('[PROBATE] PCQ status is DOWN', (done) => {
+    //         const req = {
+    //             session: {
+    //                 caseType: 'gop',
+    //                 form: {
+    //                     applicantEmail: 'test@email.com',
+    //                     ccdCase: {
+    //                         id: 1234567890123456
+    //                     }
+    //                 }
+    //             }
+    //         };
+    //         const res = {redirect: () => {
+    //             // Do nothing
+    //         }};
+    //         const redirectSpy = sinon.spy(res, 'redirect');
+    //         const next = sinon.spy();
+    //
+    //         completeEqualityTask(req, res, next);
+    //
+    //         setTimeout(() => {
+    //             expect(req.session.form.equality.pcqId).to.equal('Service down');
+    //             expect(redirectSpy.calledOnce).to.equal(true);
+    //             expect(redirectSpy.calledWith('/task-list')).to.equal(true);
+    //             redirectSpy.restore();
+    //             done();
+    //         }, 500);
+    //     });
+    //
+    //     it('[INTESTACY] PCQ status is DOWN', (done) => {
+    //         const req = {
+    //             session: {
+    //                 caseType: 'intestacy',
+    //                 form: {
+    //                     applicantEmail: 'test@email.com',
+    //                     ccdCase: {
+    //                         id: 1234567890123456
+    //                     }
+    //                 }
+    //             }
+    //         };
+    //         const res = {redirect: () => {
+    //             // Do nothing
+    //         }};
+    //         const redirectSpy = sinon.spy(res, 'redirect');
+    //         const next = sinon.spy();
+    //
+    //         completeEqualityTask(req, res, next);
+    //
+    //         setTimeout(() => {
+    //             expect(req.session.form.equality.pcqId).to.equal('Service down');
+    //             expect(redirectSpy.calledOnce).to.equal(true);
+    //             expect(redirectSpy.calledWith('/summary')).to.equal(true);
+    //             redirectSpy.restore();
+    //             done();
+    //         }, 500);
+    //     });
+    // });
 });
-
-// describe('completeEqualityTask when the service is DOWN', () => {
-//     it('[PROBATE] PCQ status is DOWN', (done) => {
-//         const req = {
-//             session: {
-//                 caseType: 'gop',
-//                 form: {
-//                     applicantEmail: 'test@email.com',
-//                     ccdCase: {
-//                         id: 1234567890123456
-//                     }
-//                 }
-//             }
-//         };
-//         const res = {redirect: () => {
-//             // Do nothing
-//         }};
-//         const redirectSpy = sinon.spy(res, 'redirect');
-//         const next = sinon.spy();
-//
-//         completeEqualityTask(req, res, next);
-//
-//         setTimeout(() => {
-//             expect(req.session.form.equality.pcqId).to.equal('Service down');
-//             expect(redirectSpy.calledOnce).to.equal(true);
-//             expect(redirectSpy.calledWith('/task-list')).to.equal(true);
-//             redirectSpy.restore();
-//             done();
-//         }, 500);
-//     });
-//
-//     it('[INTESTACY] PCQ status is DOWN', (done) => {
-//         const req = {
-//             session: {
-//                 caseType: 'intestacy',
-//                 form: {
-//                     applicantEmail: 'test@email.com',
-//                     ccdCase: {
-//                         id: 1234567890123456
-//                     }
-//                 }
-//             }
-//         };
-//         const res = {redirect: () => {
-//             // Do nothing
-//         }};
-//         const redirectSpy = sinon.spy(res, 'redirect');
-//         const next = sinon.spy();
-//
-//         completeEqualityTask(req, res, next);
-//
-//         setTimeout(() => {
-//             expect(req.session.form.equality.pcqId).to.equal('Service down');
-//             expect(redirectSpy.calledOnce).to.equal(true);
-//             expect(redirectSpy.calledWith('/summary')).to.equal(true);
-//             redirectSpy.restore();
-//             done();
-//         }, 500);
-//     });
-// });
