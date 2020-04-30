675bb133766276337458957464118710114f0cae
Luca Del Buono <luca.delbuono@hmcts.net>
2020-04-30 12:49:25 +0100

    PRO-7216: Fix unit test

--- test/service-stubs/equalityAndDiversityHealth.js
+++ test/service-stubs/equalityAndDiversityHealth.js
@@ -16,10 +16,9 @@ app.get('/health', (req, res) => {
 
 router.get('/info', (req, res) => {
     res.send({
-        'git': {
-            'commit': {
-                'time': '2018-06-05T16:31+0000',
-                'id': 'e210e75b38c6b8da03551b9f83fd909fe80832e4'
+        'buildInfo': {
+            'extra': {
+                'gitCommitId': 'e210e75b38c6b8da03551b9f83fd909fe80832e4'
             }
         }
     });
