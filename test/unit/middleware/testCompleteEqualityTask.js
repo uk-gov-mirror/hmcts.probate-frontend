706aefa91ce2683b10d7364ac3dd7c452cac7e29
Luca Del Buono <luca.delbuono@hmcts.net>
2020-05-04 11:37:36 +0100

    PRO-7216: Add more unit tests

--- test/unit/middleware/testCompleteEqualityTask.js
+++ test/unit/middleware/testCompleteEqualityTask.js
@@ -5,19 +5,17 @@ const sinon = require('sinon');
 const rewire = require('rewire');
 const completeEqualityTask = rewire('app/middleware/completeEqualityTask');
 
-let equalityStub;
-const startStub = () => {
-    equalityStub = require('test/service-stubs/equalityAndDiversityHealth');
-};
-const stopStub = () => {
-    equalityStub.close();
-    delete require.cache[require.resolve('test/service-stubs/equalityAndDiversityHealth')];
-};
-describe('completeEqualityTask', () => {
-    describe('when the service is UP', () => {
-        before(() => startStub());
-
-        it('[PROBATE] PCQ feature toggle is ON', (done) => {
+// let equalityStub;
+// const startStub = () => {
+//     equalityStub = require('test/service-stubs/equalityAndDiversityHealth');
+// };
+// const stopStub = () => {
+//     equalityStub.close();
+//     delete require.cache[require.resolve('test/service-stubs/equalityAndDiversityHealth')];
+// };
+describe.only('completeEqualityTask', () => {
+    describe('PCQ feature toggle is ON', () => {
+        it('should redirect to PCQ', (done) => {
             const params = {
                 isEnabled: true,
                 req: {
@@ -46,10 +44,78 @@ describe('completeEqualityTask', () => {
                 done();
             }, 500);
         });
-
-        after(() => stopStub());
     });
 
+    describe('PCQ feature toggle is OFF', () => {
+        it('[PROBATE] should redirect to Task List', (done) => {
+            const params = {
+                isEnabled: false,
+                req: {
+                    session: {
+                        caseType: 'gop',
+                        form: {
+                            applicantEmail: 'test@email.com',
+                            ccdCase: {
+                                id: 1234567890123456
+                            }
+                        }
+                    }
+                },
+                res: {
+                    redirect: sinon.spy()
+                },
+                next: sinon.spy()
+            };
+
+            completeEqualityTask(params);
+
+            setTimeout(() => {
+                sinon.assert.calledOnce(params.res.redirect);
+                expect(params.res.redirect.calledOnce).to.equal(true);
+                expect(params.res.redirect).to.have.been.calledWith('/task-list');
+
+                done();
+            }, 500);
+        });
+
+        it('[INTESTACY] should redirect to Summary', (done) => {
+            const params = {
+                isEnabled: false,
+                req: {
+                    session: {
+                        caseType: 'intestacy',
+                        form: {
+                            applicantEmail: 'test@email.com',
+                            ccdCase: {
+                                id: 1234567890123456
+                            }
+                        }
+                    }
+                },
+                res: {
+                    redirect: sinon.spy()
+                },
+                next: sinon.spy()
+            };
+
+            completeEqualityTask(params);
+
+            setTimeout(() => {
+                sinon.assert.calledOnce(params.res.redirect);
+                expect(params.res.redirect.calledOnce).to.equal(true);
+                expect(params.res.redirect).to.have.been.calledWith('/summary');
+
+                done();
+            }, 500);
+        });
+    });
+
+    // describe('when the service is UP', () => {
+    //     before(() => startStub());
+    //     WIP
+    //     after(() => stopStub());
+    // });
+    //
     // describe('when the service is DOWN', () => {
     //     it('[PROBATE] PCQ status is DOWN', (done) => {
     //         const req = {
