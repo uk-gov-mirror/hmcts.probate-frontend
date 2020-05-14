f1b9dd57052c6c66c3137e04e0a804bd6a8cec07
Luca Del Buono <luca.delbuono@hmcts.net>
2020-05-14 16:00:58 +0100

    PRO-7216: Prepare for PCQ health check

--- app/middleware/completeEqualityTask.js
+++ app/middleware/completeEqualityTask.js
@@ -8,43 +8,45 @@ const uuidv4 = require('uuid/v4');
 // const logger = require('app/components/logger')('Init');
 
 const completeEqualityTask = (params) => {
-    // const healthcheck = new Healthcheck();
-    // const service = {
-    //     name: config.services.equalityAndDiversity.name,
-    //     url: config.services.equalityAndDiversity.url,
-    //     gitCommitIdPath: config.services.equalityAndDiversity.gitCommitIdPath
-    // };
+    // if (params.isEnabled && !get(params.req.session.form, 'equality.pcqId', false)) {
+    //     const healthcheck = new Healthcheck();
+    //     const service = {
+    //         name: config.services.equalityAndDiversity.name,
+    //         url: config.services.equalityAndDiversity.url,
+    //         gitCommitIdPath: config.services.equalityAndDiversity.gitCommitIdPath
+    //     };
     //
-    // healthcheck.getServiceHealth(service)
-    //     .then(json => {
-    //         params.req.session.equalityHealth = json.status;
-    //         logger.info(config.services.equalityAndDiversity.name, 'is', params.req.session.equalityHealth);
+    //     healthcheck.getServiceHealth(service)
+    //         .then(json => {
+    //             params.req.session.equalityHealth = json.status;
+    //             logger.info(config.services.equalityAndDiversity.name, 'is', params.req.session.equalityHealth);
     //
-    //         const formData = ServiceMapper.map(
-    //             'FormData',
-    //             [config.services.orchestrator.url, params.req.sessionID]
-    //         );
+    //             const formData = ServiceMapper.map(
+    //                 'FormData',
+    //                 [config.services.orchestrator.url, params.req.sessionID]
+    //             );
     //
-    //         if (params.req.session.equalityHealth === 'UP') {
-    //             params.req.session.form.equality = {
-    //                 pcqId: uuidv4()
-    //             };
-    //         } else {
-    //             params.req.session.form.equality = {
-    //                 pcqId: 'Service down'
-    //             };
-    //         }
+    //             if (params.req.session.equalityHealth === 'UP') {
+    //                 params.req.session.form.equality = {
+    //                     pcqId: uuidv4()
+    //                 };
+    //             } else {
+    //                 params.req.session.form.equality = {
+    //                     pcqId: 'Service down'
+    //                 };
+    //             }
     //
-    //         formData.post(params.req.authToken, params.req.session.serviceAuthorization, params.req.session.form.ccdCase.id, params.req.session.form);
+    //             formData.post(params.req.authToken, params.req.session.serviceAuthorization, params.req.session.form.ccdCase.id, params.req.session.form);
     //
-    //         if (params.req.session.equalityHealth === 'UP') {
-    //             params.next();
-    //         } else if (params.req.session.caseType ==='intestacy') {
-    //             params.res.redirect('/summary');
-    //         } else {
-    //             params.res.redirect('/task-list');
-    //         }
-    //     });
+    //             if (params.req.session.equalityHealth === 'UP') {
+    //                 params.next();
+    //             } else if (params.req.session.caseType === 'intestacy') {
+    //                 params.res.redirect('/summary');
+    //             } else {
+    //                 params.res.redirect('/task-list');
+    //             }
+    //         });
+    // }
 
     if (params.isEnabled && !get(params.req.session.form, 'equality.pcqId', false)) {
         const formData = ServiceMapper.map(
