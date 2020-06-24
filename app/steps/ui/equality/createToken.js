cb5078775971574398cd7fc277a82715c6e44989
Bradley Wadsworth <bradley.wadsworth@hmcts.net>
2020-06-24 17:33:21 +0100

    Convert all params to string before encrypting

--- app/steps/ui/equality/createToken.js
+++ app/steps/ui/equality/createToken.js
@@ -14,6 +14,10 @@ const createToken = (params) => {
     if (tokenKey) {
         logger.info(`Using ${tokenKey === 'PROBATE_TOKEN_KEY' ? 'local' : 'Azure KV'} secret for PCQ token key`);
         const key = crypto.scryptSync(tokenKey, 'salt', 32);
+        // Convert all params to string before encrypting
+        Object.keys(params).forEach(p => {
+            params[p] = String(params[p]);
+        });
         const strParams = JSON.stringify(params);
         const cipher = crypto.createCipheriv(algorithm, key, iv);
         encrypted = cipher.update(strParams, 'utf8', 'hex');
