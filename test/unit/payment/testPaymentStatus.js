a0936a2f4c32cc32f3140eead742921d297e9c72
Elviani Huseyin <elviani.huseyin1@hmcts.net>
2020-09-25 18:47:44 +0100

    PRO-126: fix the unit test error

--- test/unit/payment/testPaymentStatus.js
+++ test/unit/payment/testPaymentStatus.js
@@ -135,7 +135,7 @@ describe('PaymentStatus', () => {
             });
             const expectedOptions = {
                 redirect: true,
-                url: '/payment-breakdown?status=failure'
+                url: '/payment-breakdown'
             };
             const session = {
                 form: {}
@@ -209,7 +209,7 @@ describe('PaymentStatus', () => {
             co(function* () {
                 const options = yield paymentStatus.runnerOptions(ctx, session);
                 expect(options.redirect).to.equal(true);
-                expect(options.url).to.equal('/payment-breakdown?status=failure');
+                expect(options.url).to.equal('/payment-breakdown');
                 expect(session.form).to.deep.equal(expectedFormData);
                 revert();
                 done();
