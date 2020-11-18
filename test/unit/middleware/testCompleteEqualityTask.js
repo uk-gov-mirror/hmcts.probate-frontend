b609d84163093766f66474baf6c0f5bac12ea60c
Elviani Huseyin <elviani.huseyin1@hmcts.net>
2020-11-18 16:18:34 +0000

    PRO-893: fixing the unit test

--- test/unit/middleware/testCompleteEqualityTask.js
+++ test/unit/middleware/testCompleteEqualityTask.js
@@ -7,126 +7,118 @@ const sinonChai = require('sinon-chai');
 chai.use(sinonChai);
 const rewire = require('rewire');
 const completeEqualityTask = rewire('app/middleware/completeEqualityTask');
-// const app = require('express')();
-// const healthcheck = require('@hmcts/nodejs-healthcheck');
 
-// let equalityStub;
-// let appStub;
-// let healthcheckStub;
-// const startStub = () => {
-//     equalityStub = require('test/service-stubs/equalityAndDiversityHealth');
-// };
-// const stopStub = () => {
-//     equalityStub.close();
-//     delete require.cache[require.resolve('test/service-stubs/equalityAndDiversityHealth')];
-// };
+let equalityStub;
+const startStub = () => {
+    equalityStub = require('test/service-stubs/equalityAndDiversityHealth');
+};
+const stopStub = () => {
+    equalityStub.close();
+    delete require.cache[require.resolve('test/service-stubs/equalityAndDiversityHealth')];
+};
 describe('completeEqualityTask', () => {
-    // describe('PCQ feature toggle is ON and health is UP', () => {
-    //     before(() => {
-    //         startStub();
-    //         appStub = sinon.stub(app , 'get');
-    //         healthcheckStub = sinon.stub(healthcheck, 'addTo');
-    //     });
+    describe('PCQ feature toggle is ON and health is UP', () => {
+        before(() => startStub());
 
-    //     it('should redirect to PCQ', (done) => {
-    //         const params = {
-    //             isEnabled: true,
-    //             req: {
-    //                 session: {
-    //                     caseType: 'gop',
-    //                     form: {
-    //                         applicantEmail: 'test@email.com',
-    //                         ccdCase: {
-    //                             id: 1234567890123456
-    //                         }
-    //                     }
-    //                 }
-    //             },
-    //             res: {
-    //                 locals: {launchDarkly: {}},
-    //                 redirect: () => {
-    //                     // Do nothing
-    //                 }
-    //             },
-    //             next: sinon.spy()
-    //         };
+        it('should redirect to PCQ', (done) => {
+            const params = {
+                isEnabled: true,
+                req: {
+                    session: {
+                        caseType: 'gop',
+                        form: {
+                            applicantEmail: 'test@email.com',
+                            ccdCase: {
+                                id: 1234567890123456
+                            }
+                        }
+                    }
+                },
+                res: {
+                    locals: {launchDarkly: {}},
+                    redirect: () => {
+                        // Do nothing
+                    }
+                },
+                next: sinon.spy()
+            };
 
-    //         completeEqualityTask(params);
+            completeEqualityTask(params);
 
-    //         setTimeout(() => {
-    //             expect(params.req.session.form.equality.pcqId).to.not.equal('Service down');
-    //             expect(params.next.calledOnce).to.equal(true);
+            setTimeout(() => {
+                expect(params.req.session.form.equality.pcqId).to.not.equal('Service down');
+                expect(params.next.calledOnce).to.equal(true);
 
-    //             done();
-    //         }, 500);
-    //     });
+                done();
+            }, 500);
+        });
 
-    //     after(() => stopStub());
-    // });
+        after(() => stopStub());
+    });
 
-    // describe('PCQ feature toggle is ON and health is DOWN', () => {
-    //     it('[PROBATE] should redirect to Task List', (done) => {
-    //         const params = {
-    //             isEnabled: true,
-    //             req: {
-    //                 session: {
-    //                     caseType: 'gop',
-    //                     form: {
-    //                         applicantEmail: 'test@email.com',
-    //                         ccdCase: {
-    //                             id: 1234567890123456
-    //                         }
-    //                     }
-    //                 }
-    //             },
-    //             res: {
-    //                 redirect: sinon.spy()
-    //             },
-    //             next: sinon.spy()
-    //         };
+    describe('PCQ feature toggle is ON and health is DOWN', () => {
+        it('[PROBATE] should redirect to Task List', (done) => {
+            const params = {
+                isEnabled: true,
+                req: {
+                    session: {
+                        caseType: 'gop',
+                        form: {
+                            applicantEmail: 'test@email.com',
+                            ccdCase: {
+                                id: 1234567890123456
+                            }
+                        }
+                    }
+                },
+                res: {
+                    redirect: sinon.spy()
+                },
+                next: sinon.spy()
+            };
 
-    //         completeEqualityTask(params);
+            completeEqualityTask(params);
 
-    //         setTimeout(() => {
-    //             sinon.assert.calledOnce(params.res.redirect);
-    //             expect(params.res.redirect.calledOnce).to.equal(true);
-    //             expect(params.res.redirect).to.have.been.calledWith('/task-list');
+            setTimeout(() => {
+                sinon.assert.calledOnce(params.res.redirect);
+                expect(params.res.redirect.calledOnce).to.equal(true);
+                expect(params.res.redirect).to.have.been.calledWith('/task-list');
 
-    //             done();
-    //         }, 2500);
-    //     });
+                done();
+            }, 2500);
+        });
 
-    //     it('[INTESTACY] should redirect to Summary', (done) => {
-    //         const params = {
-    //             isEnabled: true,
-    //             req: {
-    //                 session: {
-    //                     caseType: 'intestacy',
-    //                     form: {
-    //                         applicantEmail: 'test@email.com',
-    //                         ccdCase: {
-    //                             id: 1234567890123456
-    //                         }
-    //                     }
-    //                 }
-    //             },
-    //             res: {
-    //                 redirect: sinon.spy()
-    //             },
-    //             next: sinon.spy()
-    //         };
+        it('[INTESTACY] should redirect to Summary', (done) => {
+            const params = {
+                isEnabled: true,
+                req: {
+                    session: {
+                        caseType: 'intestacy',
+                        form: {
+                            applicantEmail: 'test@email.com',
+                            ccdCase: {
+                                id: 1234567890123456
+                            }
+                        }
+                    }
+                },
+                res: {
+                    redirect: sinon.spy()
+                },
+                next: sinon.spy()
+            };
 
-    //         completeEqualityTask(params);
+            completeEqualityTask(params);
 
-    //         setTimeout(() => {
-    //             sinon.assert.calledOnce(params.res.redirect);
-    //             expect(params.res.redirect.calledOnce).to.equal(true);
-    //             expect(params.res.redirect).to.have.been.calledWith('/summary');
+            setTimeout(() => {
+                sinon.assert.calledOnce(params.res.redirect);
+                expect(params.res.redirect.calledOnce).to.equal(true);
+                expect(params.res.redirect).to.have.been.calledWith('/summary');
 
-    //             done();
-    //         }, 2500);
-    //     });
-    // });
+                done();
+            }, 2500);
+        });
+    });
 
     describe('PCQ feature toggle is OFF', () => {
         it('[PROBATE] should redirect to Task List', (done) => {
