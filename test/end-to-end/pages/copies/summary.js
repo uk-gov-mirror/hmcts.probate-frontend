85785b442d0c9fd8c45f0d61eedc6202a3956a7a
Rob Doherty <robdohertykelly@gmail.com>
2020-12-11 17:48:27 +0000

    DTSPB-999: add waitForText() step to test/end-to-end/pages/copies/summary.js to improve test stability

--- test/end-to-end/pages/copies/summary.js
+++ test/end-to-end/pages/copies/summary.js
@@ -1,9 +1,13 @@
 'use strict';
 
+const config = require('config');
+const content = require('app/resources/en/translation/copies/summary');
+
 module.exports = async function() {
     const I = this;
 
     await I.checkPageUrl('app/steps/ui/copies/summary');
+    await I.waitForText(content.extraCopies, config.TestWaitForTextToAppear);
     const locator = {css: '.govuk-button'};
     await I.waitForElement(locator);
     await I.navByClick(locator);
