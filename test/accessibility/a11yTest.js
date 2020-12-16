b0aee6a6d537ec881a997156eac2250aab6a05f8
PhaniP <phani.perla@hmcts.net>
2020-12-16 15:30:32 +0000

    DTSPB-1073-Welsh Accessibility Tests

--- test/accessibility/a11yTest.js
+++ test/accessibility/a11yTest.js
@@ -7,10 +7,11 @@ const a11y = require('test/util/a11y');
 const expect = require('chai').expect;
 const initSteps = require('app/core/initSteps');
 const {endsWith, merge} = require('lodash');
-const commonContent = {
-    en: require('app/resources/en/translation/common')
-};
+const commonContentEn = require('app/resources/en/translation/common');
+const commonContentCy = require('app/resources/cy/translation/common');
+const languages = ['en', 'cy'];
 const caseTypes = require('app/utils/CaseTypes');
+
 const stepsToExclude = [
     'Dashboard', 'Summary', 'TaskList', 'Equality', 'PinPage', 'PinSent', 'PinResend', 'AddressLookup', 'ExecutorAddress', 'ExecutorContactDetails', 'ExecutorName',
     'ExecutorNotified', 'ExecutorNameAsOnWill', 'ExecutorApplying', 'DeleteExecutor', 'PaymentStatus', 'AddAlias', 'RemoveAlias', 'ExecutorRoles', 'ExecutorsWhenDied'
@@ -38,113 +39,117 @@ Object.keys(steps)
 
 const numSteps = Object.keys(steps).length;
 
-for (const step in steps) {
-    ((step) => {
-        const stepUrl = step.constructor.getUrl();
-        const stepUrlFirstSegment = '/' + stepUrl.split('/')[1];
-        let results;
-        let sessionData = {};
+const runTests = (language ='en') => {
 
-        if (config.whitelistedPagesAfterSubmission.includes(stepUrlFirstSegment)) {
-            sessionData = merge(commonSessionData, {
-                form: {
-                    declaration: {
-                        declarationCheckbox: 'true'
-                    },
-                    payment: {
-                        total: 0
-                    },
-                    ccdCase: {
-                        state: 'CaseCreated'
+    for (const step in steps) {
+        ((step) => {
+            const stepUrl = step.constructor.getUrl();
+            const stepUrlFirstSegment = '/' + stepUrl.split('/')[1];
+            let results;
+            let sessionData = {};
+
+            if (config.whitelistedPagesAfterSubmission.includes(stepUrlFirstSegment)) {
+                sessionData = merge(commonSessionData, {
+                    form: {
+                        declaration: {
+                            declarationCheckbox: 'true'
+                        },
+                        payment: {
+                            total: 0
+                        },
+                        ccdCase: {
+                            state: 'CaseCreated'
+                        }
                     }
-                }
-            });
-        } else if (config.whitelistedPagesAfterDeclaration.includes(stepUrlFirstSegment)) {
-            sessionData = merge(commonSessionData, {
-                form: {
-                    declaration: {
-                        declarationCheckbox: 'true'
+                });
+            } else if (config.whitelistedPagesAfterDeclaration.includes(stepUrlFirstSegment)) {
+                sessionData = merge(commonSessionData, {
+                    form: {
+                        declaration: {
+                            declarationCheckbox: 'true'
+                        }
                     }
-                }
-            });
-        }
-
-        if (config.gopOnlyPages.includes(stepUrlFirstSegment)) {
-            sessionData = merge(sessionData, {
-                form: {
-                    type: caseTypes.GOP
-                },
-                back: []
-            });
-        } else if (config.intestacyOnlyPages.includes(stepUrlFirstSegment)) {
-            sessionData = merge(sessionData, {
-                form: {
-                    type: caseTypes.INTESTACY
-                },
-                back: []
-            });
-        }
-
-        describe(`Verify accessibility for the page ${step.name}`, () => {
-            let server = null;
-            let agent = null;
-            let httpTerminator = null;
-            let title;
-
-            if (step.name === 'Declaration' || step.name === 'CoApplicantDeclaration') {
-                title = `${step.content.en.title} - ${commonContent.en.serviceName}`
-                    .replace(/&lsquo;/g, '‘')
-                    .replace(/&rsquo;/g, '’');
-            } else {
-                title = `${step.content.title} - ${commonContent.en.serviceName}`
-                    .replace(/&lsquo;/g, '‘')
-                    .replace(/&rsquo;/g, '’');
+                });
             }
 
-            before((done) => {
-                nock(config.services.orchestrator.url)
-                    .get('/invite/allAgreed/undefined')
-                    .reply(200, 'false');
+            if (config.gopOnlyPages.includes(stepUrlFirstSegment)) {
+                sessionData = merge(sessionData, {
+                    form: {
+                        type: caseTypes.GOP
+                    },
+                    back: []
+                });
+            } else if (config.intestacyOnlyPages.includes(stepUrlFirstSegment)) {
+                sessionData = merge(sessionData, {
+                    form: {
+                        type: caseTypes.INTESTACY
+                    },
+                    back: []
+                });
+            }
 
-                const app = require('app');
-                server = app.init(true, sessionData);
-                httpTerminator = createHttpTerminator({server: server.http});
+            describe(`Verify accessibility for the page ${step.name} - ${language}`, () => {
+                let server = null;
+                let agent = null;
+                let httpTerminator = null;
+                let title;
+                const commonContent = language === 'en' ? commonContentEn : commonContentCy;
 
-                agent = request.agent(server.app);
-                co(function* () {
-                    let urlSuffix = '';
-                    if (endsWith(agent.get(stepUrl), '*')) {
-                        urlSuffix = '/0';
-                    }
-                    results = yield a11y(agent.get(stepUrl).url + urlSuffix, title);
-                })
-                    .then(done, done)
-                    .catch((error) => {
-                        done(error);
-                    });
+                if (step.name === 'Declaration' || step.name === 'CoApplicantDeclaration') {
+                    title = `${step.content.title} - ${commonContent.serviceName}`
+                        .replace(/&lsquo;/g, '‘')
+                        .replace(/&rsquo;/g, '’');
+                } else {
+                    title = `${step.content.title} - ${commonContent.serviceName}`
+                        .replace(/&lsquo;/g, '‘')
+                        .replace(/&rsquo;/g, '’');
+                }
+
+                before((done) => {
+                    nock(config.services.orchestrator.url)
+                        .get('/invite/allAgreed/undefined')
+                        .reply(200, 'false');
+
+                    const app = require('app');
+                    server = app.init(true, sessionData);
+                    httpTerminator = createHttpTerminator({server: server.http});
+
+                    agent = request.agent(server.app);
+                    co(function* () {
+                        let urlSuffix = '';
+                        if (endsWith(agent.get(`${stepUrl}?lng=${language}`), '*')) {
+                            urlSuffix = '/0';
+                        }
+                        results = yield a11y(agent.get(`${stepUrl}?lng=${language}`).url + urlSuffix, title);
+                    })
+                        .then(done, done)
+                        .catch((error) => {
+                            done(error);
+                        });
+                });
+
+                after(async () => {
+                    nock.cleanAll();
+                    await httpTerminator.terminate();
+                    stepNum += 1;
+                });
+
+                it('should not generate any errors', () => {
+                    const errors = results.issues.filter((res) => res.type === 'error');
+
+                    expect(errors.length).to.equal(0, JSON.stringify(errors, null, 2));
+                });
+
+                it('should not generate any warnings', () => {
+                    const warnings = results.issues.filter((res) => res.type === 'warning');
+
+                    expect(warnings.length).to.equal(0, JSON.stringify(warnings, null, 2));
+                });
             });
+        })(steps[step]);
+    }
 
-            after(async () => {
-                nock.cleanAll();
-                await httpTerminator.terminate();
-                stepNum += 1;
-            });
-
-            it('should not generate any errors', () => {
-                const errors = results.issues.filter((res) => res.type === 'error');
-
-                expect(results.documentTitle).to.equal(title);
-                expect(errors.length).to.equal(0, JSON.stringify(errors, null, 2));
-            });
-
-            it('should not generate any warnings', () => {
-                const warnings = results.issues.filter((res) => res.type === 'warning');
-
-                expect(warnings.length).to.equal(0, JSON.stringify(warnings, null, 2));
-            });
-        });
-    })(steps[step]);
-}
+};
 
 /*
 setTimeout(function () {
@@ -159,3 +164,7 @@ setInterval(() => {
         process.exit();
     }
 }, 10000);
+
+languages.forEach(language => {
+    runTests(language);
+});
