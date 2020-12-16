1a6880ad60180ad1ae52cbd82a9d2457e7e22341
Rob Doherty <robdohertykelly@gmail.com>
2020-12-16 11:59:49 +0000

    DTSPB-999: better functional e2e test waiting for IDAM page to correctly load

--- test/end-to-end/pages/IDAM/signIn.js
+++ test/end-to-end/pages/IDAM/signIn.js
@@ -18,7 +18,7 @@ module.exports = async function (noScreenerQuestions = false) {
             await I.navByClick({css: 'a[href="/dashboard"]'});
         }
 
-        await I.waitForText('Sign in', testConfig.TestWaitForTextToAppear);
+        await I.waitForText('Sign in', testConfig.TestWaitForTextToAppear, 'h1');
         await I.fillField('username', process.env.testCitizenEmail);
         await I.fillField('password', process.env.testCitizenPassword);
 
