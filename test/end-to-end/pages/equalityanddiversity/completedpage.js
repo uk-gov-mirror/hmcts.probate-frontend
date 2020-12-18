962d7302896a9bbac5680145318734213add0881
Rob Doherty <robdohertykelly@gmail.com>
2020-12-18 09:45:20 +0000

    DTSPB-1085: improved element waiting for crossbrowser tests

--- test/end-to-end/pages/equalityanddiversity/completedpage.js
+++ test/end-to-end/pages/equalityanddiversity/completedpage.js
@@ -5,5 +5,6 @@ const commonContent = require('app/resources/en/translation/common');
 module.exports = async function() {
     const I = this;
     await I.wait(3);
+    await I.waitForText(commonContent.saveAndContinue);
     await I.navByClick(commonContent.saveAndContinue);
 };
