84229ba0140c6735a7b2655a79fd889148aa59d5
Rob Doherty <robdohertykelly@gmail.com>
2020-12-18 15:31:00 +0000

    DTSPB-1085: improved waiting for elements in e2e functional and crossbrowser tests

--- test/end-to-end/pages/coapplicant/startPage.js
+++ test/end-to-end/pages/coapplicant/startPage.js
@@ -1,8 +1,12 @@
 'use strict';
 
+const config = require('config');
+const content = require('app/resources/en/translation/coapplicant/startpage');
+
 module.exports = async function() {
     const I = this;
 
     await I.checkPageUrl('app/steps/ui/coapplicant/startpage');
+    await I.waitForText(content.subHeader1, config.TestWaitForTextToAppear);
     await I.navByClick('.govuk-button');
 };
--- test/end-to-end/pages/dashboard/chooseApplication.js
+++ test/end-to-end/pages/dashboard/chooseApplication.js
@@ -2,22 +2,22 @@
 'use strict';
 
 const content = require('app/resources/en/translation/dashboard');
+const testConfig = require('config');
 
 module.exports = async function() {
     const I = this;
 
     await I.checkPageUrl('app/steps/ui/dashboard');
-    await I.waitForText(content.header);
 
     // we do need to allow refreshing the page here as it takes time to populate ccd, and storing data in the ccd
     // database gives a success before is actually populated, so is async.
     for (let i = 0; i <= 5; i++) {
-        const result = await I.checkForText('Continue application');
+        await I.waitForText(content.header, testConfig.TestWaitForTextToAppear);
+        const result = await I.checkForText('Continue application', 10);
         if (result === true) {
             break;
         }
         await I.refreshPage();
-        await I.wait(3);
     }
 
     await I.see(content.tableHeaderCcdCaseId);
