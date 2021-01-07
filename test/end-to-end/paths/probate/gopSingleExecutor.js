524e49b09d81fcb1ffdf09fc5a4ed79556a49d60
PhaniP <phani.perla@hmcts.net>
2021-01-07 16:35:06 +0000

    DTSPB-1063-GOP-Welsh-E2E

--- test/end-to-end/paths/probate/gopSingleExecutor.js
+++ test/end-to-end/paths/probate/gopSingleExecutor.js
@@ -9,9 +9,9 @@ const optionNo = '-2';
 const bilingualGOP = false;
 const uploadingDocuments = false;
 const config = require('config');
-const languages = ['cy'];
+const languages = ['en', 'cy'];
 
-Feature('GOP-Single Executor').retry(0);
+Feature('GOP-Single Executor').retry(TestConfigurator.getRetryFeatures());
 
 languages.forEach(language => {
 
@@ -144,7 +144,7 @@ languages.forEach(language => {
         // Thank You
         await I.seeThankYouPage(language);
     }).tag('@e2e')
-        .retry(0);
+        .retry(2);
 
     Before(async () => {
         await TestConfigurator.initLaunchDarkly();
