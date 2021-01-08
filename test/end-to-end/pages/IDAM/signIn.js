eff68c8107aa69ffbe1df2a4ab1bb16fd1e0753d
PhaniP <phani.perla@hmcts.net>
2021-01-08 16:24:30 +0000

    DTSPB-1063 GOP Welsh Tests

--- test/end-to-end/pages/IDAM/signIn.js
+++ test/end-to-end/pages/IDAM/signIn.js
@@ -22,7 +22,6 @@ module.exports = async function (language ='en', noScreenerQuestions = false) {
             await I.navByClick({css: 'a[href="/dashboard"]'});
         }
 
-        await I.waitForText('Sign in', testConfig.TestWaitForTextToAppear, 'h1');
         await I.fillField('username', process.env.testCitizenEmail);
         await I.fillField('password', process.env.testCitizenPassword);
         await I.navByClick(commonContent.signIn);
