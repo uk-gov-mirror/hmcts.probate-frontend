9913491b2af75e31bb9fca8e0fb249e845b8dbd0
Mariana <maryanasoad@hotmail.com>
2021-01-18 08:44:37 +0000

    wait for text added

--- test/end-to-end/pages/equalityanddiversity/continueToQuestions.js
+++ test/end-to-end/pages/equalityanddiversity/continueToQuestions.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const config = require('config');
 const commonContentEn = require('app/resources/en/translation/common');
 const commonContentCy = require('app/resources/cy/translation/common');
 
@@ -9,6 +10,6 @@ module.exports = async function(language ='en') {
 
     await I.wait(3);
 
-    await I.seeInCurrentUrl('/start-page');
+    await I.waitForText('Equality and diversity questions', config.TestWaitForTextToAppear);
     await I.navByClick(commonContent.continueToQuestions);
 };
