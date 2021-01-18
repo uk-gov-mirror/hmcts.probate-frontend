8f2b6670fe605885ad348f11f15a955d77fd89f2
PhaniP <phani.perla@hmcts.net>
2021-01-18 22:03:12 +0000

    DTSPB-1063- Revert back Moch library versions

--- test/end-to-end/pages/tasklist/tasklist.js
+++ test/end-to-end/pages/tasklist/tasklist.js
@@ -5,6 +5,5 @@ module.exports = async function () {
     const I = this;
     await I.checkPageUrl('app/steps/ui/tasklist');
     const locator = {css: '.govuk-button'};
-    await I.waitForElement(locator);
     await I.click(locator);
 };
