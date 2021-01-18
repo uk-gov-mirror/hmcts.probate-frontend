b1e90b33aa1fae5b7fb54d61260711a2cb82412b
Mariana <maryanasoad@hotmail.com>
2021-01-18 14:47:07 +0000

    added single executor jouney

--- test/end-to-end/pages/equalityanddiversity/startpage.js
+++ test/end-to-end/pages/equalityanddiversity/startpage.js
@@ -12,6 +12,7 @@ module.exports = async function() {
 
     // await I.checkPageUrl('app/steps/ui/equality');
     await I.waitForText('Equality and diversity questions', config.TestWaitForTextToAppear);
+    await I.takeScreenshot();
     const backButtonLocator = {css: '#back-button'};
     await I.waitForElement(backButtonLocator, config.TestWaitForElementToAppear);
     await I.navByClick(backButtonLocator);
