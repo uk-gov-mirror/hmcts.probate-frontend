77fcdacb162683499cb13ff122d2dc40622de8e3
Mariana <maryanasoad@hotmail.com>
2021-01-18 13:20:04 +0000

    added screenshot

--- test/end-to-end/pages/equalityanddiversity/continueToQuestions.js
+++ test/end-to-end/pages/equalityanddiversity/continueToQuestions.js
@@ -1,15 +1,16 @@
 'use strict';
 
 const config = require('config');
-const commonContentEn = require('app/resources/en/translation/common');
-const commonContentCy = require('app/resources/cy/translation/common');
+//const commonContentEn = require('app/resources/en/translation/common');
+//const commonContentCy = require('app/resources/cy/translation/common');
 
-module.exports = async function(language ='en') {
+module.exports = async function(/*language ='en'*/) {
     const I = this;
-    const commonContent = language === 'en' ? commonContentEn : commonContentCy;
+    //const commonContent = language === 'en' ? commonContentEn : commonContentCy;
 
     await I.wait(3);
 
     await I.waitForText('Equality and diversity questions', config.TestWaitForTextToAppear);
-    await I.navByClick(commonContent.continueToQuestions);
+    await I.takeScreenshot();
+    await I.navByClick({xpath: '//*[@id="main-content"]/div/div/form/ul/li[1]/button'});
 };
