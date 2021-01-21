5b8af539e47adb85b3fb9399a03eb42855187e6c
Rob Doherty <robdohertykelly@gmail.com>
2021-01-21 14:58:13 +0000

    DTSPB-1248: further changes needed to make multipleExecutors.js and intestacy.en.test.js e2e tests run in Welsh language.
    Includes:
    - fixes for checkPageUrl() references

--- test/end-to-end/pages/signout/signout.js
+++ test/end-to-end/pages/signout/signout.js
@@ -8,7 +8,7 @@ module.exports = async function(language = 'en') {
     const I = this;
     const signOutContent = language === 'en' ? signOutContentEn : signOutContentCy;
 
-    await I.checkPageUrl('app/steps/ui/sign-out');
+    await I.checkPageUrl('app/steps/ui/signout');
     await I.waitForText(signOutContent.header, testConfig.TestWaitForTextToAppear);
     const locator = {css: '#main-content > div > div > p:nth-child(3) > a'};
     await I.waitForElement(locator);
