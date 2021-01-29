e4722ff9de6e026f496fcf4db5302c9cbc774992
Rob Doherty <robdohertykelly@gmail.com>
2021-01-29 15:23:29 +0000

    DTSPB-1308: make deathcertificate.js e2e test step wait for tab to open when testSurvey is true

--- test/end-to-end/pages/screeners/deathcertificate.js
+++ test/end-to-end/pages/screeners/deathcertificate.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-await-in-loop */
 'use strict';
 
 const commonContentEn = require('app/resources/en/translation/common');
@@ -10,7 +11,15 @@ module.exports = async function(language ='en', answer, testSurvey = false) {
     await I.waitForText(commonContent.yes);
 
     if (testSurvey) {
+        const originalTabs = await I.grabNumberOfOpenTabs();
         await I.click({css: 'body > div.govuk-width-container > div > p > span > a:nth-child(1)'});
+        for (let i = 0; i <= 5; i++) {
+            const currentTabs = await I.grabNumberOfOpenTabs();
+            if (currentTabs > originalTabs) {
+                break;
+            }
+            await I.wait(1);
+        }
         await I.switchToNextTab(1);
         // running locally I get no internet here so have commented ths, but at least we've proved we've opened a new tab.
         // await I.waitForVisible({css: '#cmdGo'});
