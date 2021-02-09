28160b08d341a2a9bbbabd4570e1c5e46b2a1fe6
Arun <arun.kumar@hmcts.net>
2021-02-09 17:32:35 +0000

    DTSPB-1412-Fixed Flaky-Test

--- test/end-to-end/pages/language/bilingualgrant.js
+++ test/end-to-end/pages/language/bilingualgrant.js
@@ -4,13 +4,12 @@ const contentEn = require('app/resources/en/translation/common');
 const contentCy = require('app/resources/cy/translation/common');
 
 // eslint-disable-next-line no-unused-vars
-module.exports = async function(language = 'en', answer) {
+module.exports = async function(language = 'en') {
     const I = this;
     const commonContent = language === 'en' ? contentEn : contentCy;
-
     await I.checkPageUrl('app/steps/ui/language');
     const locator = {css: '#bilingual-2'};
-    await I.waitForEnabled(locator);
+    await I.waitForEnabled(locator, 3);
     await I.seeCheckboxIsChecked(locator);
     await I.navByClick(commonContent.saveAndContinue);
 };
