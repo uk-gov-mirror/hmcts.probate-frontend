6190b79cdf3193ee54f1dda32bb5a4d7449f1e3b
Arun <arun.kumar@hmcts.net>
2021-02-09 15:12:25 +0000

    DTSPB-1412 Fixed the flaky E2E test

--- test/end-to-end/pages/language/bilingualgrant.js
+++ test/end-to-end/pages/language/bilingualgrant.js
@@ -3,12 +3,13 @@
 const contentEn = require('app/resources/en/translation/common');
 const contentCy = require('app/resources/cy/translation/common');
 
+// eslint-disable-next-line no-unused-vars
 module.exports = async function(language = 'en', answer) {
     const I = this;
     const commonContent = language === 'en' ? contentEn : contentCy;
 
     await I.checkPageUrl('app/steps/ui/language');
-    const locator = {css: `#bilingual${answer}`};
+    const locator = {css: '#bilingual-2'};
     await I.waitForEnabled(locator);
     await I.seeCheckboxIsChecked(locator);
     await I.navByClick(commonContent.saveAndContinue);
