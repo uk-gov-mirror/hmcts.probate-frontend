ce1dd327e8901c0d2da82929d4e1a5bc95043f56
Megan <meganaliceslattery@gmail.com>
2021-03-10 13:27:50 +0000

    Use lodash get Amend

--- app/utils/ScreenerValidation.js
+++ app/utils/ScreenerValidation.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const config = require('config');
+const {get} = require('lodash');
 
 class ScreenerValidation {
 
     getScreeners(journeyType, formdata) {
-        const deathCertificateNotInEnglish = formdata && formdata.screeners && formdata.screeners.deathCertificateInEnglish ? formdata.screeners.deathCertificateInEnglish === 'optionNo' : false;
+        const deathCertificateNotInEnglish = get(formdata, 'screeners.deathCertificateInEnglish') ? formdata.screeners.deathCertificateInEnglish === 'optionNo' : false;
 
         return deathCertificateNotInEnglish ? config[`${journeyType}ScreenersDeathCertificateNotInEnglish`] : config[`${journeyType}ScreenersDeathCertificateInEnglish`];
     }
