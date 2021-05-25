c51dda60cabe70ec0cc0a9b410e77d7fae97b563
Elviani Huseyin <elviani.huseyin1@hmcts.net>
2021-05-25 15:02:18 +0100

    DTSPB-1670: fixing the postcode issues

--- app/routes.js
+++ app/routes.js
@@ -30,7 +30,9 @@ const FormatUrl = require('app/utils/FormatUrl');
 router.all('*', (req, res, next) => {
     req.log = logger(req.sessionID);
     req.log.info(`Processing ${req.method} for ${req.originalUrl}`);
-    next();
+    if (!req.originalUrl.match(/public/g)) {
+        next();
+    }
 });
 
 router.use((req, res, next) => {
