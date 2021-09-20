231f1cbd98ab93278b6edbca7d2726a494459648
itoulou <touloumbadjiani@gmail.com>
2021-09-20 16:21:41 +0100

    add next step options

--- app/steps/ui/will/willhasvisibledamage/index.js
+++ app/steps/ui/will/willhasvisibledamage/index.js
@@ -7,6 +7,14 @@ class WillHasVisibleDamage extends ValidationStep {
     static getUrl() {
         return '/will-has-damage';
     }
+
+    nextStepOptions() {
+        return {
+            options: [
+                {key: 'willHasVisibleDamage', value: 'optionYes', choice: 'willDoesHaveVisibleDamage'},
+            ]
+        };
+    }
 }
 
 module.exports = WillHasVisibleDamage;
