9152da60e719238047a11177dde4da3fac3283f4
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2021-11-17 16:37:46 +0000

    DTSPB-2454: use date objects in ee check

--- app/utils/ExceptedEstateDod.js
+++ app/utils/ExceptedEstateDod.js
@@ -7,7 +7,7 @@ class ExceptedEstateDod {
         if (!date) {
             throw new TypeError('no deceased date date of death found');
         }
-        return date >= config.exceptedEstateDateOfDeath;
+        return new Date(date).getTime() >= new Date(config.exceptedEstateDateOfDeath).getTime();
     }
 }
 
