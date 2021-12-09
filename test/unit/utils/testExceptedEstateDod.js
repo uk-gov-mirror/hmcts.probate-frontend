de79282e02efa1e589c1738857feb3010a4e1b8b
isha2040 <58481974+isha2040@users.noreply.github.com>
2021-12-09 09:07:17 +0000

    Dtspb 2500 move dod ee to env var (#1507)
    * DTSPB-2500 Add Dod for EE as a secret with default value of 2022-01-01
    
    * Bumping chart version
    
    * Added a deafult value to the secret
    
    * Added changes for secret
    
    * fix yarn lint
    
    * Correct the way vault secret is passed
    
    * hardcoded the dod for component test
    
    * Add the secret in setUpSecrets.js
    
    * Remove the secret from default.yml compeltely
    
    * Add secret explicitly in main.tf also removed the field from testing-component.yaml
    
    * Revert the main.tf change
    
    * Introduced Hierarchy to see if the secret gets picked
    
    * Correct indentation
    
    * Add the secret in values preview yaml
    
    * Add test
    
    * Add test
    
    * Add log
    
    * Replaced - to _
    
    * Remove changes to have this as a secret but only env var
    
    * Remove secret related code
    
    * Remove the console logs and commented code
    
    * Remove hierarchy
    
    * Remove extra line from default.yaml
    
    Co-authored-by: hmcts-jenkins-probate <62423138+hmcts-jenkins-probate[bot]@users.noreply.github.com>
    Co-authored-by: Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>

---
+++ test/unit/utils/testExceptedEstateDod.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+describe('ExceptedEstateDod.js', () => {
+    beforeEach(() => {
+        delete require.cache[require.resolve('app/utils/ExceptedEstateDod')];
+        delete require.cache[require.resolve('config')];
+    });
+
+    describe('test', () => {
+        it('should return true if dod > threshold ', (done) => {
+            const ExceptedEstateDod = require('app/utils/ExceptedEstateDod');
+            const date = new Date('2022-01-01').getTime();
+            const result = ExceptedEstateDod.afterEeDodThreshold(date);
+            expect(result).to.equal(true);
+            done();
+        });
+
+        it('should return false if dod < threshold ', (done) => {
+            const ExceptedEstateDod = require('app/utils/ExceptedEstateDod');
+            const date = new Date('2021-01-01').getTime();
+            const result = ExceptedEstateDod.afterEeDodThreshold(date);
+            expect(result).to.equal(false);
+            done();
+        });
+
+        it('should override threshold from env var ', (done) => {
+            process.env.EXCEPTED_ESTATE_DATE_OF_DEATH = '2021-01-01';
+            const ExceptedEstateDod = require('app/utils/ExceptedEstateDod');
+            const date = new Date('2021-06-06').getTime();
+            const result = ExceptedEstateDod.afterEeDodThreshold(date);
+            expect(result).to.equal(true);
+            delete process.env.EXCEPTED_ESTATE_DATE_OF_DEATH;
+            done();
+        });
+
+    });
+});
