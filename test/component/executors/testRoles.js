256fe2f64caa9f94925bfb80a591ff970e1eb3a6
AbdullahShahHMCTS <77341369+AbdullahShahHMCTS@users.noreply.github.com>
2022-05-30 10:08:05 +0100

    Dtspb 2804 opening times update (#1657)
    * DTSPB-2775_Webchat_Center_Closed
    
    * DTSPB-2775_Webchat_Center_Closed changed openinghours
    
    * welsh
    
    * suppress

--- test/component/executors/testRoles.js
+++ test/component/executors/testRoles.js
@@ -63,8 +63,6 @@ describe('executor-roles', () => {
                         helpTelephoneOpeningHoursTitle: commonContent.helpTelephoneOpeningHoursTitle,
                         helpTelephoneOpeningHours1: commonContent.helpTelephoneOpeningHours1,
                         helpTelephoneOpeningHours2: commonContent.helpTelephoneOpeningHours2,
-                        helpTelephoneOpeningHours3: commonContent.helpTelephoneOpeningHours3,
-                        helpTelephoneOpeningHours4: commonContent.helpTelephoneOpeningHours4,
                         helpEmailLabel: commonContent.helpEmailLabel.replace(/{contactEmailAddress}/g, commonContent.helpEmail)
                     };
 
