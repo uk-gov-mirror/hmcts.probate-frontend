4672d6d3b0c066c2e6dc89dafbb0f7c37860e1b2
AbdullahShahHMCTS <77341369+AbdullahShahHMCTS@users.noreply.github.com>
2022-08-01 19:57:42 +0100

    Dtspb 2942 webchat 180 minutes ewt (#1689)
    * DTSPB-2942 - wait time increase
    
    * DTSPB-2942 - changed user text
    
    * DTSPB-2942 - changed if clause and commented
    
    * fix comments
    
    * Update avaya-webchat-2.js
    
    put back lost condition
    
    * Update avaya-webchat-2.js
    
    format
    
    * reinstate else statemene
    
    * Update avaya-webchat-2.js
    
    * reinstate else statemene
    
    * Update avaya-webchat-2.js
    
    * formatting
    
    * Update common.json
    
    helpchat times update
    
    * Update common.json
    
    helpchat times
    
    * Update avaya-webchat-2.js
    
    180minutes fix
    
    * Dtspb 2942 (#1691)
    
    * Update common.json
    
    helpchat times update
    
    * Update common.json
    
    helpchat times
    
    * test changes (do not merge)
    
    * revert
    
    Co-authored-by: DelythJustice <97962563+DelythJustice@users.noreply.github.com>

--- app/assets/javascripts/avaya-webchat-2.js
+++ app/assets/javascripts/avaya-webchat-2.js
@@ -46,14 +46,15 @@
             avayaAgentBusy.hidden = true;
             avayaWebchatClose.hidden = true;
             avayaWebchatMaintenance.hidden = true;
+
             if(ccState === 'Open'){
-                if(availableAgents > 0 && ewt <= 300){
+                if(ewt <= 10800){
                     avayaWebchatOpen.hidden = false;
                 }else{
                     avayaAgentBusy.hidden = false;
                 }
             }else{
-               avayaWebchatClose.hidden = false;
+                avayaWebchatClose.hidden = false;
             }
         });
     }
