8979a5340382bb6dc57470dce6dee0b556307056
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2022-09-01 17:55:42 +0100

    DTSPB-2913: update unit test (#1702)

--- test/unit/testRoutes.js
+++ test/unit/testRoutes.js
@@ -1,10 +1,20 @@
 'use strict';
-
-const routes = require('app/routes');
+const proxyquire = require('proxyquire');
 const expect = require('chai').expect;
+const configStub = { };
 
 describe('routes', () => {
+
     it('should contain /inviteIdList', () => {
+        configStub.environment = 'local';
+        const routes = proxyquire('app/routes', {'config': configStub});
         expect(routes.stack.some(s => s.route && s.route.path && s.route.path === '/inviteIdList')).to.equal(true);
     });
+
+    it('should not contain /inviteIdList', () => {
+        configStub.environment = 'prod';
+        const routes = proxyquire('app/routes', {'config': configStub});
+        expect(routes.stack.some(s => s.route && s.route.path && s.route.path === '/inviteIdList')).to.equal(false);
+    });
+
 });
