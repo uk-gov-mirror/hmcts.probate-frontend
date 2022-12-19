8d7e495005327bccb2f71ca4534ae4ec64349c80
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2022-12-19 19:35:27 +0000

    DTSPB-3138 modified e2e test

--- test/end-to-end/pages/payment/paymentstatus.js
+++ test/end-to-end/pages/payment/paymentstatus.js
@@ -7,7 +7,7 @@ module.exports = async function(language ='en') {
     const paymentStatusContent = require(`app/resources/${language}/translation/payment/status`);
     await I.checkInUrl('/payment-status');
 
-    await I.waitForText(paymentStatusContent.question, testConfig.TestWaitForTextToAppear);
+    await I.waitForText(paymentStatusContent.paragraph1, testConfig.TestWaitForTextToAppear);
     const locator = {css: '.govuk-button'};
     await I.waitForEnabled(locator);
     await I.navByClick(locator);
