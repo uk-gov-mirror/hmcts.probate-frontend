49d76e64208c10badc3bf52e70b926c128da24ba
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2023-01-03 13:44:18 +0000

    DTSPB-3146 Citizen Hub Case overview

---
+++ app/steps/ui/citizenshub/index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const Step = require('app/core/steps/Step');
+const FormatCcdCaseId = require('app/utils/FormatCcdCaseId');
+const FormatName = require('app/utils/FormatName');
+const DocumentsWrapper = require('app/wrappers/Documents');
+
+class CitizensHub extends Step {
+
+    static getUrl () {
+        return '/citizens-hub';
+    }
+
+    handleGet(ctx, formdata) {
+        const documentsWrapper = new DocumentsWrapper(formdata);
+        ctx.documentsRequired = documentsWrapper.documentsRequired();
+        ctx.checkAnswersSummary = false;
+        ctx.legalDeclaration = false;
+        if (formdata.checkAnswersSummary) {
+            ctx.checkAnswersSummary = true;
+        }
+        if (formdata.legalDeclaration) {
+            ctx.legalDeclaration = true;
+        }
+        return [ctx];
+    }
+
+    getContextData(req) {
+        const ctx = super.getContextData(req);
+        ctx.ccdReferenceNumber = FormatCcdCaseId.format(req.session.form.ccdCase);
+        ctx.ccdReferenceNumberAccessible = FormatCcdCaseId.formatAccessible(req.session.form.ccdCase);
+        ctx.deceasedName = FormatName.format(req.session.form.deceased);
+        return ctx;
+    }
+
+    action(ctx, formdata) {
+        super.action(ctx, formdata);
+        delete ctx.ccdReferenceNumber;
+        delete ctx.ccdReferenceNumberAccessible;
+        return [ctx, formdata];
+    }
+}
+
+module.exports = CitizensHub;
