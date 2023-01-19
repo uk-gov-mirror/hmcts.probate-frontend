e5d8f299afe34a67ad0f8abc15c95a1527ed44ec
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2023-01-19 11:50:48 +0000

    DTSPB-3142 Update screens flow and add download legal Document

--- app/steps/ui/thankyou/index.js
+++ app/steps/ui/thankyou/index.js
@@ -5,6 +5,13 @@ const FormatCcdCaseId = require('app/utils/FormatCcdCaseId');
 const DocumentsWrapper = require('app/wrappers/Documents');
 const caseTypes = require('../../../utils/CaseTypes');
 const CaseProgress = require('app/utils/CaseProgress');
+const DocumentPageUtil = require('app/utils/DocumentPageUtil');
+const ExecutorsWrapper = require('app/wrappers/Executors');
+const WillWrapper = require('app/wrappers/Will');
+const RegistryWrapper = require('app/wrappers/Registry');
+const DeathCertificateWrapper = require('app/wrappers/DeathCertificate');
+const ApplicantWrapper = require('app/wrappers/Applicant');
+const DeceasedWrapper = require('app/wrappers/Deceased');
 
 class ThankYou extends Step {
 
@@ -12,7 +19,7 @@ class ThankYou extends Step {
         return '/thank-you';
     }
 
-    handleGet(ctx, formdata) {
+    handleGet(ctx, formdata, featureToggles, language) {
         const documentsWrapper = new DocumentsWrapper(formdata);
         ctx.documentsRequired = documentsWrapper.documentsRequired();
         ctx.checkAnswersSummary = false;
@@ -23,6 +30,38 @@ class ThankYou extends Step {
         if (formdata.legalDeclaration) {
             ctx.legalDeclaration = true;
         }
+
+        const executorsWrapper = new ExecutorsWrapper(formdata.executors);
+        const willWrapper = new WillWrapper(formdata.will);
+        const deathCertWrapper = new DeathCertificateWrapper(formdata.deceased);
+        const registryAddress = (new RegistryWrapper(formdata.registry)).address();
+        const applicantWrapper = new ApplicantWrapper(formdata);
+        const deceasedWrapper = new DeceasedWrapper(formdata.deceased);
+
+        const content = this.generateContent(ctx, formdata, language);
+        ctx.registryAddress = registryAddress ? registryAddress : content.address;
+        ctx.interimDeathCertificate = deathCertWrapper.hasInterimDeathCertificate();
+        ctx.foreignDeathCertificate = deathCertWrapper.hasForeignDeathCertificate();
+        ctx.foreignDeathCertTranslatedSeparately = deathCertWrapper.isForeignDeathCertTranslatedSeparately();
+
+        if (ctx.caseType === caseTypes.GOP) {
+            ctx.hasCodicils = willWrapper.hasCodicils();
+            ctx.codicilsNumber = willWrapper.codicilsNumber();
+            ctx.hasMultipleApplicants = executorsWrapper.hasMultipleApplicants();
+            ctx.hasRenunciated = executorsWrapper.hasRenunciated();
+            ctx.executorsNameChangedByDeedPollList = executorsWrapper.executorsNameChangedByDeedPoll();
+        } else {
+            ctx.spouseRenouncing = deceasedWrapper.hasMarriedStatus() && applicantWrapper.isApplicantChild();
+            ctx.isSpouseGivingUpAdminRights = ctx.spouseRenouncing && applicantWrapper.isSpouseRenouncing() && !deceasedWrapper.hasAnyOtherChildren();
+        }
+
+        if (formdata.will && formdata.will.deceasedWrittenWishes) {
+            ctx.deceasedWrittenWishes = formdata.will.deceasedWrittenWishes;
+        }
+
+        ctx.is205 = formdata.iht && formdata.iht.method === 'optionPaper' && formdata.iht.form === 'optionIHT205';
+        ctx.is207 = formdata.iht && ((formdata.iht.method === 'optionPaper' && formdata.iht.form === 'optionIHT207') || (formdata.iht.ihtFormEstateId === 'optionIHT207'));
+        ctx.checkListItems = DocumentPageUtil.getCheckListItems(ctx, content);
         return [ctx];
     }
 
