8bb14a9a1d675d105b613ae502195d9e2b66f9a9
danielbe11 <daniel.bell@solirius.com>
2023-01-24 13:59:27 +0000

    More

--- test/unit/testEquality.js
+++ test/unit/testEquality.js
@@ -45,7 +45,7 @@ describe('Equality', () => {
 
                     expect(options).to.deep.equal({
                         redirect: true,
-                        url: `http://localhost:4000/service-endpoint?serviceId=PROBATE&actor=APPLICANT&pcqId=78e69022-2468-4370-a88e-bea2a80fa51f&ccdCaseId=1234567890123456&partyId=applicant@email.com&returnUrl=http://localhost:3000/task-list&language=en&token=${token}`
+                        url: `/service-endpoint?serviceId=PROBATE&actor=APPLICANT&pcqId=78e69022-2468-4370-a88e-bea2a80fa51f&ccdCaseId=1234567890123456&partyId=applicant@email.com&returnUrl=http://localhost:3000/task-list&language=en&token=${token}`
                     });
                     done();
                 }).catch(err => {
@@ -77,7 +77,7 @@ describe('Equality', () => {
 
                     expect(options).to.deep.equal({
                         redirect: true,
-                        url: 'http://localhost:4000/service-endpoint?serviceId=PROBATE&actor=APPLICANT&pcqId=78e69022-2468-4370-a88e-bea2a80fa51f&ccdCaseId=1234567890123456&partyId=applicant@email.com&returnUrl=http://localhost:3000/task-list&language=en'
+                        url: '/service-endpoint?serviceId=PROBATE&actor=APPLICANT&pcqId=78e69022-2468-4370-a88e-bea2a80fa51f&ccdCaseId=1234567890123456&partyId=applicant@email.com&returnUrl=http://localhost:3000/task-list&language=en'
                     });
                     done();
                 }).catch(err => {
