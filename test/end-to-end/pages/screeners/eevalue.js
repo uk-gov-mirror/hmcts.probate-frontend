d6017a8fdea514212f501ca42040f73049ee1c4e
nilay <nilay.patel@hmcts.net>
2023-01-25 11:07:42 +0000

    e2e test fixes

--- test/end-to-end/pages/screeners/eevalue.js
+++ test/end-to-end/pages/screeners/eevalue.js
@@ -8,9 +8,7 @@ module.exports = async function(language = 'en') {
 
     const locator = {css: '#eeEstateValued'};
     await I.waitForText(eeEstateValuedContent.question);
-    let hint = eeEstateValuedContent.hint;
-    hint = hint.split('>')[0].split('<a')[0] + hint.split('</a>')[0].split('>')[1] + hint.split('</a>')[1].split('<a')[0] + hint.split('</a>')[1].split('>')[1] + hint.split('</a>')[2];
-    await I.see(hint);
+
     await I.waitForEnabled(locator);
     await I.click(locator);
     await I.navByClick(commonContent.continue, 'button.govuk-button');
