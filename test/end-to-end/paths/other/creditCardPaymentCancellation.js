29786320389cb8296394b69f4509fbf122f8c92e
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2023-03-24 11:24:56 +0000

    DTSPB-3176 e2e fix

--- test/end-to-end/paths/other/creditCardPaymentCancellation.js
+++ test/end-to-end/paths/other/creditCardPaymentCancellation.js
@@ -76,13 +76,20 @@ getTestLanguages().forEach(language => {
             await I.selectDiedEngOrWales(language, optionNo);
             await I.selectEnglishForeignDeathCert(language, optionNo);
             await I.selectForeignDeathCertTranslation(language, optionYes);
-
-            await I.selectInheritanceMethod(language, ihtPost);
-
-            if (TestConfigurator.getUseGovPay() === 'true') {
-                await I.enterGrossAndNet(language, '205', '600000', '300000');
+            const isIHTEnabled = await TestConfigurator.checkFeatureToggle('probate-stop-ihtonline');
+            if (isIHTEnabled) {
+                if (TestConfigurator.getUseGovPay() === 'true') {
+                    await I.enterGrossAndNet(language, '207', '600000', '300000');
+                } else {
+                    await I.enterGrossAndNet(language, '207', '500', '400');
+                }
             } else {
-                await I.enterGrossAndNet(language, '205', '500', '400');
+                await I.selectInheritanceMethod(language, ihtPost);
+                if (TestConfigurator.getUseGovPay() === 'true') {
+                    await I.enterGrossAndNet(language, '205', '600000', '300000');
+                } else {
+                    await I.enterGrossAndNet(language, '205', '500', '400');
+                }
             }
 
             await I.selectDeceasedAlias(language, optionNo);
