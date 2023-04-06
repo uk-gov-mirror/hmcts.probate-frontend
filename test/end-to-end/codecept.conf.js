4c866bdede3eeb5dc68ddb264fb0ae2f106ccbcc
Marian.Irudayanathan <marian.irudayanathan@justice.gov.uk>
2023-04-06 08:55:44 +0100

    DTSPB-3138 Citizen Hub e2e Fix - Remove args in codecept.conf.js

--- test/end-to-end/codecept.conf.js
+++ test/end-to-end/codecept.conf.js
@@ -17,7 +17,7 @@ exports.config = {
                     height: 960
                 },
                 args: [
-                    '--headless', '--disable-gpu', '--no-sandbox', '--allow-running-insecure-content', '--ignore-certificate-errors', '--disable-dev-shm-usage',
+                    '--headless', '--no-sandbox', '--allow-running-insecure-content', '--ignore-certificate-errors',
                     //'--proxy-server=proxyout.reform.hmcts.net:8080',
                     //'--proxy-bypass-list=*beta*LB.reform.hmcts.net',
                     '--window-size=1440,1400'
