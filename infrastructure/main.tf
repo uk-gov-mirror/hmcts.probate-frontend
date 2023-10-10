f67ad98170691b90b283eee69b6d86761cf8a0d4
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-10-10 11:54:49 +0100

    update

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,19 +8,23 @@ locals {
 
 
 module "probate-frontend-redis-cache" {
-  source   = "git@github.com:hmcts/cnp-module-redis?ref=master"
-  product  = "${var.product}-${var.component}-redis-cache"
-  location = var.location
-  env      = var.env
-  common_tags  = var.common_tags
-  private_endpoint_enabled = true
-  redis_version = "6"
-  business_area = "cft"
+  source                        = "git@github.com:hmcts/cnp-module-redis?ref=master"
+  product                       = "${var.product}-${var.component}-redis-cache"
+  location                      = var.location
+  env                           = var.env
+  common_tags                   = var.common_tags
+  private_endpoint_enabled      = true
+  redis_version                 = "6"
+  business_area                 = "cft"
   public_network_access_enabled = false
+  sku_name                      = var.sku_name
+  family                        = var.family
+  capacity                      = var.capacity
+
 }
 
 data "azurerm_key_vault" "probate_key_vault" {
-  name = local.vaultName
+  name                = local.vaultName
   resource_group_name = local.vaultName
 }
 
