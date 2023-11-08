6e28a93acfc019789851102961c024300e904881
DelythJustice <delyth.jones11@justice.gov.uk>
2023-11-08 20:06:39 +0000

    DTSPB-3705 coverage

--- test/unit/iht/testSubmittedToHmrc.js
+++ test/unit/iht/testSubmittedToHmrc.js
@@ -2,16 +2,16 @@
 
 const initSteps = require('app/core/initSteps');
 const expect = require('chai').expect;
-const spy = require('sinon').spy;
+const sinon = require('sinon');
 const steps = initSteps([`${__dirname}/../../../app/steps/action/`, `${__dirname}/../../../app/steps/ui`]);
 const SubmittedToHmrc = steps.SubmittedToHmrc;
 const probateJourney = require('app/journeys/probate');
 
-describe('SubmittedToHmrc', () => {
+describe.only('SubmittedToHmrc', () => {
     describe('getUrl()', () => {
         it('should return the correct url', (done) => {
             // Create a spy for the getUrl() method
-            const getUrlSpy = spy(SubmittedToHmrc.constructor, 'getUrl');
+            const getUrlSpy = sinon.spy(SubmittedToHmrc.constructor, 'getUrl');
             const url = SubmittedToHmrc.constructor.getUrl();
             // Assert that the method was called
             expect(getUrlSpy.called).to.equal(true);
@@ -49,6 +49,31 @@ describe('SubmittedToHmrc', () => {
     });
 
     describe('next()', () => {
+        it('should return HmrcLetter step when ihtFormEstateId is optionIHT400', () => {
+            const req = {
+                session: {
+                    journey: {} // Mock the journey data
+                }
+            };
+            const ctx = {
+                ihtFormEstateId: 'optionIHT400'
+            };
+            // Stub the static method nextStepOptions on the SubmittedToHmrc class
+            const nextStepOptionsStub = sinon.stub(SubmittedToHmrc, 'nextStepOptions').returns({
+                options: [
+                    {key: 'optionIHT400', value: 'optionIHT400', choice: 'optionIHT400'},
+                    {key: 'optionIHT400421', value: 'optionIHT400421', choice: 'optionIHT400421'},
+                    {key: 'optionNotRequired', value: 'optionNotRequired', choice: 'optionNotRequired'}
+                ]
+            });
+            const result = SubmittedToHmrc.next(req, ctx);
+            expect(result).toString()
+                .includes('HmrcLetter');
+            // eslint-disable-next-line no-unused-expressions
+            expect(ctx.estateValueCompleted).to.be.undefined;
+            // eslint-disable-next-line no-unused-expressions
+            nextStepOptionsStub.restore();
+        });
         it('should set nextStep to IhtEstateValued when optionIHT400', (done) => {
             ctx.ihtFormEstateId = 'optionIHT400';
             const expectedStep = steps.HmrcLetter;
