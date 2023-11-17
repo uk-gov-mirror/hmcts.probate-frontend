ef78941ae5d4cb46c92ea2307de78332b520973b
DelythJustice <delyth.jones11@justice.gov.uk>
2023-11-17 16:02:57 +0000

    DTSPB-3705 added test

--- test/unit/iht/testNewSubmittedToHmrc.js
+++ test/unit/iht/testNewSubmittedToHmrc.js
@@ -21,6 +21,59 @@ describe.only('NewSubmittedToHmrc', () => {
         });
     });
 
+    describe('getContextData()', () => {
+        it('should return the context with the radio buttons set', (done) => {
+            const req = {
+                session: {
+                    form: {
+                        iht: {
+                            estateValueCompleted: 'optionYes',
+                            ihtFormEstateId: 'optionIHT400421'
+                        }
+                    }
+                }
+            };
+
+            const ctx = NewSubmittedToHmrc.getContextData(req);
+            expect(ctx.ihtFormIdTesting).to.equal('optionIHT400421');
+            done();
+        });
+    });
+    describe('handlePost()', () => {
+        let ctx;
+        let formdata;
+        let errors;
+
+        it('should update ctx with estate values for optionIHT400421', () => {
+            ctx = {
+                ihtFormIdTesting: 'optionIHT400421',
+                ihtFormEstateId: 'initialValue',
+                estateValueCompleted: 'initialOption',
+            };
+            formdata = {
+                iht: {
+                    ihtFormEstateId: 'initialValue',
+                    estateValueCompleted: 'initialOption',
+                },
+            };
+            errors = [];
+
+            NewSubmittedToHmrc.handlePost(ctx, errors, formdata, {language: 'en'});
+
+            // Assert the expected changes in ctx
+            expect(ctx).to.deep.equal({
+                ihtFormIdTesting: 'optionIHT400421',
+                ihtFormEstateId: 'optionIHT400421',
+                estateValueCompleted: 'optionYes', // Expected change
+            });
+
+            // Assert other expectations (e.g., errors array, no string replacement)
+
+            // You may not need done() for synchronous tests
+        });
+
+        // Add more test cases for other conditions if needed
+    });
     describe('nextStepOptions()', () => {
         it('should return the correct next step options', (done) => {
             const result = NewSubmittedToHmrc.nextStepOptions();
