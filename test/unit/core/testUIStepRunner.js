9d841872e52429f81ddc60e419b68983a70ff8b3
jamiealbertelli <james.albertelli@justice.gov.uk>
2024-01-17 14:53:04 +0000

    DTSPB-3463: Added additional functions to test step object

--- test/unit/core/testUIStepRunner.js
+++ test/unit/core/testUIStepRunner.js
@@ -21,7 +21,9 @@ describe('UIStepRunner', () => {
     };
     const req400 = {
         session: {
+            userLoggedIn: true,
             language: 'en',
+            regId: 'regid123',
             form: {
                 caseType: 'gop',
                 ccdCase: {
@@ -29,14 +31,10 @@ describe('UIStepRunner', () => {
                     state: 'Pending'
                 }
             },
-            caseType: 'gop'
+            back: ['hello']
         },
-        body: {
-            addressLine1: '143 Caerfai Bay Road',
-            postTown: 'town',
-            newPostCode: 'L23 6WW',
-            country: 'United Kingdon',
-            postcode: 'L23 6WW'
+        log: {
+            error: sinon.spy()
         }
     };
     it('declarationCheckbox should be false where hasDataChanged is true', (done) => {
@@ -72,32 +70,88 @@ describe('UIStepRunner', () => {
     });
     it('On 400 error formSubmissionUnsuccessful is displayed in user journey', (done) => {
         const stepName = 'DeceasedAddress';
+        const generateContent = {
+            'title': 'Copies - UK',
+            'question': 'How many extra official copies of the grant do you need for use in the UK?',
+            'paragraph1': 'You&rsquo;ll receive a free official copy of a grant of probate with your application fee.',
+            'paragraph2': 'Order extra official copies of the grant if you need to send them to different asset holders, for example, a copy for banks, insurance policies, shares and property.',
+            'paragraph3': 'Extra official copies cost &pound;1.50 each',
+            'copies': 'Number of extra official copies',
+            'questionOld': 'How many extra official copies of the grant do you need for use in the UK?',
+            'paragraph1Old': 'You&rsquo;ll receive a free official copy of a grant of probate with your application fee.',
+            'paragraph2Old': 'Order extra official copies of the grant if you need to send them to different asset holders, for example, a copy for banks, insurance policies, shares and property.',
+            'paragraph3Old': 'Extra official copies cost &pound;1.50 each',
+            'copiesOld': 'Number of extra official copies',
+            'detailTitle': 'What is an official copy?',
+            'detailText1': 'It&rsquo;s a copy of the grant with a holographic silver seal on the front of it. A bank may ask you for a certified copy to release funds to you.',
+            'detailText2': 'Official copies are used for assets in the UK only.',
+            'errors': 'key \'copies.uk.errors (en)\' returned an object instead of string.'
+        };
+        const generateFields = {
+            'uk': {
+                'value': '2',
+                'error': false
+            },
+            'overseas': {
+                'value': '0',
+                'error': false
+            },
+            'language': {
+                'value': 'en',
+                'error': false
+            },
+            'isAvayaWebChatEnabled': {
+                'value': 'true',
+                'error': false
+            },
+            'isWebChatEnabled': {
+                'value': 'true',
+                'error': false
+            },
+            'isGaEnabled': {
+                'value': 'true',
+                'error': false
+            }
+        };
+        const commonContent = {
+            'uk': 2,
+            'overseas': 0,
+            'language': 'en',
+            'isAvayaWebChatEnabled': true,
+            'isWebChatEnabled': true,
+            'isGaEnabled': true
+        };
         const step = {
             name: stepName,
-            validate: () => [true, []],
+            template: 'ui/copies/uk/template',
+            validate: () => [false, []],
             getContextData: () => '',
             nextStepUrl: () => '',
             action: () => [{}, req400.session.form],
             constructor: {
                 getUrl: () => 'hello'
-            }
+            },
+            shouldPersistFormData: () => true,
+            persistFormData: () => ({name: 'Error', message: 'xxxxxxx'}),
+            generateContent: () => (generateContent),
+            generateFields: () => (generateFields),
+            commonContent: () => (commonContent),
         };
 
-        const hasDataChangedStub = sinon.stub(DetectDataChange.prototype, 'hasDataChanged').returns(true);
         const expectedForm = {
-            declarationCheckbox: 'false',
-            hasDataChanged: true
+            id: 1234567890123456,
         };
         const res = {
-            redirect: (url) => url
+            redirect: (url) => url,
         };
-
         const runner = new UIStepRunner();
         co(function* () {
             yield runner.handlePost(step, req400, res);
-            expect(req.session.form.declaration).to.deep.equal(expectedForm);
+            expect(res.statusCode).to.equal(400);
+            expect(res).to.have.property('body');
+            expect(res.body).to.equal('wrong header');
+            expect(req.session.form.ccdCase.id).to.deep.equal(expectedForm);
             expect(req.session.form.eventDescription).equal('Page completed: hello');
-            hasDataChangedStub.restore();
             done();
         });
     });
