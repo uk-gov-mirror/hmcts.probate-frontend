b7d44ff9b52e540ba515d5bccfa5e97d84b3e4e9
jamiealbertelli <james.albertelli@justice.gov.uk>
2024-01-23 14:53:02 +0000

    DTSPB-3463: Tidied up useless test objects and renamed test description

--- test/unit/core/testUIStepRunner.js
+++ test/unit/core/testUIStepRunner.js
@@ -21,7 +21,6 @@ describe('UIStepRunner', () => {
     };
     const req400 = {
         session: {
-            userLoggedIn: true,
             language: 'en',
             regId: 'regid123',
             form: {
@@ -33,6 +32,7 @@ describe('UIStepRunner', () => {
             },
             back: ['hello']
         },
+        userLoggedIn: true,
         log: {
             error: sinon.spy()
         }
@@ -68,59 +68,8 @@ describe('UIStepRunner', () => {
         });
 
     });
-    it('On 400 error formSubmissionUnsuccessful is displayed in user journey', (done) => {
+    it('When a 400 Bad Request occurs an Error should occur', (done) => {
         const stepName = 'DeceasedAddress';
-        const generateContent = {
-            'title': 'Copies - UK',
-            'question': 'How many extra official copies of the grant do you need for use in the UK?',
-            'paragraph1': 'You&rsquo;ll receive a free official copy of a grant of probate with your application fee.',
-            'paragraph2': 'Order extra official copies of the grant if you need to send them to different asset holders, for example, a copy for banks, insurance policies, shares and property.',
-            'paragraph3': 'Extra official copies cost &pound;1.50 each',
-            'copies': 'Number of extra official copies',
-            'questionOld': 'How many extra official copies of the grant do you need for use in the UK?',
-            'paragraph1Old': 'You&rsquo;ll receive a free official copy of a grant of probate with your application fee.',
-            'paragraph2Old': 'Order extra official copies of the grant if you need to send them to different asset holders, for example, a copy for banks, insurance policies, shares and property.',
-            'paragraph3Old': 'Extra official copies cost &pound;1.50 each',
-            'copiesOld': 'Number of extra official copies',
-            'detailTitle': 'What is an official copy?',
-            'detailText1': 'It&rsquo;s a copy of the grant with a holographic silver seal on the front of it. A bank may ask you for a certified copy to release funds to you.',
-            'detailText2': 'Official copies are used for assets in the UK only.',
-            'errors': 'key \'copies.uk.errors (en)\' returned an object instead of string.'
-        };
-        const generateFields = {
-            'uk': {
-                'value': '2',
-                'error': false
-            },
-            'overseas': {
-                'value': '0',
-                'error': false
-            },
-            'language': {
-                'value': 'en',
-                'error': false
-            },
-            'isAvayaWebChatEnabled': {
-                'value': 'true',
-                'error': false
-            },
-            'isWebChatEnabled': {
-                'value': 'true',
-                'error': false
-            },
-            'isGaEnabled': {
-                'value': 'true',
-                'error': false
-            }
-        };
-        const commonContent = {
-            'uk': 2,
-            'overseas': 0,
-            'language': 'en',
-            'isAvayaWebChatEnabled': true,
-            'isWebChatEnabled': true,
-            'isGaEnabled': true
-        };
         const step = {
             name: stepName,
             template: 'ui/copies/uk/template',
@@ -133,25 +82,25 @@ describe('UIStepRunner', () => {
             },
             shouldPersistFormData: () => true,
             persistFormData: () => ({name: 'Error', message: 'xxxxxxx'}),
-            generateContent: () => (generateContent),
-            generateFields: () => (generateFields),
-            commonContent: () => (commonContent),
+            generateContent: () => '',
+            generateFields: () => '',
+            commonContent: () => ''
         };
 
-        const expectedForm = {
-            id: 1234567890123456,
-        };
-        const res = {
+        const res400 = {
             redirect: (url) => url,
         };
+
+        res400.render = sinon.spy();
+        res400.statusCode=400;
+        res400.body = 'wrong header';
+
         const runner = new UIStepRunner();
         co(function* () {
-            yield runner.handlePost(step, req400, res);
-            expect(res.statusCode).to.equal(400);
-            expect(res).to.have.property('body');
-            expect(res.body).to.equal('wrong header');
-            expect(req.session.form.ccdCase.id).to.deep.equal(expectedForm);
-            expect(req.session.form.eventDescription).equal('Page completed: hello');
+            yield runner.handlePost(step, req400, res400);
+            expect(res400.statusCode).to.equal(400);
+            expect(res400).to.have.property('body');
+            expect(res400.body).to.equal('wrong header');
             done();
         });
     });
