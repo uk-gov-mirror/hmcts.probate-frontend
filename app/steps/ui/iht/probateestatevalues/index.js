881029a6f31864afd4cfd90d4d79dc6046127d72
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2024-01-25 13:53:22 +0000

    DTSPB-3970 Fix test

--- app/steps/ui/iht/probateestatevalues/index.js
+++ app/steps/ui/iht/probateestatevalues/index.js
@@ -41,14 +41,6 @@ class ProbateEstateValues extends ValidationStep {
         ctx.grossValue = parseFloat(numeral(ctx.grossValueField).format('0.00'));
         ctx.netValue = parseFloat(numeral(ctx.netValueField).format('0.00'));
 
-        if (!validator.isCurrency(ctx.grossValueField, {symbol: '£', allow_negatives: false})) {
-            errors.push(FieldError('grossValueField', 'invalidCurrencyFormat', this.resourcePath, this.generateContent({}, {}, session.language), session.language));
-        }
-
-        if (!validator.isCurrency(ctx.netValueField, {symbol: '£', allow_negatives: false})) {
-            errors.push(FieldError('netValueField', 'invalidCurrencyFormat', this.resourcePath, this.generateContent({}, {}, session.language), session.language));
-        }
-
         if (ctx.netValue > ctx.grossValue) {
             errors.push(FieldError('netValueField', 'netValueGreaterThanGross', this.resourcePath, this.generateContent({}, {}, session.language), session.language));
         }
