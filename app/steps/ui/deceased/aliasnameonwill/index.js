60149751afafae4162f1ee31375f0a0f2c23624d
jamiealbertelli <james.albertelli@justice.gov.uk>
2024-07-10 17:11:54 +0100

    DTSPB-4070: Add AliasNameOnWill string concat to display on CYA page

--- app/steps/ui/deceased/aliasnameonwill/index.js
+++ app/steps/ui/deceased/aliasnameonwill/index.js
@@ -1,11 +1,19 @@
 'use strict';
 
 const ValidationStep = require('app/core/steps/ValidationStep');
+const FormatName = require('../../../../utils/FormatName');
 
 class DeceasedAliasNameOnWill extends ValidationStep {
 
     static getUrl() {
         return '/deceased-alias-name-on-will';
+    }
+
+    getContextData(req) {
+        const ctx = super.getContextData(req);
+        const formdata = req.session.form;
+        ctx.aliasNameOnWill = FormatName.formatAliasNameOnWIll(formdata.deceased);
+        return ctx;
     }
 
     action(ctx, formdata) {
