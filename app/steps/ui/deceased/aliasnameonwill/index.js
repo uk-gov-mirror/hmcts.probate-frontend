9f804af59b7ac1845877da8da2dd08bf45432fb2
jamiealbertelli <james.albertelli@justice.gov.uk>
2024-07-11 14:30:21 +0100

    DTSPB-4070: Remove fake name used for testing on preview

--- app/steps/ui/deceased/aliasnameonwill/index.js
+++ app/steps/ui/deceased/aliasnameonwill/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const ValidationStep = require('app/core/steps/ValidationStep');
-//const FormatName = require('../../../../utils/FormatName');
+const FormatName = require('../../../../utils/FormatName');
 
 class DeceasedAliasNameOnWill extends ValidationStep {
 
@@ -11,8 +11,7 @@ class DeceasedAliasNameOnWill extends ValidationStep {
 
     action(ctx, formdata) {
         super.action(ctx, formdata);
-        //ctx.aliasNameOnWill = FormatName.formatAliasNameOnWIll(formdata.deceased);
-        ctx.aliasNameOnWill = 'FAKE NAME FOR TESTING';
+        ctx.aliasNameOnWill = FormatName.formatAliasNameOnWIll(formdata.deceased);
         return [ctx, formdata];
     }
 }
