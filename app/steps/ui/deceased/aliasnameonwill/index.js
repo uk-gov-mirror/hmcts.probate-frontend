91d0e01ba7c084c2b83f62e4ee1f441eb759dc17
jamiealbertelli <james.albertelli@justice.gov.uk>
2024-07-11 14:16:58 +0100

    DTSPB-4070: Add fake name to test on preview

--- app/steps/ui/deceased/aliasnameonwill/index.js
+++ app/steps/ui/deceased/aliasnameonwill/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const ValidationStep = require('app/core/steps/ValidationStep');
-const FormatName = require('../../../../utils/FormatName');
+//const FormatName = require('../../../../utils/FormatName');
 
 class DeceasedAliasNameOnWill extends ValidationStep {
 
@@ -11,7 +11,8 @@ class DeceasedAliasNameOnWill extends ValidationStep {
 
     action(ctx, formdata) {
         super.action(ctx, formdata);
-        ctx.aliasNameOnWill = FormatName.formatAliasNameOnWIll(formdata.deceased);
+        //ctx.aliasNameOnWill = FormatName.formatAliasNameOnWIll(formdata.deceased);
+        ctx.aliasNameOnWill = 'FAKE NAME FOR TESTING';
         return [ctx, formdata];
     }
 }
