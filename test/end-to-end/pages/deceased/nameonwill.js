1edf5f9791fcde284cbb3520a63c1490b7e906d7
marian.irudyanathan <marian.irudayanathan@justice.gov.uk>
2024-07-23 21:15:13 +0100

    DTSPB-4070 - Update changes to deceased alias name on will for all GOP e2e tests

--- test/end-to-end/pages/deceased/nameonwill.js
+++ test/end-to-end/pages/deceased/nameonwill.js
@@ -6,11 +6,11 @@ module.exports = async function(language = 'en', answer = null) {
     const I = this;
 
     const commonContent = require(`app/resources/${language}/translation/common`);
-    const aliasContent = require(`app/resources/${language}/translation/deceased/alias`);
+    const aliasContent = require(`app/resources/${language}/translation/deceased/nameasonwill`);
 
-    await I.checkInUrl('/deceased-alias');
-    await I.waitForText(aliasContent.intestacyParagraph1, config.TestWaitForTextToAppear);
-    const locator = {css: `#alias${answer}`};
+    await I.checkInUrl('/deceased-name-as-on-will');
+    await I.waitForText(aliasContent.explanation1, config.TestWaitForTextToAppear);
+    const locator = {css: `#nameAsOnTheWill${answer}`};
     await I.waitForEnabled(locator);
     await I.click(locator);
 
