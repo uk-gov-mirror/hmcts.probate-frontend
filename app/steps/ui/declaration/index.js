ad4c95f47be9792df1c16a668f5e10044e87532a
jamiealbertelli <james.albertelli@justice.gov.uk>
2024-10-08 16:44:47 +0100

    DTSPB-4367: Fix params

--- app/steps/ui/declaration/index.js
+++ app/steps/ui/declaration/index.js
@@ -141,7 +141,7 @@ class Declaration extends ValidationStep {
 
             const executorsApplying = ctx.executorsWrapper.executorsApplying();
             const executorsApplyingText = {
-                en: this.executorsApplying(ctx.executorsWrapper, executorsApplying, content.en, hasCodicils, codicilsNumber, formdata, req.session.language),
+                en: this.executorsApplying(ctx.hasMultipleApplicants, executorsApplying, content.en, hasCodicils, codicilsNumber, formdata, req.session.language),
                 cy: this.executorsApplying(ctx.hasMultipleApplicants, executorsApplying, content.cy, hasCodicils, codicilsNumber, formdata, req.session.language)
             };
 
