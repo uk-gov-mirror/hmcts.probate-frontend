d98ad85a72083d2406f0e7d4d7cf732907bef2c1
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2024-10-14 16:34:10 +0100

    DTSPB-4261 Added call to OS

--- app/steps/ui/reviewresponse/index.js
+++ app/steps/ui/reviewresponse/index.js
@@ -3,6 +3,7 @@
 const ValidationStep = require('app/core/steps/ValidationStep');
 const FieldError = require('app/components/error');
 const {get} = require('lodash');
+const config = require('config');
 
 class ReviewResponse extends ValidationStep {
 
@@ -19,12 +20,21 @@ class ReviewResponse extends ValidationStep {
         return ctx;
     }
 
-    handlePost(ctx, errors, formdata, session) {
+    handlePost(ctx, errors, formdata, session, req) {
         const error = formdata.documents && formdata.documents.error;
         if (error) {
             errors = errors || [];
             errors.push(FieldError('file', error, this.resourcePath, this.generateContent({}, {}, session.language), session.language));
             delete formdata.documents.error;
+        }
+        if (error === null) {
+            const document = new Document(config.services.orchestrator.url, ctx.sessionID);
+            document.notifyApplicant(ctx.ccdCase.id, req.authToken, req.session.serviceAuthorization)
+                .then(result => {
+                    if (result.name === 'Error') {
+                        throw new ReferenceError('Error sending notification about doc upload');
+                    }
+                });
         }
         return [ctx, errors];
     }
