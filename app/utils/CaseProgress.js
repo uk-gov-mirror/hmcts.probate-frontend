c92efbae693b3b1bc6ef0b10eab6b8d16f0ddcbe
FeliTam <feli.tam@justice.gov.uk>
2024-10-24 10:37:40 +0100

    DTSPB-4264 update CaseProgress

--- app/utils/CaseProgress.js
+++ app/utils/CaseProgress.js
@@ -25,11 +25,11 @@ class CaseProgress {
     }
 
     static informationProvided(state, documentUploadIssue, citizenResponseSubmittedDate) {
-        return state === 'BOCaseClosed' && documentUploadIssue !== 'optionYes' && citizenResponseSubmittedDate;
+        return (state === 'BOCaseStopped' || state === 'Dormant') && documentUploadIssue !== 'optionYes' && citizenResponseSubmittedDate;
     }
 
     static partialInformationProvided(state, documentUploadIssue, citizenResponseSubmittedDate) {
-        return state === 'BOCaseClosed' && documentUploadIssue === 'optionYes' && citizenResponseSubmittedDate;
+        return (state === 'BOCaseStopped' || state === 'Dormant') && documentUploadIssue === 'optionYes' && citizenResponseSubmittedDate;
     }
 }
 
