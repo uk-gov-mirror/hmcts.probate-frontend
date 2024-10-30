b9e68f6f744a36c0d3e5afa07f0dc80f66bac765
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2024-10-30 20:29:10 +0000

    DTSPB-4240 Fix Sonar

--- app/components/step-utils.js
+++ app/components/step-utils.js
@@ -111,7 +111,6 @@ const getScrennersPreviousUrl = (ctx, req, res, steps, currentStepName) => {
     const StartEligibilityStep = 'StartEligibility';
     let loopingStep = steps[StartEligibilityStep];
     if (currentStepName === 'StartEligibility' && req.userLoggedIn) {
-        previousUrl = '';
         ctx.previousUrl = previousUrl;
         return;
     }
