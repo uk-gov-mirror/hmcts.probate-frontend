72cdef59370fbbfc28c5fe913568d9cd15691702
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2024-11-14 12:29:29 +0000

    DTSPB-4260 remove typo

--- app/steps/ui/provideinformation/index.js
+++ app/steps/ui/provideinformation/index.js
@@ -29,10 +29,6 @@ class ProvideInformation extends ValidationStep {
         return ctx;
     }
     handlePost(ctx, errors, formdata, session) {
-        console.log(`handlePost error: ${session.form.documents.error}`);
-        if (session.form.documents.error && session.form.documents.error.length() >0) {
-            errors.push(FieldError('file', session.form.documents.error, this.resourcePath, this.generateContent({}, {}, session.language), session.language));
-        }
         if (typeof ctx.documentUploadIssue === 'undefined' || !ctx.documentUploadIssue) {
             ctx.documentUploadIssue = 'false';
         }
