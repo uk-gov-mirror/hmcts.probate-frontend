d7ab92e9d83e6847c3b37c0dc4fce6443b85188b
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2024-12-04 14:42:25 +0000

    DTSPB-4136 add Deceased Name to question & error Message

--- app/steps/ui/will/deceasedwrittenwishes/index.js
+++ app/steps/ui/will/deceasedwrittenwishes/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const ValidationStep = require('app/core/steps/ValidationStep');
+const FormatName = require('../../../../utils/FormatName');
 
 class DeceasedWrittenWishes extends ValidationStep {
 
@@ -8,6 +9,19 @@ class DeceasedWrittenWishes extends ValidationStep {
         return '/deceased-written-wishes';
     }
 
+    getContextData(req) {
+        const ctx = super.getContextData(req);
+        ctx.deceasedName = FormatName.format(req.session.form.deceased);
+        return ctx;
+    }
+
+    generateFields(language, ctx, errors) {
+        const fields = super.generateFields(language, ctx, errors);
+        if (fields.deceasedName && errors) {
+            errors[0].msg = errors[0].msg.replace('{deceasedName}', fields.deceasedName.value);
+        }
+        return fields;
+    }
 }
 
 module.exports = DeceasedWrittenWishes;
