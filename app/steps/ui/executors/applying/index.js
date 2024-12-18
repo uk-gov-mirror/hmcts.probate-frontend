3c78e26de594aeb98869d4f12425055467a7b125
jamiealbertelli <james.albertelli@justice.gov.uk>
2024-12-18 12:49:32 +0000

    DTSPB-4361: Update index with dealingwithestate checkbox logic whilst also dealing with radios in nextStepOptions()

--- app/steps/ui/executors/applying/index.js
+++ app/steps/ui/executors/applying/index.js
@@ -2,6 +2,8 @@
 
 const ValidationStep = require('app/core/steps/ValidationStep');
 const ExecutorsWrapper = require('app/wrappers/Executors');
+const FormatName = require('../../../../utils/FormatName');
+const {includes, some, tail} = require('lodash');
 
 class ExecutorsApplying extends ValidationStep {
 
@@ -9,16 +11,59 @@ class ExecutorsApplying extends ValidationStep {
         return '/other-executors-applying';
     }
 
-    handlePost(ctx) {
-        if (ctx.otherExecutorsApplying === 'optionNo') {
-            const executorsWrapper = new ExecutorsWrapper(ctx);
-            executorsWrapper.executors(true)
+    getContextData(req) {
+        const formdata = req.session.form;
+        const applicant = formdata.applicant;
+        const ctx = super.getContextData(req);
+        if (ctx.list) {
+            ctx.options = (new ExecutorsWrapper(ctx)).aliveExecutors()
                 .map(executor => {
-                    delete executor.isApplying;
-                    return executor;
+                    if (executor.isApplicant) {
+                        const optionValue = applicant.alias ? applicant.alias : FormatName.format(executor);
+                        return {value: optionValue, text: optionValue, checked: true, disabled: true};
+                    }
+                    return {value: executor.fullName, text: executor.fullName, checked: executor.isApplying === true};
                 });
         }
-        return [ctx];
+        return ctx;
+    }
+
+    pruneExecutorData(data) {
+        if (data.isApplying) {
+            delete data.isDead;
+            delete data.diedBefore;
+            delete data.notApplyingReason;
+            delete data.notApplyingKey;
+        } else {
+            delete data.isApplying;
+            delete data.address;
+            delete data.currentName;
+            delete data.currentNameReason;
+            delete data.email;
+            delete data.hasOtherName;
+            delete data.mobile;
+            delete data.postcode;
+        }
+        return data;
+    }
+
+    handlePost(ctx, errors) {
+        for (let i = 1; i < ctx.executorsNumber; i++) {
+            ctx.list[i].isApplying = includes(ctx.executorsApplying, ctx.list[i].fullName);
+            ctx.list[i] = this.pruneExecutorData(ctx.list[i]);
+        }
+        return [ctx, errors];
+    }
+
+    action(ctx, formdata) {
+        super.action(ctx, formdata);
+        delete ctx.options;
+        delete ctx.executorsApplying;
+        return [ctx, formdata];
+    }
+
+    isComplete(ctx) {
+        return [some(tail(ctx.list), exec => exec.isApplying === true), 'inProgress'];
     }
 
     nextStepOptions() {
