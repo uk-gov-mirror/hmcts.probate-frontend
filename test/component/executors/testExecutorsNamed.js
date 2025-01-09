30b8ea732443cd59ec6b4a87bd345dbb7de7efde
jamiealbertelli <james.albertelli@justice.gov.uk>
2025-01-09 10:23:42 +0000

    DTSPB-4477: Prelim test updates

--- test/component/executors/testExecutorsNamed.js
+++ test/component/executors/testExecutorsNamed.js
@@ -1,18 +1,46 @@
 'use strict';
 
 const TestWrapper = require('test/util/TestWrapper');
-const Equality = require('app/steps/ui/equality');
 const ExecutorsNames = require('app/steps/ui/executors/names');
+const ExecutorsAllAlive = require('app/steps/ui/executors/allalive');
 const testCommonContent = require('test/component/common/testCommonContent.js');
 const caseTypes = require('app/utils/CaseTypes');
 
-describe('executors-number', () => {
-    let testWrapper;
+describe('executors-named', () => {
+    let testWrapper, sessionData;
     const expectedNextUrlForExecNames = ExecutorsNames.getUrl();
-    const expectedNextUrlForEquality = Equality.getUrl();
+    const expectedNextUrlForAllAlive = ExecutorsAllAlive.getUrl();
 
     beforeEach(() => {
-        testWrapper = new TestWrapper('ExecutorsNumber');
+        testWrapper = new TestWrapper('ExecutorsNamed');
+        sessionData = {
+            type: caseTypes.GOP,
+            ccdCase: {
+                state: 'Pending',
+                id: 1234567890123456
+            },
+            applicant: {
+                'firstName': 'Bobby',
+                'lastName': 'Applicant',
+                'nameAsOnTheWill': 'optionYes',
+                'isApplying': true,
+                'isApplicant': true,
+                'fullName': 'Bobby Applicant'
+            },
+            executors: {
+                executorsNumber: 2,
+                list: [
+                    {
+                        'fullName': 'Fred Exec One',
+                        'isApplying': false
+                    },
+                    {
+                        'fullName': 'Jeff Exec Two',
+                        'isApplying': false
+                    }
+                ]
+            }
+        };
     });
 
     afterEach(() => {
@@ -20,17 +48,60 @@ describe('executors-number', () => {
     });
 
     describe('Verify Content, Errors and Redirection', () => {
-        testCommonContent.runTest('ExecutorsNumber', null, null, [], false, {type: caseTypes.GOP});
+        testCommonContent.runTest('ExecutorsNamed', null, null, [], false, {type: caseTypes.GOP,
+            ccdCase: {
+                state: 'Pending',
+                id: 1234567890123456
+            },
+            applicant: {
+                'firstName': 'Bobby',
+                'lastName': 'Applicant',
+                'nameAsOnTheWill': 'optionYes',
+                'isApplying': true,
+                'isApplicant': true,
+                'fullName': 'Bobby Applicant'
+            },
+            executors: {
+                executorsNumber: 2,
+                list: [
+                    {
+                        'fullName': 'Fred Exec One',
+                        'isApplying': false
+                    },
+                    {
+                        'fullName': 'Jeff Exec Two',
+                        'isApplying': false
+                    }
+                ]
+            }});
 
-        it('test content loaded on the page', (done) => {
-            const sessionData = {
-                type: caseTypes.GOP,
-                ccdCase: {
-                    state: 'Pending',
-                    id: 1234567890123456
-                }
-            };
+        //working
+        it('test redirection to names page when selecting yes with multiple executors', (done) => {
+            const data = {executorsNamed: 'optionYes'};
+            testWrapper.testRedirect(done, data, expectedNextUrlForExecNames);
+        });
 
+        //This should be directing to all alive but on the PR is currently moving to equality
+        it('test redirection to equality page when selecting no', (done) => {
+            const data = {executorsNamed: 'optionNo'};
+            testWrapper.testRedirect(done, data, expectedNextUrlForAllAlive);
+        });
+
+        it('test errors message displayed for no number entered', (done) => {
+            testWrapper.testErrors(done, {}, 'required');
+        });
+
+        //Working on these below
+        it('test correct content loaded on the page when lead applicant does not have an alias', (done) => {
+            testWrapper.agent.post('/prepare-session/form')
+                .send(sessionData)
+                .end(() => {
+                    testWrapper.testContent(done);
+                });
+        });
+
+        it('test correct content loaded on the page when lead applicant does have an alias', (done) => {
+            sessionData.executors.list[0].fullName = 'Fred Exec One';
             testWrapper.agent.post('/prepare-session/form')
                 .send(sessionData)
                 .end(() => {
@@ -39,19 +110,15 @@ describe('executors-number', () => {
         });
 
         it('test errors message displayed for invalid data', (done) => {
-            const data = {executorsNumber: 'abd'};
+            const data = {executorsNamed: 'abd'};
 
             testWrapper.testErrors(done, data, 'invalid');
         });
 
         it('test errors message displayed for invalid data - negative numbers', (done) => {
-            const data = {executorsNumber: '-1'};
+            const data = {executorsNamed: '-1'};
 
             testWrapper.testErrors(done, data, 'invalid');
-        });
-
-        it('test errors message displayed for no number entered', (done) => {
-            testWrapper.testErrors(done, {}, 'required');
         });
 
         it('test it displays the errors when there are more than 20 executors', (done) => {
@@ -60,16 +127,17 @@ describe('executors-number', () => {
             testWrapper.testErrors(done, data, 'invalid');
         });
 
-        it(`test it redirects to next page: ${expectedNextUrlForExecNames}`, (done) => {
-            const data = {executorsNumber: 2};
+        it('test errors message displayed for invalid data', (done) => {
+            testWrapper.agent.post('/prepare-session/form')
+                .send(sessionData)
+                .end(() => {
+                    const data = {
+                        applicant: ['x']
+                    };
+                    const errorsToTest = ['executorNamed'];
 
-            testWrapper.testRedirect(done, data, expectedNextUrlForExecNames);
-        });
-
-        it(`test it redirects to next page when there is only one executor: ${expectedNextUrlForEquality}`, (done) => {
-            const data = {executorsNumber: 1};
-
-            testWrapper.testRedirect(done, data, expectedNextUrlForEquality);
+                    testWrapper.testErrors(done, data, 'invalid', errorsToTest);
+                });
         });
     });
 });
