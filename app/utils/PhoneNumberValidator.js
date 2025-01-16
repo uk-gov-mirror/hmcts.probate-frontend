c0bb16bd57b7fc7808400d22e4e29bc0a93f9870
jamiealbertelli <james.albertelli@justice.gov.uk>
2025-01-16 11:54:48 +0000

    DTSPB-4477: Remove num length for error control

--- app/utils/PhoneNumberValidator.js
+++ app/utils/PhoneNumberValidator.js
@@ -2,17 +2,10 @@ const parsePhoneNumber = require('libphonenumber-js/mobile').parsePhoneNumber;
 
 class PhoneNumberValidator {
     static validateMobilePhoneNumber(phoneNumber) {
-        const MIN_PHONE_LENGTH = 11;
-        const MAX_PHONE_LENGTH = 15;
-
         try {
             const parsedPhoneNumber = parsePhoneNumber(phoneNumber, 'GB');
             if (parsedPhoneNumber && parsedPhoneNumber.isValid()) {
                 return {isValid: true};
-            } else if (phoneNumber.length < MIN_PHONE_LENGTH) {
-                return {isValid: false, errorType: 'tooShort'};
-            } else if (phoneNumber.length > MAX_PHONE_LENGTH) {
-                return {isValid: false, errorType: 'tooLong'};
             }
             return {isValid: false, errorType: 'invalid'};
         } catch (error) {
