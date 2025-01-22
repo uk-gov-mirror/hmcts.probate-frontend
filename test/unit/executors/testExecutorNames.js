889346b97384771a5e700f308bd3a2f2e18e999c
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2025-01-22 18:44:47 +0000

    DTSPB-4361 Added test

--- test/unit/executors/testExecutorNames.js
+++ test/unit/executors/testExecutorNames.js
@@ -4,7 +4,7 @@ const expect = require('chai').expect;
 const steps = initSteps([`${__dirname}/../../../app/steps/action/`, `${__dirname}/../../../app/steps/ui`]);
 const ExecutorsNames = steps.ExecutorsNames;
 
-describe.only('ExecutorsNames', () => {
+describe('ExecutorsNames', () => {
     describe('getUrl()', () => {
         it('should return the correct url', (done) => {
             const url = ExecutorsNames.constructor.getUrl();
