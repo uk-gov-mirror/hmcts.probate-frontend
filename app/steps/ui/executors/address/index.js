a015930ab290ec65f80f9c385bc89d882fc9903b
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2025-03-05 15:34:13 +0000

    DTSPB-4361 Fix name text

--- app/steps/ui/executors/address/index.js
+++ app/steps/ui/executors/address/index.js
@@ -23,7 +23,7 @@ class ExecutorAddress extends AddressStep {
             ctx.index = this.recalcIndex(ctx, 0);
             ctx.redirect = `${pageUrl}/${ctx.index}`;
         }
-        ctx.otherExecName = ctx.list[ctx.index] && ctx.list[ctx.index].fullName;
+        ctx.otherExecName = ctx.list[ctx.index] && ctx.list[ctx.index].hasOtherName ? ctx.list[ctx.index].currentName : ctx.list[ctx.index].fullName;
         ctx.executorsWrapper = new ExecutorsWrapper(ctx);
 
         return ctx;
