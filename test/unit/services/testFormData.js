365965cafc60b6c1133128ad9e4eb02f7fd9de9c
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2025-04-14 15:50:34 +0100

    DTSPB-4663 Fix unit test with testFormData.js

--- test/unit/services/testFormData.js
+++ test/unit/services/testFormData.js
@@ -42,12 +42,14 @@ describe('FormDataService', () => {
     it('should call post() successfully', (done) => {
         const endpoint = 'http://localhost';
         const ccdCaseId = 1234567890123456;
+        const lastModifiedDateTime = '2018-01-01T12:12:12.123';
         const inputForm = {caseType: caseTypes.GOP, deceased: {name: 'test'}};
         const expectedForm = {type: caseTypes.GOP, caseType: caseTypes.GOP, deceased: {name: 'test'}};
         const authToken = 'authToken';
         const serviceAuthorisation = 'serviceAuthorisation';
         const formData = new FormData(endpoint, 'abc123');
-        const path = formData.replacePlaceholderInPath(config.services.orchestrator.paths.forms, 'ccdCaseId', ccdCaseId);
+        let path = formData.replacePlaceholderInPath(config.services.orchestrator.paths.save_forms, 'ccdCaseId', ccdCaseId);
+        path = formData.replacePlaceholderInPath(path, 'lastModifiedDateTime', lastModifiedDateTime);
         nock(endpoint, {
             reqheaders: {
                 'Content-Type': 'application/json',
@@ -58,7 +60,7 @@ describe('FormDataService', () => {
             .reply(200, expectedForm);
 
         co(function* () {
-            const actualForm = yield formData.post(authToken, serviceAuthorisation, ccdCaseId, inputForm);
+            const actualForm = yield formData.post(authToken, serviceAuthorisation, ccdCaseId, lastModifiedDateTime, inputForm);
             expect(actualForm).to.deep.equal(expectedForm);
             done();
         }).catch(err => {
