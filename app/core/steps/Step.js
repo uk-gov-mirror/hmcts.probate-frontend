41cd40488d443f4882dfdea64e01ba63755bcf65
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2025-06-26 12:21:11 +0100

    DTSPB-4792 Prevent prototype pollution

--- app/core/steps/Step.js
+++ app/core/steps/Step.js
@@ -60,7 +60,7 @@ class Step {
     getContextData(req) {
         const session = req.session;
         let ctx = {};
-        merge(ctx, session.form[this.section] || {});
+        Object.assign(ctx, session.form[this.section] || {});
         ctx.sessionID = req.sessionID;
         ctx.caseType = caseTypes.getCaseType(session);
         ctx.userLoggedIn = false;
