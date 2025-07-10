92e09dbcad09d6c98bd079141fcf572fa16e88aa
iswarya.pepakayala <Iswarya.Pepakayala@justice.gov.uk>
2025-07-10 14:29:58 +0100

    DTSPB-4523 Changed e2e

--- test/end-to-end/pages/ee/newsubmittedtohmrc.js
+++ test/end-to-end/pages/ee/newsubmittedtohmrc.js
@@ -5,8 +5,8 @@ module.exports = async function(language ='en', answer = null) {
     const commonContent = require(`app/resources/${language}/translation/common`);
 
     await I.checkInUrl('/new-submitted-to-hmrc');
-    await I.waitForEnabled(`#estateValueCompleted${answer}`);
-    await I.click(`#estateValueCompleted${answer}`);
+    await I.waitForEnabled(`#ihtFormEstateId${answer}`);
+    await I.click(`#ihtFormEstateId${answer}`);
     await I.navByClick(commonContent.saveAndContinue, 'button.govuk-button');
 
 };
