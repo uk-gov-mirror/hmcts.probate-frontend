9f5e8036058d7d2befc4a95278a14c0bf1d3497a
iswarya.pepakayala <Iswarya.Pepakayala@justice.gov.uk>
2025-08-13 12:52:05 +0100

    DTSPB-4740 Fix text with multiple links

--- app/utils/DocumentPageUtil.js
+++ app/utils/DocumentPageUtil.js
@@ -103,7 +103,8 @@ class DocumentPageUtil {
             checkListItems.push(this.getCheckListItemTextOnly(content['checklist-item10-iht207']));
         }
         if (executorsWrapper.hasRenunciated()) {
-            checkListItems.push(this.getCheckListItemTextWithLink(content['checklist-item8-renunciated'], config.links.renunciationForm));
+            checkListItems.push(this.getCheckListItemTextWithLinks(content['checklist-item8-renunciated'],
+                [config.links.applicationFormPA15, config.links.applicationFormPA17]));
         }
         if (executorsWrapper.executorsNameChangedByDeedPoll() && executorsWrapper.executorsNameChangedByDeedPoll().length > 0) {
             executorsWrapper.executorsNameChangedByDeedPoll().forEach(executor => {
@@ -139,7 +140,69 @@ class DocumentPageUtil {
         }
         throw new Error(`there is no link in content item: "${contentCheckListItem}"`);
     }
+    static getCheckListItemTextWithLinks(contentCheckListItem, links) {
+        if (!Array.isArray(links) || links.length === 0) {
+            throw new Error('please pass at least one valid url');
+        }
 
+        const splitContentItem = contentCheckListItem.split(/(<a.*?<\/a>)/g).filter(Boolean);
+        const result = [];
+        let linkIndex = 0;
+
+        for (let i = 0; i < splitContentItem.length; i++) {
+            if (splitContentItem[i].includes('<a')) {
+                if (!links[linkIndex]) {
+                    throw new Error('Not enough links provided for the number of anchors in content');
+                }
+                const linkText = splitContentItem[i].replace(/<a.*?>(.*?)<\/a>/, '$1');
+                result.push({
+                    text: linkText,
+                    type: 'textWithLink',
+                    url: links[linkIndex],
+                    beforeLinkText: splitContentItem[i - 1] || '',
+                    afterLinkText: splitContentItem[i + 1] || ''
+                });
+                // eslint-disable-next-line no-plusplus
+                linkIndex++;
+            }
+        }
+        if (!result.length) {
+            throw new Error(`there is no link in content item: "${contentCheckListItem}"`);
+        }
+        console.log('CheckListItemTextWithLinks result:', JSON.stringify(result));
+        return result.map(item => item.text).join('');
+    }
+
+    static getCheckListItemTextWithLinksMerged(contentCheckListItem, links) {
+        if (!Array.isArray(links) || links.length === 0) {
+            throw new Error('please pass at least one valid url');
+        }
+
+        const splitContentItem = contentCheckListItem.split(/(<a.*?<\/a>)/g).filter(Boolean);
+        let linkIndex = 0;
+        let mergedText = '';
+
+        for (let i = 0; i < splitContentItem.length; i++) {
+            if (splitContentItem[i].includes('<a')) {
+                if (!links[linkIndex]) {
+                    throw new Error('Not enough links provided for the number of anchors in content');
+                }
+                const linkText = splitContentItem[i].replace(/<a.*?>(.*?)<\/a>/, '$1');
+                mergedText += `<a href="${links[linkIndex]}" target="_blank" aria-label="${linkText}">${linkText}</a>`;
+                // eslint-disable-next-line no-plusplus
+                linkIndex++;
+            } else {
+                mergedText += splitContentItem[i];
+            }
+        }
+
+        return {
+            type: 'textWithLink',
+            beforeLinkText: mergedText, // send everything as one string
+            url: '',
+            text: ''
+        };
+    }
     static noDocsRequired(formdata) {
         const documentsWrapper = new DocumentsWrapper(formdata);
         return !documentsWrapper.documentsRequired();
