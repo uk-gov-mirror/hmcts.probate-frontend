041783474cf55a9c19537394a4efc85f8477141e
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2025-09-17 17:00:19 +0100

    DTSPB-4967 remove ctx from isFtFeesIncrease2025 toggle

--- app/steps/ui/copies/uk/index.js
+++ app/steps/ui/copies/uk/index.js
@@ -17,8 +17,8 @@ class CopiesUk extends ValidationStep {
     }
 
     handleGet(ctx, formdata, featureToggles) {
-        ctx.isFtFeesIncrease2025 = featureToggle.isEnabled(featureToggles, 'ft_probate_fee_increase_2025');
-        ctx.copiesUKFee = ctx.isFtFeesIncrease2025 ? config.fee.copiesUKFee2025 : config.fee.copiesUKFee;
+        const isFtFeesIncrease2025 = featureToggle.isEnabled(featureToggles, 'ft_probate_fee_increase_2025');
+        ctx.copiesUKFee = isFtFeesIncrease2025 ? config.fee.copiesUKFee2025 : config.fee.copiesUKFee;
         return [ctx];
     }
 
