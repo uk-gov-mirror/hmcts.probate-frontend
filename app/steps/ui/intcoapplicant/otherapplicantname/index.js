c13a5779b009a98582b70760cc6331d701491fd4
Jamie Albertelli <James.Albertelli@justice.gov.uk>
2025-10-06 10:59:37 +0100

    DTSPB-4728: Add other applicant WIP changes

---
+++ app/steps/ui/intcoapplicant/otherapplicantname/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const ValidationStep = require('app/core/steps/ValidationStep');
+const pageUrl = '/other-applicant-name';
+
+class OtherApplicantName extends ValidationStep {
+
+    static getUrl(index = '*') {
+        return `${pageUrl}/${index}`;
+    }
+
+    handleGet(ctx) {
+        if (ctx.list[ctx.index]) {
+            ctx.otherApplicant = ctx.list[ctx.index].otherApplicant;
+        }
+        return [ctx];
+    }
+
+    getContextData(req) {
+        const ctx = super.getContextData(req);
+        //const formdata = req.session.form;
+        return ctx;
+    }
+
+    shouldPersistFormData() {
+        return false;
+    }
+
+    nextStepUrl(req, ctx) {
+        return this.next(req, ctx).constructor.getUrl('adoptedIn');
+    }
+
+    nextStepOptions(ctx) {
+        //ctx.continue = get(ctx, 'index', -1) !== -1;
+    }
+}
+
+module.exports = OtherApplicantName;
