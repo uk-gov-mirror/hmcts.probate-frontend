8c0916861ef927cd24ff0da067d4029a722c8de8
Jamie Albertelli <James.Albertelli@justice.gov.uk>
2025-11-10 12:50:15 +0000

    DTSPB-4743: Add sibling adopted in screen

---
+++ app/steps/ui/details/siblingadoptedin/index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const ValidationStep = require('app/core/steps/ValidationStep');
+const FormatName = require('../../../../utils/FormatName');
+
+class SiblingAdoptedIn extends ValidationStep {
+
+    static getUrl() {
+        return '/sibling-adopted-in';
+    }
+
+    getContextData(req) {
+        const ctx = super.getContextData(req);
+        const formdata = req.session.form;
+        ctx.deceasedName = FormatName.format(formdata.deceased);
+        return ctx;
+    }
+
+    generateFields(language, ctx, errors) {
+        const fields = super.generateFields(language, ctx, errors);
+        if (fields.deceasedName && errors) {
+            errors[0].msg = errors[0].msg.replace('{deceasedName}', fields.deceasedName.value);
+        }
+        return fields;
+    }
+
+    nextStepOptions() {
+        return {
+            options: [
+                {key: 'siblingAdoptedIn', value: 'optionYes', choice: 'siblingAdoptedIn'},
+                {key: 'siblingAdoptedIn', value: 'optionNo', choice: 'siblingNotAdoptedIn'},
+            ]
+        };
+    }
+
+    handlePost(ctx, errors, formdata) {
+        const applicant = formdata.applicant;
+        ctx.relationshipToDeceased = applicant && applicant.relationshipToDeceased;
+        return [ctx, errors];
+    }
+}
+
+module.exports = SiblingAdoptedIn;
